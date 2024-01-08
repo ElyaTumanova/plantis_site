94837c5b336587943f303c86485b604cf42045ed
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-08 20:39:19 +0300

    search popup

--- assets/js/search-popup.js
+++ assets/js/search-popup.js
@@ -1,6 +1,6 @@
 const searchBtn = document.querySelector('.search-btn');
 const searchPopup = document.querySelector('.search-popup');
-console.log (searchBtn);
+console.log (searchPopup);
 searchBtn.addEventListener ("click", (evt)=>{
     searchPopup.classList.add ('search-popup_active');
 });
