eb420e4e6f0f92a2aa57392d7198469ef6e7135a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-08 15:25:07 +0300

    search update

--- includes/ajax.php
+++ includes/ajax.php
@@ -26,6 +26,7 @@ function plnt_search_ajax_action_callback (){
                 <div class="search-result__item">
                     <a href="<?php echo get_permalink();?>" class="search-result__link" target="blank">
                         <span class="search-result__title"><?php echo get_the_title();?></span>
+                        <span class="search-result__title"><?php echo get_the_content();?></span>
                     </a>  
                 </div>
             </div>
