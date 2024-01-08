09052963443c4aede895c0091a8e916f8edf5880
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-08 20:38:17 +0300

    search popup

--- assets/js/search-popup.js
+++ assets/js/search-popup.js
@@ -1,2 +1,6 @@
 const searchBtn = document.querySelector('.search-btn');
+const searchPopup = document.querySelector('.search-popup');
 console.log (searchBtn);
+searchBtn.addEventListener ("click", (evt)=>{
+    searchPopup.classList.add ('search-popup_active');
+});
--- template-parts/popups/search-popup.php
+++ template-parts/popups/search-popup.php
@@ -3,8 +3,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-	
-<div class="search">
-    <?get_search_form();?>
-    <div class="search-result"></div>
-</div><!-- .search -->
+<div class="search-popup">
+    <div class="search">
+        <?get_search_form();?>
+        <div class="search-result"></div>
+    </div>
+</div>	
