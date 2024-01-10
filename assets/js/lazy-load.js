87d1929d6c43e7eca55e03617376a6d75c2ded87
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-10 18:51:28 +0300

    lazy load

--- assets/js/lazy-load.js
+++ assets/js/lazy-load.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
         entries.forEach(function(entry) {
           if (entry.isIntersecting) {
             var image = entry.target;
+            console.log (image);
             image.src = image.dataset.src;
             image.classList.remove("lazy");
             imageObserver.unobserve(image);
