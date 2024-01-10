19ff21562ee4674ae893f2f209ce344c42e66526
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-10 23:09:58 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -4,9 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
+// табы
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 
 
+// фото товара, бейдж распродажа
 remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
 remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20 );
 
@@ -20,6 +22,22 @@ function plnt_product_image_wrap () {
         woocommerce_show_product_images();
     ?>
     </div>
+    <?php 
+}
+
+//цена и кнопка в корзину
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+
+add_action('woocommerce_after_single_product_summary', 'plnt_price_wrap', 10);
+
+function plnt_price_wrap(){
+    ?>
+    <div class="product__price-wrap">
     <?php
-    
+        woocommerce_template_single_price();
+        woocommerce_template_single_add_to_cart();
+    ?>
+    </div>
+    <?php 
 }
