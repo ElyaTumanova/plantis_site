a760ad1bf37a9dba13e35f8e2e4309edb3bb0cc1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-12 22:10:45 +0300

    card

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -28,6 +28,9 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 
 		wp_enqueue_script( 'quantity-buttons', get_template_directory_uri() .
 		                                     '/assets/js/quantity-buttons.js', array( 'jquery' ), null, true );
+											 
+		wp_enqueue_script( 'jquery.flexslider', get_template_directory_uri() .
+		                                     '/assets/js/jquery.flexslider.js', array( 'jquery' ), null, true );
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
