51faa664bd3680dc5a981f229ec39069e103766c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-12 19:43:03 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -38,7 +38,7 @@ function truemisha_sale_badge() {
 		$percentage = ( ( $product->get_regular_price() - $product->get_sale_price() ) / $product->get_regular_price() ) * 100;
  
 	if ( $percentage > 0 ) {
-		echo '<div class="onsale">Скидка ' . round( $percentage ) . '%</div>';
+		echo '<div class="onsale"><span class="onsale__skidka">Скидка </span>' . round( $percentage ) . '%</div>';
 	}
 }
 
