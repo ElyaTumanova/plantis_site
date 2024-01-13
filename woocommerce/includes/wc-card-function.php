133d3f94dc16b5dde8d536ba7e859b3439d0315b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-13 11:19:29 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -156,6 +156,7 @@ add_filter('woocommerce_upsell_display_args', function ($args) {
 add_filter( 'woocommerce_product_upsells_products_heading' , 'plnt_upsells_heading' );
 
 function plnt_upsells_heading () {
+    echo $plants_cat_id;
     $idCats = check_category ();
     foreach ($idCats as $cat){
         switch ($cat) {
