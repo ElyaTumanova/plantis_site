72bea815d6f8493494e4b43e34024f3abe49c11b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-13 12:23:41 +0300

    constants

--- includes/constants.php
+++ includes/constants.php
@@ -3,7 +3,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-global $plants_cat_id = 90;
+global $plants_cat_id;
+$plants_cat_id = 90;
 $gorshki_cat_id = 86;
 $treez_cat_id = 802;
 
