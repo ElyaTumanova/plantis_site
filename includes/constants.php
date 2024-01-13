11f3054c17806647b25e8defda751df1dc12bdfe
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-13 12:45:08 +0300

    constants

--- includes/constants.php
+++ includes/constants.php
@@ -3,6 +3,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+global carbon_get_theme_option();
 // constants for categories IDs
 //$plants_cat_id = 90;
 $plants_cat_id = carbon_get_theme_option('plants_cat_id');
