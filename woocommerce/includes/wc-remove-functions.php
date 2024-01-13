b9813179fa9806800229b66e6de72be71dfec73a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-13 14:59:05 +0300

    wc styles

--- woocommerce/includes/wc-remove-functions.php
+++ woocommerce/includes/wc-remove-functions.php
@@ -7,16 +7,16 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Set WooCommerce image dimensions upon theme activation
  */
 // Remove each style one by one
-add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
-function jk_dequeue_styles( $enqueue_styles ) {
-	//unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
-	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
-	//unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
-	return $enqueue_styles;
-}
+// add_filter( 'woocommerce_enqueue_styles', 'jk_dequeue_styles' );
+// function jk_dequeue_styles( $enqueue_styles ) {
+// 	unset( $enqueue_styles['woocommerce-general'] );	// Remove the gloss
+// 	unset( $enqueue_styles['woocommerce-layout'] );		// Remove the layout
+// 	unset( $enqueue_styles['woocommerce-smallscreen'] );	// Remove the smallscreen optimisation
+// 	return $enqueue_styles;
+// }
 
 // Or just remove them all in one line
-//add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
 /*remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
