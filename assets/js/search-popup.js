81ba0311897cd1d01f5262c9a2bd850812343619
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-14 14:44:29 +0300

    search

--- assets/js/search-popup.js
+++ assets/js/search-popup.js
@@ -1,9 +1,25 @@
 const searchBtn = document.querySelector('.search-btn');
 const searchPopup = document.querySelector('.search-popup');
+const searchClose = document.querySelector('.search__close');
+const searchOverlay = document.querySelector('.popup-overlay');
+
+
 // const searchInput = document.querySelector('.search'); 
 console.log (searchBtn);
-console.log (searchPopup);
+console.log (searchClose);
 searchBtn.addEventListener ("click", (evt)=>{
     searchPopup.classList.toggle ('search-popup_active');
     // searchInput.classList.add ('container');
 });
+searchClose.addEventListener ("click", (evt)=>{
+    searchPopup.classList.remove ('search-popup_active');
+});
+searchOverlay.addEventListener ("click", (evt)=>{
+    searchPopup.classList.remove ('search-popup_active');
+});
+
+document.addEventListener('keydown', function(e){
+    if((e.key=='Escape'||e.key=='Esc')){ 
+        searchPopup.classList.remove ('search-popup_active');
+    }
+}, true);
