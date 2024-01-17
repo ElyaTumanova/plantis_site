209bc46a8ba74d2599df833941ff12a827d37f3f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-17 18:35:17 +0300

    main

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@ get_header(); ?>
 	<div>может быть это главная?</div>
 	<main id="main" class="site-main" role="main">
 
-	<div class="main__banners">
+	<div class="main__banners nivo-main-banner-gallery">
 		<?php $banner_1 = carbon_get_theme_option('main_banner_1');?>
 		<?php $banner_1_name = carbon_get_theme_option('main_banner_1_name');?>
 		<?php $banner_2 = carbon_get_theme_option('main_banner_2');?>
@@ -16,12 +16,26 @@ get_header(); ?>
 		<?php $banner_4 = carbon_get_theme_option('main_banner_4');?>
 		<?php $banner_4_name = carbon_get_theme_option('main_banner_4_name');?>
 
-		<!-- <script>
-		console.log (<?php echo $banner_1 ?>);
-		console.log (<?php echo $banner_4 ?>);
-		</script> -->
-		<?php if() ?>
+
 		<img src="<?php echo $banner_1 ?>" class="main__banner-img" alt="<?php echo $banner_1_name?>" width="900" height="280">
+		<img src="<?php echo $banner_2 ?>" class="main__banner-img" alt="<?php echo $banner_2_name?>" width="900" height="280">
+
+		<script>
+			jQuery(function($){
+				$('.nivo-catalog-gallery').nivoSlider({
+					effect: 'slideInLeft',               // эффекты, например: 'fold, fade, sliceDown, sliceDownLeft, sliceUp, sliceUpLeft, sliceUpDown, sliceUpDownLeft, slideInRight, slideInLeft'
+					animSpeed: 200,                 // скорость анимации
+					pauseTime: 3000,                // пауза между сменой слайдов
+					directionNav: true,             // нужно ли отображать кнопки перехода на следующий и предыдущий слайд
+					controlNav: true,               // 1,2,3... навигация (например в виде точек)
+					pauseOnHover: true,             // останавливать прокрутку слайдов при наведении мыши
+					manualAdvance: true,           // true - отключить автопрокрутку
+					prevText: '&#10094;',               // текст перехода на предыдущий слайд
+					nextText: '&#10095;',               // текст кнопки перехода на следующий слайд
+					randomStart: false,             // начинать со случайного слайда
+				});
+			});
+		</script>
 	</div>
 	<div class="main__sale-gallery">
 		<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="3" orderby="rand" category="komnatnye-rasteniya"]') ?>
