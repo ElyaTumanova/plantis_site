87d3c3f8f3893f62677a396402ff01ed23369c32
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-17 11:57:33 +0300

    catalog

---
+++ assets/js/catalog-menu.js
@@ -0,0 +1,15 @@
+const dropdown = document.querySelectorAll('.catalog__dropdown');
+console.log(dropdown);
+
+dropdown.forEach((el) => {
+	console.log(el);
+	const menu = el.querySelector('.catalog__dropdown-menu');
+	// const button = el.querySelector('.icon-arrow');
+	// console.log(menu);
+	button.addEventListener('click', function (event) {
+		console.log('Произошло событие', event.target);
+		menu.classList.toggle('catalog__dropdown-menu_show');
+		// button.classList.toggle('open');
+		event.stopPropagation();
+	})
+})
