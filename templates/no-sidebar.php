416e6a644608f22d5e36f9d8d7073b531a0a3919
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-17 16:41:33 +0300

    main

---
+++ templates/no-sidebar.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Полная ширина
+ */
+
+get_header(); ?>
+
+<div class="content-area">
+    <main id="main" class="site-main">
+
+        <?php
+        while ( have_posts() ) : the_post();
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                </header><!-- .entry-header -->
+
+                <div class="entry-content">
+                    <?php
+
+                    the_content();
+                    ?>
+                </div><!-- .entry-content -->
+
+            </article><!-- #post-<?php the_ID(); ?> -->
+        <?php
+        endwhile; // End of the loop.
+        ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+
+get_footer();
