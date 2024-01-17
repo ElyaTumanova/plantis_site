df0dcf119740161d7d2f46811c31d25267130146
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-17 17:02:27 +0300

    main

--- front-page.php
+++ front-page.php
@@ -5,6 +5,10 @@ get_header(); ?>
 <div class="content-area">
 	<div>может быть это главная?</div>
 	<main id="main" class="site-main" role="main">
+
+	<div class="catalog__sidebar">
+		<?php echo do_shortcode('[products on_sale="true" limit="8" columns="3" orderby="rand" category="komnatnye-rasteniya"]') ?>
+    </div>
 			
 	</main><!-- #main -->
 </div><!-- #primary -->
