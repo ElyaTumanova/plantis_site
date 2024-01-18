3fe7f9f1fcde28e8eed36f4385cfe953f493b8da
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-18 12:59:15 +0300

    main

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -39,9 +39,6 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 		wp_enqueue_script( 'catalog-menu', get_template_directory_uri() .
 		                                     '/assets/js/catalog-menu.js', array( 'jquery' ), null, true );
 
-		wp_enqueue_script( 'script-init', get_template_directory_uri() .
-		                                     '/assets/js/script-init.js', array( 'jquery' ), null, false );
-
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
