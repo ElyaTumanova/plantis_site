da293df15fffedf272482c22b29543ca08d2b21a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-18 14:05:30 +0300

    main

--- front-page.php
+++ front-page.php
@@ -11,37 +11,39 @@ get_header(); ?>
 		<div class="main__banners-wrap">
 			<?php get_template_part('template-parts/main-banners-gallery');?>
 		</div>
+
+		<div class="main__sale-gallery-wrap">
+			<div class="main__sale-gallery">
+				<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
 		
-		<div class="main__sale-gallery">
-			<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
-	
-			<script type="text/javascript">
-				jQuery(window).load(function() {
-					jQuery(".main-sale-slider > ul").flexisel({
-						visibleItems:3,
-						animationSpeed: 1000,
-						autoPlay: false,
-						autoPlaySpeed: 3000,
-						pauseOnHover: true,
-						enableResponsiveBreakpoints: true,
-						responsiveBreakpoints: {
-							portrait: {
-								changePoint:480,
-								visibleItems: 1
-							},
-							landscape: {
-								changePoint:640,
-								visibleItems:1
-							},
-							tablet: {
-								changePoint:768,
-								visibleItems: 1
+				<script type="text/javascript">
+					jQuery(window).load(function() {
+						jQuery(".main-sale-slider > ul").flexisel({
+							visibleItems:3,
+							animationSpeed: 1000,
+							autoPlay: false,
+							autoPlaySpeed: 3000,
+							pauseOnHover: true,
+							enableResponsiveBreakpoints: true,
+							responsiveBreakpoints: {
+								portrait: {
+									changePoint:480,
+									visibleItems: 1
+								},
+								landscape: {
+									changePoint:640,
+									visibleItems:1
+								},
+								tablet: {
+									changePoint:768,
+									visibleItems: 1
+								}
 							}
-						}
+						});
 					});
-				});
-			</script>
-		</div>			
+				</script>
+			</div>			
+		</div>
 	</div>
 
 	</main><!-- #main -->
