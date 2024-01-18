bbc36734aaaa8c9289203328ca68a2557225a836
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-18 13:20:02 +0300

    main

--- template-parts/main-banners-gallery.php
+++ template-parts/main-banners-gallery.php
@@ -20,3 +20,19 @@ if ( ! defined( 'ABSPATH' ) ) {
     <img src="<?php echo $banner_2 ?>" class="main__banner-img" style="visibility:hidden;" alt="<?php echo $banner_2_name?>" width="900" height="280">
 </div>
 
+<script>
+		jQuery(function($){
+			$('.nivo-main-banner-gallery').nivoSlider({
+				effect: 'fade',               // эффекты, например: 'fold, fade, sliceDown, sliceDownLeft, sliceUp, sliceUpLeft, sliceUpDown, sliceUpDownLeft, slideInRight, slideInLeft'
+				animSpeed: 200,                 // скорость анимации
+				pauseTime: 3000,                // пауза между сменой слайдов
+				directionNav: true,             // нужно ли отображать кнопки перехода на следующий и предыдущий слайд
+				controlNav: true,               // 1,2,3... навигация (например в виде точек)
+				pauseOnHover: true,             // останавливать прокрутку слайдов при наведении мыши
+				manualAdvance: true,           // true - отключить автопрокрутку
+				prevText: '&#10094;',               // текст перехода на предыдущий слайд
+				nextText: '&#10095;',               // текст кнопки перехода на следующий слайд
+				randomStart: false,             // начинать со случайного слайда
+			})
+		});
+	</script>
