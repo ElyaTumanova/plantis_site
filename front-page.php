8f76c63cfbaf118c02bcc11019bb0c8f222e3579
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-18 13:59:15 +0300

    main

--- front-page.php
+++ front-page.php
@@ -7,43 +7,43 @@ get_header(); ?>
 <div class="content-area">
 	<main id="main" class="site-main" role="main">
 
-	<div class="main__banners-wrap">
-		<?php get_template_part('template-parts/main-banners-gallery');?>
+	<div class="main__wrap">
+		<div class="main__banners-wrap">
+			<?php get_template_part('template-parts/main-banners-gallery');?>
+		</div>
+		
+		<div class="main__sale-gallery">
+			<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
+	
+			<script type="text/javascript">
+				jQuery(window).load(function() {
+					jQuery(".main-sale-slider > ul").flexisel({
+						visibleItems:3,
+						animationSpeed: 1000,
+						autoPlay: false,
+						autoPlaySpeed: 3000,
+						pauseOnHover: true,
+						enableResponsiveBreakpoints: true,
+						responsiveBreakpoints: {
+							portrait: {
+								changePoint:480,
+								visibleItems: 1
+							},
+							landscape: {
+								changePoint:640,
+								visibleItems:1
+							},
+							tablet: {
+								changePoint:768,
+								visibleItems: 1
+							}
+						}
+					});
+				});
+			</script>
+		</div>			
 	</div>
 
-
-	
-	<div class="main__sale-gallery">
-		<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
-
-		<script type="text/javascript">
-            jQuery(window).load(function() {
-                jQuery(".main-sale-slider > ul").flexisel({
-                    visibleItems:3,
-                    animationSpeed: 1000,
-                    autoPlay: false,
-                    autoPlaySpeed: 3000,
-                    pauseOnHover: true,
-                    enableResponsiveBreakpoints: true,
-                    responsiveBreakpoints: {
-                        portrait: {
-                            changePoint:480,
-                            visibleItems: 1
-                        },
-                        landscape: {
-                            changePoint:640,
-                            visibleItems:1
-                        },
-                        tablet: {
-                            changePoint:768,
-                            visibleItems: 1
-                        }
-                    }
-                });
-            });
-		</script>
-    </div>
-			
 	</main><!-- #main -->
 </div><!-- #primary -->
 
