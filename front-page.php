3f401359b82569fefb187897b5df90244aa253dc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-19 14:21:31 +0300

    main

--- front-page.php
+++ front-page.php
@@ -16,7 +16,7 @@ get_header(); ?>
 			<div class="main__sale-gallery">
 				<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
 		
-				<script type="text/javascript">
+				<!-- <script type="text/javascript">
 					jQuery(window).load(function() {
 						jQuery(".main-sale-slider > ul").flexisel({
 							visibleItems:1,
@@ -41,7 +41,7 @@ get_header(); ?>
 							}
 						});
 					});
-				</script>
+				</script> -->
 			</div>			
 		</div>
 	</div>
