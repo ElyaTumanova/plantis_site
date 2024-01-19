312161448cac990e85be0341d76c12ca612f862c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-19 16:11:35 +0300

    main

--- front-page.php
+++ front-page.php
@@ -6,44 +6,45 @@ get_header(); ?>
 
 <div class="content-area">
 	<main id="main" class="site-main" role="main">
-
-	<div class="main__wrap container">
-		<div class="main__banners-wrap">
-			<?php get_template_part('template-parts/main-banners-gallery');?>
-		</div>
-
-		<div class="main__sale-gallery-wrap">
-			<h2 class="main__sale-gallery-title heading-2">Спецпредложения</h2>
-			<div class="main__sale-gallery">
-				<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
-		
-				<script type="text/javascript">
-					jQuery(window).load(function() {
-						jQuery(".main-sale-slider > ul").flexisel({
-							visibleItems:1,
-							animationSpeed: 1000,
-							autoPlay: false,
-							autoPlaySpeed: 3000,
-							pauseOnHover: true,
-							enableResponsiveBreakpoints: true,
-							responsiveBreakpoints: {
-								portrait: {
-									changePoint:480,
-									visibleItems: 1
-								},
-								landscape: {
-									changePoint:640,
-									visibleItems:1
-								},
-								tablet: {
-									changePoint:768,
-									visibleItems: 1
+	<div class="main__wrap-colored-bg">
+		<div class="main__wrap container">
+			<div class="main__banners-wrap">
+				<?php get_template_part('template-parts/main-banners-gallery');?>
+			</div>
+	
+			<div class="main__sale-gallery-wrap">
+				<h2 class="main__sale-gallery-title heading-2">Спецпредложения</h2>
+				<div class="main__sale-gallery">
+					<?php echo do_shortcode('[products on_sale="true" class="main-sale-slider" limit="8" columns="1" orderby="rand" category="komnatnye-rasteniya"]') ?>
+			
+					<script type="text/javascript">
+						jQuery(window).load(function() {
+							jQuery(".main-sale-slider > ul").flexisel({
+								visibleItems:1,
+								animationSpeed: 1000,
+								autoPlay: false,
+								autoPlaySpeed: 3000,
+								pauseOnHover: true,
+								enableResponsiveBreakpoints: true,
+								responsiveBreakpoints: {
+									portrait: {
+										changePoint:480,
+										visibleItems: 1
+									},
+									landscape: {
+										changePoint:640,
+										visibleItems:1
+									},
+									tablet: {
+										changePoint:768,
+										visibleItems: 1
+									}
 								}
-							}
+							});
 						});
-					});
-				</script>
-			</div>			
+					</script>
+				</div>			
+			</div>
 		</div>
 	</div>
 
