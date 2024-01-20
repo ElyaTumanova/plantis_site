b764dc635c8d52cc19755d6f3e11b953aff6b364
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-20 16:09:48 +0300

    cart

--- woocommerce/includes/wc-cart-functions.php
+++ woocommerce/includes/wc-cart-functions.php
@@ -45,7 +45,7 @@ function my_get_cart_amount () {
 // // изменяем кнопку "в корзину" после добавления товара в корзину
 
 
-add_filter( 'woocommerce_product_single_add_to_cart_text', 'truemisha_single_product_btn_text' ); // текст для страницы самого товара
+//add_filter( 'woocommerce_product_single_add_to_cart_text', 'truemisha_single_product_btn_text' ); // текст для страницы самого товара
  
 function truemisha_single_product_btn_text( $text ) {
 	if( WC()->cart->find_product_in_cart( WC()->cart->generate_cart_id( get_the_ID() ) ) ) {
@@ -56,7 +56,7 @@ function truemisha_single_product_btn_text( $text ) {
 }
  
 
-add_filter( 'woocommerce_product_add_to_cart_text', 'truemisha_product_btn_text', 20, 2 ); //текст для страниц каталога товаров, категорий товаров и т д
+//add_filter( 'woocommerce_product_add_to_cart_text', 'truemisha_product_btn_text', 20, 2 ); //текст для страниц каталога товаров, категорий товаров и т д
  
 function truemisha_product_btn_text( $text, $product ) {
 	if( 
@@ -70,3 +70,32 @@ function truemisha_product_btn_text( $text, $product ) {
 	}
 	return $text;
 }
+
+add_action( 'wp_footer', 'ajax_button_text_js_script' );
+function ajax_button_text_js_script() {
+	if (!(is_product() )){
+		$text = __('Добавлен', 'woocommerce');
+		$svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path fill="#fff" d="M8.305 20.982.179 12.856l2.031-2.031 6.095 6.094 13.08-13.08 2.03 2.032-15.11 15.111Z"/></svg>' 
+		?>;
+	   <script>
+		  jQuery(function($) {
+			   var text = '<?php echo $text; ?>',      $this;
+				var svg = '<?php echo $svg; ?>',      $this;
+				$(document.body).on('click', '.ajax_add_to_cart', function(event){
+					$this = $(this); 
+				});
+
+				$(document.body).on('added_to_cart', function(event,b,data){
+					
+					var buttonContent = '<span class="jet-woo-button-content"> <span class="button-label">'+text+'</span> <span class="button-icon"> '+svg+'</span> </span>'
+					var buttonText = '<span class="button-label">'+text+'</span>';
+					var buttonIcon = '<span class="button-icon"> '+svg+'</span>';
+					console.log ($this)
+					//$this.html(buttonText).attr('data-tip',text);
+					$this.html(buttonContent);
+				});
+			});
+		</script>
+	  <?php 
+	}
+ }
