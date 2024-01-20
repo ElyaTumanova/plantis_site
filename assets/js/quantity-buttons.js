8b337fe2e1a65689aa042d49853f9c2875f25d12
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-20 15:06:00 +0300

    cart

--- assets/js/quantity-buttons.js
+++ assets/js/quantity-buttons.js
@@ -24,7 +24,7 @@ jQuery(function ($){
 
 	$( '[name="update_cart"]' ).removeAttr("disabled").trigger( 'click' ); // автообновление корзины без перезагрузки
 
-	$.ajax( $fragment_refresh );
+	refresh_cart_fragment();
  
 });
 })
