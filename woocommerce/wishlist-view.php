518fcd37ad22676b5e776f7ccc132378ae398f54
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-21 13:30:14 +0300

    wishlist

--- woocommerce/wishlist-view.php
+++ woocommerce/wishlist-view.php
@@ -499,9 +499,10 @@ if ( ! defined( 'YITH_WCWL' ) ) {
 								<?php woocommerce_template_loop_add_to_cart( array( 'quantity' => $show_quantity ? $item->get_quantity() : 1 ) ); ?>
                                 <?php echo '<pre>';
                                 print_r( $item->get_stock_status() );
+                                print_r( add_to_cart_url() );
+                                print_r( add_to_cart_text() );
                                 echo '</pre>';
                                 
-                                echo apply_filters( 'woocommerce_loop_add_to_cart_args', wp_parse_args( $args, $defaults ), $product );
                                 ?>
 							<?php endif ?>
 
