174a516109060fe1f31b9cc59c86213593ef4b28
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-21 16:11:32 +0300

    pagination

--- functions.php
+++ functions.php
@@ -38,5 +38,4 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	require get_template_directory() . '/woocommerce/includes/wc-card-function.php';
 	require get_template_directory() . '/woocommerce/includes/wc-catalog-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-wishlist-finctions.php';
-	require get_template_directory() . '/woocommerce/includes/wc-pagination-functions.php';
 }
