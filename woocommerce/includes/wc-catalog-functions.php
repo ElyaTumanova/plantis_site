052ea1b941af030ac13aff65ae1371a3d7befff4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-21 15:57:31 +0300

    pagination

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -30,6 +30,15 @@ function plnt_catalog_sidebar() {
     <?php 
 };
 
+add_action('woocommerce_before_shop_loop','plnt_catalog_products_wrap_start',40);
+
+function plnt_catalog_products_wrap_start() {
+	?>
+    <div class="catalog__products-wrap">
+    </div>
+    <?php 
+};
+
 remove_action('woocommerce_sidebar','woocommerce_get_sidebar', 10);
 
 add_action('woocommerce_after_shop_loop','plnt_catalog_grid_end',20);
