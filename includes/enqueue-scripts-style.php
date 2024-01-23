6439a85f3d15c66482433f2730591895e7edc8c1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-23 13:43:20 +0300

    preorder

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -65,6 +65,8 @@ if ( ! function_exists( 'ast_styles' ) ) {
 		                             '/assets/css/card.css', array(), null, 'all' );
 		wp_enqueue_style( 'catalog', get_template_directory_uri() .
 		                             '/assets/css/catalog.css', array(), null, 'all' );
+		wp_enqueue_style( 'catalog', get_template_directory_uri() .
+		                             '/assets/css/popup.css', array(), null, 'all' );
 		// wp_enqueue_style( 'progressive-image', get_template_directory_uri() .
 		//                              '/assets/css/progressive-image.css', array(), null, 'all' ); // for lazy load
 		wp_enqueue_style( 'FlexSlider', get_template_directory_uri() .
--- template-parts/popups/preorder-popup.php
+++ template-parts/popups/preorder-popup.php
@@ -3,11 +3,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<div class="preorder-popup">
+<div class="preorder-popup popup popup_active">
     <div class="preorder container">
         <div class="preorder__wrap">
-            ТУТ БУДЕТ ФОРМА
-            <div class="preorder__close">&#10006;</div>
+            <h2 class="preopder__heading heading-2">Предзаказ товара</h2>
+            <span class="preorder__close">&#10006;</span>
+            <div class="preorder__form">ТУТ БУДЕТ ФОРМА</div>
+            <p class="preorder__text">Нажимая кнопку "Отправить", вы даете согласие на обработку своих персональных данных и соглашаетесь с положениями, описанными в нашей политике конфиденциальности.</p>
         </div>
     </div>
     <div class="popup-overlay"></div>
