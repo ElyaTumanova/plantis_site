0be21838d8e1bbf8118d7ba8f857a2a20d07f707
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-23 15:49:43 +0300

    preorder

--- assets/js/preorder-popup.js
+++ assets/js/preorder-popup.js
@@ -4,26 +4,30 @@ const preorderPopup = document.querySelector('.preorder-popup');
 const preorderClosePopupBtn = document.querySelector('.preorder__close');
 const preorderPopupOverlay = document.querySelector('.preorder__popup-overlay');
 
+if (preorderPopup === null || preorderOpenPopupBtn === null) {
+    return;
+} else {
+    preorderOpenPopupBtn.addEventListener ("click", (evt)=>{
+        toggle_preorder_popup ();
+    });
 
-preorderOpenPopupBtn.addEventListener ("click", (evt)=>{
-    toggle_preorder_popup ();
-});
 
-preorderClosePopupBtn.addEventListener ("click", (evt)=>{
-    toggle_preorder_popup ();
-});
+    preorderClosePopupBtn.addEventListener ("click", (evt)=>{
+        toggle_preorder_popup ();
+    });
 
-preorderPopupOverlay.addEventListener ("click", (evt)=>{
-    toggle_preorder_popup ();
-});
+    preorderPopupOverlay.addEventListener ("click", (evt)=>{
+        toggle_preorder_popup ();
+    });
 
-document.addEventListener('keydown', function(e){
-    if((e.key=='Escape'||e.key=='Esc')){
-        if(preorderPopup.classList.contains('popup_active')) {
-            toggle_preorder_popup ();
-        } 
-    }
-}, true);
+    document.addEventListener('keydown', function(e){
+        if((e.key=='Escape'||e.key=='Esc')){
+            if(preorderPopup.classList.contains('popup_active')) {
+                toggle_preorder_popup ();
+            } 
+        }
+    }, true);
+}
 
 function toggle_preorder_popup () {
     preorderPopup.classList.toggle ('popup_active');
--- footer.php
+++ footer.php
@@ -5,7 +5,12 @@
 
 <?php get_template_part('template-parts/popups/search-popup');?>
 
-<?php wc_get_template_part('template-parts/popups/preorder-popup');?>
+<?php 
+    global $product;
+    if (is_product() && $product->get_stock_status() ==='outofstock') {
+        wc_get_template_part('template-parts/popups/preorder-popup');
+    }
+?>
 
 <?php wp_footer(); ?>
 
