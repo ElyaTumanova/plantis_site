06955ded073352f4305781f6bcea87795730f0b2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-23 13:45:16 +0300

    preorder

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -65,7 +65,7 @@ if ( ! function_exists( 'ast_styles' ) ) {
 		                             '/assets/css/card.css', array(), null, 'all' );
 		wp_enqueue_style( 'catalog', get_template_directory_uri() .
 		                             '/assets/css/catalog.css', array(), null, 'all' );
-		wp_enqueue_style( 'catalog', get_template_directory_uri() .
+		wp_enqueue_style( 'popup', get_template_directory_uri() .
 		                             '/assets/css/popup.css', array(), null, 'all' );
 		// wp_enqueue_style( 'progressive-image', get_template_directory_uri() .
 		//                              '/assets/css/progressive-image.css', array(), null, 'all' ); // for lazy load
