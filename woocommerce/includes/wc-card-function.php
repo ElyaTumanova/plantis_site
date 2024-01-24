09ceb25313603fe4e82e020f02a9f09e3e438a4f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-24 16:44:32 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -86,6 +86,7 @@ function plnt_product_gallery( $options ) {
 //цена и кнопка в корзину, кнопка в избранное
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+remove_action('woocommerce_before_single_product','woocommerce_output_all_notices',10); /* уведомления woocommerce*/
 
 add_action('woocommerce_after_single_product_summary', 'plnt_price_wrap', 5);
 
@@ -98,6 +99,7 @@ function plnt_price_wrap(){
         woocommerce_template_single_add_to_cart();
         //plnt_wish_wrap(); //кнопка в избранное для be rocket wishlist
         plnt_outofstock_info();
+        woocommerce_output_all_notices();
         get_template_part('template-parts/delivery-info'); // delivery info for card
         ?>
     </div>
@@ -306,3 +308,5 @@ function plnt_category_link () {
 	<a href="' . $link . '">'.$text.'</a>
     </div>';
 }
+
+
