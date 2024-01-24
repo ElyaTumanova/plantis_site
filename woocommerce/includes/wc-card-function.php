96283b89793e8a022adf4010032f7b6f4f7fd543
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-24 21:50:28 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -88,6 +88,8 @@ remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_singl
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 //remove_action('woocommerce_before_single_product','woocommerce_output_all_notices', 10); /* уведомления woocommerce*/
 
+add_filter( 'woocommerce_cart_redirect_after_error', '__return_false' );
+
 /**
  * Add fragments for notices
  */
