43a96773df37d61d4b3207f6a74ff6d74bee793e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-24 14:14:18 +0300

    main

---
+++ template-parts/plnt-cross-sells.php
@@ -0,0 +1,86 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+
+    $crosssell_ids = get_post_meta( get_the_ID(), '_crosssell_ids' );
+
+    if( !empty ($crosssell_ids) ){
+
+        $crosssell_ids = $crosssell_ids[0];
+
+        if(count($crosssell_ids)>0){
+
+            $args = array(
+                'post_type' => 'product',
+                'ignore_sticky_posts' => 1,
+                'no_found_rows' => 1,
+                'posts_per_page' => 8,
+                'orderby' => $rand,
+                'post__in' => $crosssell_ids,
+                'meta_query' => array( 
+                    array(
+                        'key'       => '_stock_status',
+                        'value'     => 'outofstock',
+                        'compare'   => 'NOT IN'
+                    )
+                )
+            );
+
+            $products = new WP_Query( $args );
+
+            if ( $products->have_posts() ) : ?>
+
+                <div class="cross-sells">
+
+                <h2 class="heading-2"><?php _e( 'Похожие растения', 'woocommerce' ) ?></h2>
+
+                <ul id="flexisel-cross-sells" class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
+
+                    <?php while ( $products->have_posts() ) : $products->the_post(); ?>
+
+                    <?php wc_get_template_part( 'content', 'product' ); ?>
+
+                    <?php endwhile; // end of the loop. ?>
+
+                </ul>
+
+                <script type="text/javascript">
+                    jQuery(window).load(function() {
+                        jQuery("#flexisel-cross-sells").flexisel({
+                            visibleItems:3,
+                            columnGaps: 30,
+                            animationSpeed: 1000,
+                            autoPlay: false,
+                            autoPlaySpeed: 3000,
+                            pauseOnHover: true,
+                            enableResponsiveBreakpoints: true,
+                            responsiveBreakpoints: {
+                                portrait: {
+                                    changePoint:480,
+                                    visibleItems: 1
+                                },
+                                landscape: {
+                                    changePoint:640,
+                                    visibleItems:2
+                                },
+                                tablet: {
+                                    changePoint:768,
+                                    visibleItems: 3
+                                }
+                            }
+                        });
+                    });
+                </script>
+
+                </div>
+
+            <?php endif;
+
+        }
+
+        wp_reset_query();
+
+    }
+
--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -21,7 +21,7 @@ function plnt_card_grid_end () {
     <?php 
 };
 
-// табы и описание
+// табы(убираем) и описание
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 add_action('woocommerce_before_single_product_summary', 'plnt_product_description', 10);
 
@@ -256,85 +256,7 @@ add_action('woocommerce_after_single_product_summary','plnt_get_cross_sells', 20
 
 
 function plnt_get_cross_sells(){
-    $crosssell_ids = get_post_meta( get_the_ID(), '_crosssell_ids' );
-
-    if( !empty ($crosssell_ids) ){
-
-        $crosssell_ids = $crosssell_ids[0];
-
-        if(count($crosssell_ids)>0){
-
-            $args = array(
-                'post_type' => 'product',
-                'ignore_sticky_posts' => 1,
-                'no_found_rows' => 1,
-                'posts_per_page' => 8,
-                'orderby' => $rand,
-                'post__in' => $crosssell_ids,
-                'meta_query' => array( 
-                    array(
-                        'key'       => '_stock_status',
-                        'value'     => 'outofstock',
-                        'compare'   => 'NOT IN'
-                    )
-                )
-            );
-
-            $products = new WP_Query( $args );
-
-            if ( $products->have_posts() ) : ?>
-
-                <div class="cross-sells">
-
-                <h2 class="heading-2"><?php _e( 'Похожие растения', 'woocommerce' ) ?></h2>
-
-                <ul id="flexisel-cross-sells" class="products columns-<?php echo esc_attr( wc_get_loop_prop( 'columns' ) ); ?>">
-
-                    <?php while ( $products->have_posts() ) : $products->the_post(); ?>
-
-                    <?php wc_get_template_part( 'content', 'product' ); ?>
-
-                    <?php endwhile; // end of the loop. ?>
-
-                </ul>
-
-                <script type="text/javascript">
-                    jQuery(window).load(function() {
-                        jQuery("#flexisel-cross-sells").flexisel({
-                            visibleItems:3,
-                            columnGaps: 30,
-                            animationSpeed: 1000,
-                            autoPlay: false,
-                            autoPlaySpeed: 3000,
-                            pauseOnHover: true,
-                            enableResponsiveBreakpoints: true,
-                            responsiveBreakpoints: {
-                                portrait: {
-                                    changePoint:480,
-                                    visibleItems: 1
-                                },
-                                landscape: {
-                                    changePoint:640,
-                                    visibleItems:2
-                                },
-                                tablet: {
-                                    changePoint:768,
-                                    visibleItems: 3
-                                }
-                            }
-                        });
-                    });
-                </script>
-
-                </div>
-
-            <?php endif;
-
-        }
-
-        wp_reset_query();
-
-    }
+    get_template_part('template-parts/plnt-cross-sells');
 }
 
 
@@ -372,7 +294,7 @@ function plnt_category_link () {
 			}
 	}	
 	echo '<div class="card__toback-link">
-	<span>&#10094; </span>
+	<span>&#10094;</span>
 	<a href="' . $link . '">'.$text.'</a>
     </div>';
 }
