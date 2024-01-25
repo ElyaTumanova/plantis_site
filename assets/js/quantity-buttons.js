24b51bde7164bed0dda95f3ff7d3af5f0cd09835
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-25 12:02:59 +0300

    card

--- assets/js/quantity-buttons.js
+++ assets/js/quantity-buttons.js
@@ -27,7 +27,7 @@ jQuery(function ($){
 	}
 
 	qty.attr('value', newVal );  //устанавливаем новое значение для инпута
-	qty.parent().parent().find(".add_to_cart_button").attr( 'data-quantity', newVal ); //устанавливаем новое значение для атрибута кнопки добавить в корзину. div "quantity" должен идти сразу за кнопкой в корзирну
+	qty.parent().parent().find(".add_to_cart_button").attr( 'data-quantity', newVal ); //устанавливаем новое значение для атрибута кнопки добавить в корзину. div "quantity" должен находится в одном родительском узле с кнопкой в корзирну
 
 
 	$( '[name="update_cart"]' ).removeAttr("disabled").trigger( 'click' ); // автообновление корзины без перезагрузки
@@ -40,6 +40,6 @@ jQuery(function ($){
 jQuery(function ($){	
 	$( 'div.quantity .qty' ).change( function() {
 	const qty = $(this).val();
-	$(this).parent().prev(".add_to_cart_button").attr( 'data-quantity', qty );
+	$(this).parent().parent().find(".add_to_cart_button").attr( 'data-quantity', qty );
 	} )
 });
