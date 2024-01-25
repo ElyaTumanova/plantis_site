5f2f0e5924be34fd0320d8399906a0641c7468f2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-25 11:00:38 +0300

    card

--- assets/js/quantity-buttons.js
+++ assets/js/quantity-buttons.js
@@ -1,43 +1,46 @@
 jQuery(function ($){
+	console.log('hi');
     $( 'body' ).on( 'click', 'button.plus, button.minus', function() {
  
 	var qty = $(this).parent().find( 'input' ),
-	    val = parseInt( qty.val() ),
-	    min = parseInt( qty.attr( 'min' ) ),
-	    max = parseInt( qty.attr( 'max' ) ),
-	    step = parseInt( qty.attr( 'step' ) );
+	val = parseInt( qty.val() ),
+	min = parseInt( qty.attr( 'min' ) ),
+	max = parseInt( qty.attr( 'max' ) ),
+	step = parseInt( qty.attr( 'step' ) );
+	console.log($(this));
+	console.log(qty);
  
 	// дальше меняем значение количества в зависимости от нажатия кнопки
-	if ( $( this ).is( '.plus' ) ) {
-		if ( max && ( max <= val ) ) {
-			qty.val( max );
-		} else {
-			qty.val( val + step );
-		}
-	} else {
-		if ( min && ( min >= val ) ) {
-			qty.val( min );
-		} else if ( val > 1 ) {
-			qty.val( val - step );
-		}
-	}
+	// if ( $( this ).is( '.plus' ) ) {
+	// 	if ( max && ( max <= val ) ) {
+	// 		qty.val( max );
+	// 	} else {
+	// 		qty.val( val + step );
+	// 	}
+	// } else {
+	// 	if ( min && ( min >= val ) ) {
+	// 		qty.val( min );
+	// 	} else if ( val > 1 ) {
+	// 		qty.val( val - step );
+	// 	}
+	// }
 
-	$( '[name="update_cart"]' ).removeAttr("disabled").trigger( 'click' ); // автообновление корзины без перезагрузки
+	//$( '[name="update_cart"]' ).removeAttr("disabled").trigger( 'click' ); // автообновление корзины без перезагрузки
  
 });
 })
 
 jQuery(function ($){
-	console.log('hi');
+	//console.log('hi');
 	
 	$( 'div.quantity .qty' ).change( function() {
-	console.log('hello');
+	//console.log('hello');
 
 	const qty = $(this).val();
-	console.log(qty);
-	console.log($(this).parent().prev());
+	//console.log(qty);
+	//console.log($(this).parent().prev());
 	$(this).parent().prev().attr( 'data-quantity', qty );
-	console.log($(this).parent().prev());
+	//console.log($(this).parent().prev());
 
 	} )
 });
