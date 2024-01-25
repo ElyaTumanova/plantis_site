681c489f96aff4f1e2af5c6485c96a4804fd3314
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-25 17:39:44 +0300

    cart

--- woocommerce/cart/mini-cart.php
+++ woocommerce/cart/mini-cart.php
@@ -66,7 +66,7 @@ do_action( 'woocommerce_before_mini_cart' ); ?>
 						<a class="product" href="<?php echo esc_url( $product_permalink ); ?>">
 							<?php echo $thumbnail; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 						</a>
-                        <span class="product-name"><?php echo wp_kses_post( $product_name )?>;</span>
+                        <span class="product-name"><?php echo wp_kses_post( $product_name )?></span>
 					<?php endif; ?>
 					<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 					<?php echo apply_filters( 'woocommerce_widget_cart_item_quantity', '<span class="quantity">' . sprintf( '%s &times; %s', $cart_item['quantity'], $product_price ) . '</span>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
