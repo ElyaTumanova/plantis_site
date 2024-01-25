04fecd83dfe7be4d68c4d5faf77b3f351ddb52bd
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-25 16:28:14 +0300

    cart

--- template-parts/card-banners.php
+++ template-parts/card-banners.php
@@ -10,5 +10,5 @@ if ( ! defined( 'ABSPATH' ) ) {
     <?php get_template_part('template-parts/social-media-btns');?>
 </div>
 <div class="card_banner">
-    <p>При покупке <a href="<?php get_site_url()?>/product-category/gorshki_i_kashpo/" target="_blank" rel="noopener">горшка </a>— пересадка в подарок!</p>
+    <p>При покупке <a href="<?php echo get_site_url()?>/product-category/gorshki_i_kashpo/" target="_blank" rel="noopener">горшка </a>— пересадка в подарок!</p>
 </div>
