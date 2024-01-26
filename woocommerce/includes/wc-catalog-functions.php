fa4cc89c8ceb4074f2ed7759df46a935ecafbb04
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-26 12:41:30 +0300

    catalog

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -56,6 +56,7 @@ function plnt_catalog_sidebar() {
     <div class="catalog__sidebar">
 		<?php plnt_catalog_menu() ?>
 		<div class="catalog__sidebar-filters">
+			<?php echo do_shortcode('[br_filter_single filter_id=6110]') ?>
 			<?php echo do_shortcode('[br_filter_single filter_id=6055]') ?>
 			<?php echo do_shortcode('[br_filter_single filter_id=6056]') ?>
 			<?php echo do_shortcode('[br_filter_single filter_id=6108]') ?>
