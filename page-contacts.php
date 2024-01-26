b6d08e93e77eb152c955c50a1caefc9bc1a4c068
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-26 15:51:08 +0300

    catalog

--- page-contacts.php
+++ page-contacts.php
@@ -4,9 +4,41 @@
 get_header(); ?>
 
 <div class="content-area">
-    <div>Это контакты<div>
+
 	<main id="main" class="site-main" role="main">
-	
+	<header class="entry-header">
+        <h1 class="entry-title">Контакты</h1>                
+	</header>
+
+	<div class="contacts__phones">
+		<h2 class="contacts__phones-heading heading-2">Телефон</h2>
+		<a class="contacts__phone" href="tel:+78002015790">8 800 201 57 90</a>
+		<a class="contacts__phone" href="tel:+79647687944">8 964 768 79 44</a>
+	</div>
+
+	<div class="contacts__mail">
+		<h2 class="contacts__mail-heading heading-2">Почта</h2>
+		<a class="contacts__mail-link" href="mailto:INFO@PLANTIS.SHOP">INFO@PLANTIS.SHOP</a>
+	</div>
+
+	<div class="contacts__social-media">
+		<?php get_template_part('template-parts/social-media-btns');?>
+	</div>
+
+	<div class="contacts__work-hours">
+		<h2 class="contacts__work-hours-heading heading-2">Часы работы</h2>
+		<p>Ежедневно с 10 до 20</p>
+	</div>
+
+	<div class="contacts__adress">
+		<h2 class="contacts__work-hours-heading heading-2">Адрес</h2>
+		<p>г. Москва, ул. Мещерякова, д.3</p>
+		<p>м. Тушинская</p>
+	</div>
+	<div class="contacts__map">
+		<iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=237252555639" width="560" height="400" frameborder="0"></iframe>
+	</div>
+		
 	
 	</main><!-- #main -->
 </div><!-- #primary -->
