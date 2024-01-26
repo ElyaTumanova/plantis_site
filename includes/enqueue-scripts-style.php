c88bf9de4cac963c23be884ceb33c86474619ec1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-26 10:48:57 +0300

    catalog

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -80,7 +80,13 @@ if ( ! function_exists( 'ast_styles' ) ) {
 		wp_enqueue_style( 'nivo-slider', get_template_directory_uri() .
 		                             '/assets/css/nivo-slider.css', array(), null, 'all' );
 		wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap', array(), null, 'all' );
-
-		wp_dequeue_style( 'berocket_aapf_widget-style' );
 	}
+}
+
+add_action( 'wp_enqueue_scripts', 'plnt_styles_off', 999 );
+ 
+function plnt_styles_off() {
+
+	wp_dequeue_style( 'berocket_aapf_widget-style' );
+ 
 }
