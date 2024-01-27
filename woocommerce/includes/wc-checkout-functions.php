f0cf14580540e99e6e4a0da7fc729db51aa55b0a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-27 16:07:20 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -2,3 +2,8 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+
+
+remove_action('woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20);
+add_action('woocommerce_after_checkout_billing_form', 'woocommerce_checkout_payment', 20);
+
