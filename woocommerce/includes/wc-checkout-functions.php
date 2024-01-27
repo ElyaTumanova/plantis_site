1f226284de50d12eeece2c8d07ab1d88217fdfe4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-27 18:08:15 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -3,11 +3,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-// переместили блок с выбором способа оплаты
+// структура страницы
+
+// // переместили блок с выбором способа оплаты
 remove_action('woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20);
 add_action('woocommerce_checkout_shipping', 'woocommerce_checkout_payment', 20);
 
-// информация о пересадке в горшок
+// // информация о пересадке в горшок
 add_action('woocommerce_checkout_shipping', 'plnt_checkout_peresadka_info', 15);
 
 function plnt_checkout_peresadka_info(){
@@ -16,8 +18,7 @@ function plnt_checkout_peresadka_info(){
 	<?php
 }
 
-
-// информация об условиях доставки
+// // информация об условиях доставки
 add_action( 'woocommerce_checkout_order_review', 'plnt_delivery_condition_info', 30 );
 
 function plnt_delivery_condition_info () {
@@ -27,7 +28,7 @@ function plnt_delivery_condition_info () {
 		Важно! Срочную доставку "день в день" можно оформить до 18 часов.</div>';
 }
 
-// до бесплатной доставки осталось
+// // до бесплатной доставки осталось
 add_action( 'woocommerce_checkout_order_review', 'my_delivery_small_oder_info', 20 );
 
 function my_delivery_small_oder_info () {
@@ -42,4 +43,22 @@ function my_delivery_small_oder_info () {
 			До бесплатной доставки внутри МКАД осталось <span>'.$cart,'</span> рублей!</div>';
 		}
 	}	
+}
+
+// доп функции
+
+/* Уведомление об ошибке в оформлении заказа */
+add_action( 'woocommerce_after_checkout_validation', 'checkout_validation_unique_error', 9999, 2 );
+function checkout_validation_unique_error( $data, $errors ){
+    // Check for any validation errors
+    if( ! empty( $errors->get_error_codes() ) ) {
+
+        // Remove all validation errors
+        foreach( $errors->get_error_codes() as $code ) {
+            $errors->remove( $code );
+        }
+
+        // Add a unique custom one
+        $errors->add( 'validation', 'Упс! Не все обязательные поля были заполнены' );
+    }
 }
