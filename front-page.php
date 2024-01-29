ce68d575b6cfbf18a6f28148608494ebd2af0b9c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-29 12:26:32 +0300

    main mob

--- front-page.php
+++ front-page.php
@@ -27,15 +27,15 @@ get_header(); ?>
 								responsiveBreakpoints: {
 									portrait: {
 										changePoint:480,
-										visibleItems: 1
+										visibleItems: 2
 									},
 									landscape: {
 										changePoint:640,
-										visibleItems:1
+										visibleItems:2
 									},
 									tablet: {
 										changePoint:768,
-										visibleItems: 1
+										visibleItems: 2
 									}
 								}
 							});
