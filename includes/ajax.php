6672d68e8652d56698a45c6d9c1dc5f86ae92117
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-31 18:21:45 +0300

    search

--- includes/ajax.php
+++ includes/ajax.php
@@ -56,7 +56,7 @@ function plnt_search_ajax_action_callback (){
             <?php
         }
         ?>
-        <a class="search-result__btn button" href="<?php echo site_url()?>/?s=<?php get_query_var( 's' )?>">Посмотреть все</a>
+        <a class="search-result__btn button" href="<?php echo site_url()?>/?s=<?php echo get_query_var( 's' )?>">Посмотреть все</a>
         <?php
     } else {
         ?>
