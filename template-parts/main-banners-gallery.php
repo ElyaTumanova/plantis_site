aea4e11516140c8934aae48d6ef934ecf3aecc78
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-01-31 12:39:07 +0300

    catalog

--- template-parts/main-banners-gallery.php
+++ template-parts/main-banners-gallery.php
@@ -6,24 +6,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php 
 	$banner_1 = carbon_get_theme_option('main_banner_1');
+	$banner_1_mob = carbon_get_theme_option('main_banner_1_mob');
 	$banner_1_name = carbon_get_theme_option('main_banner_1_name');
 	$banner_2 = carbon_get_theme_option('main_banner_2');
+	$banner_2_mob = carbon_get_theme_option('main_banner_2_mob');
 	$banner_2_name = carbon_get_theme_option('main_banner_2_name');
 	$banner_3 = carbon_get_theme_option('main_banner_3');
+	$banner_3_mob = carbon_get_theme_option('main_banner_3_mob');
 	$banner_3_name = carbon_get_theme_option('main_banner_3_name');
 	$banner_4 = carbon_get_theme_option('main_banner_4');
+	$banner_4_mob = carbon_get_theme_option('main_banner_4_mob');
 	$banner_4_name = carbon_get_theme_option('main_banner_4_name');
-
-
-		// выводим переменные с размерами
-		echo "Ширина экрана: ". $_GET['width'] ."<br />\n";
-		echo "Высота экрана: ". $_GET['height'] ."<br />\n";
-
 ?>
 
 <div class="main__banners nivo-main-banner-gallery">
     <img src="<?php echo $banner_1 ?>" class="main__banner-img" alt="<?php echo $banner_1_name?>">
     <img src="<?php echo $banner_2 ?>" class="main__banner-img" style="visibility:hidden;" alt="<?php echo $banner_2_name?>">
+</div>
+
+<div class="main__banners_mob nivo-main-banner-gallery">
+    <img src="<?php echo $banner_1_mob ?>" class="main__banner-img" alt="<?php echo $banner_1_name?>">
+    <img src="<?php echo $banner_2_mob ?>" class="main__banner-img" style="visibility:hidden;" alt="<?php echo $banner_2_name?>">
 </div>
 
 <script>
