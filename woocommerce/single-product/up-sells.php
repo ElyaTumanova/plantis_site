2a58f4d1896c5b38f45f7a5a3e175c70d84f8199
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-04 18:40:24 +0300

    card mob

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -50,34 +50,35 @@ if ( $upsells ) : ?>
 
         </ul>
         
-        <?php echo count($upsells)  ?>
-        <script type="text/javascript">
-            jQuery(window).load(function() {
-                jQuery("#flexisel-upsells").flexisel({
-                    visibleItems:3,
-                    columnGaps: 30,
-                    animationSpeed: 1000,
-                    autoPlay: false,
-                    autoPlaySpeed: 3000,
-                    pauseOnHover: true,
-                    enableResponsiveBreakpoints: true,
-                    responsiveBreakpoints: {
-                        portrait: {
-                            changePoint:480,
-                            visibleItems: 1
-                        },
-                        landscape: {
-                            changePoint:640,
-                            visibleItems:2
-                        },
-                        tablet: {
-                            changePoint:768,
-                            visibleItems: 3
+        <?php if (count($upsells) >3) {  ?>
+            <script type="text/javascript">
+                jQuery(window).load(function() {
+                    jQuery("#flexisel-upsells").flexisel({
+                        visibleItems:3,
+                        columnGaps: 30,
+                        animationSpeed: 1000,
+                        autoPlay: false,
+                        autoPlaySpeed: 3000,
+                        pauseOnHover: true,
+                        enableResponsiveBreakpoints: true,
+                        responsiveBreakpoints: {
+                            portrait: {
+                                changePoint:480,
+                                visibleItems: 1
+                            },
+                            landscape: {
+                                changePoint:640,
+                                visibleItems:2
+                            },
+                            tablet: {
+                                changePoint:768,
+                                visibleItems: 3
+                            }
                         }
-                    }
+                    });
                 });
-            });
-		</script>
+            </script>
+        <?php }
 
         </div>
 
