ca110dccda894f8140bc108af5ed37c6965fd44b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-06 18:49:49 +0300

    pages

--- page-delivery.php
+++ page-delivery.php
@@ -7,7 +7,7 @@ get_header(); ?>
             <div class="delivery__wrap">
                 <h1 class="entry-header">Доставка</h1>
 
-                <h3 class="delivery__heading">Если ваш заказ <b>от 2000</b> рублей:</h3>
+                <h3 class="delivery__heading heading-2">Если ваш заказ <b>от 2000</b> рублей:</h3>
                 <div class="delivery__text">
                     <p class="delivery__subheading"><strong>Доставка на следующий день или позже:</strong></p>
                         <ul>
@@ -21,7 +21,7 @@ get_header(); ?>
                     </ul>				
                 </div>
                 
-                <h3 class="delivery__heading">Если ваш заказ <b>до 2000</b> рублей:</h3>
+                <h3 class="delivery__heading heading-2">Если ваш заказ <b>до 2000</b> рублей:</h3>
                 <div class="delivery__text">
 				    <p class="delivery__subheading"><strong>Доставка на следующий день или позже:</strong></p>
                     <ul>
@@ -33,12 +33,12 @@ get_header(); ?>
                         <li>в пределах МКАД — 990 рублей;</li>
                         <li>за пределы МКАД (до 5 км) — 1190 рублей.</li>
                     </ul>
-                    <div class="delivery__note">В итоговой стоимости заказа не учитывается цена доставки!</div>
-                    <div>Мы работаем без выходных, поэтому <strong>доставка осуществляется каждый день.</strong></div>
-                    <div>При заказе товаров на сумму <strong>свыше 15 000 рублей</strong> доставка осуществляется бесплатно.</div>					
+                    <p class="delivery__note">В итоговой стоимости заказа не учитывается цена доставки!</p>
+                    <p>Мы работаем без выходных, поэтому <strong>доставка осуществляется каждый день.</strong></p>
+                    <p>При заказе товаров на сумму <strong>свыше 15 000 рублей</strong> доставка осуществляется бесплатно.</p>					
                 </div>
 
-                <h3 class="delivery__heading">Интервалы доставки</h3>
+                <h3 class="delivery__heading heading-2">Интервалы доставки</h3>
                 <div class="delivery__text">
                     <ul>
                         <li>с 11:00 до 21:00;</li>
@@ -49,8 +49,8 @@ get_header(); ?>
                     <p>При оформлении срочной доставки “день в день” менеджер согласует с вами удобный интервал доставки.</p>					
                 </div>
 
-                <h3 class="delivery__heading">Связаться с нами</h3>
-                <div class="delivery__text">Ничего страшного, если вы не можете принять заказ в согласованные дату и время. В таком случае просим связаться с нами удобным для вас способом.</div>
+                <h3 class="delivery__heading heading-2">Связаться с нами</h3>
+                <p>Ничего страшного, если вы не можете принять заказ в согласованные дату и время. В таком случае просим связаться с нами удобным для вас способом.</p>
 
                 <?php get_template_part('template-parts/contacts_part');?>
 
@@ -58,18 +58,15 @@ get_header(); ?>
             <div class="delivery__wrap">
                 <h2 class="entry-header">Самовывоз</h2>
                 
-                <h3 class="delivery__heading">Где</h3>
-                <div class="delivery__text">Вы можете бесплатно забрать товары из нашего шоурума по адресу г. Москва, ул. Мещерякова, д.3 (от м. Тушинская или м. Сокол).</div>
+                <h3 class="delivery__heading heading-2">Где</h3>
+                <p>Вы можете бесплатно забрать товары из нашего шоурума по адресу г. Москва, ул. Мещерякова, д.3 (от м. Тушинская или м. Сокол).</p>
                 <div class="delivery__map">
 					<iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=237252555639" width="560" height="400" frameborder="0"></iframe>
 				</div>
 
-                <h3 class="delivery__heading">Когда</h3>
-                <div class="delivery__text">
+                <h3 class="delivery__heading heading-2">Когда</h3>
                     <p>Мы ждем Вас ежедневно в рабочие часы.</p>
                     <p>Необходимо предварительно связаться с нами и договорится о времени Вашего прибытия.</p>
-                </div>
-
             </div>
 
             					
