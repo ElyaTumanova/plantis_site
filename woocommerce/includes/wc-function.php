3ae8c6d98b260b5eefb71dcaea1b8d661b662835
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-06 18:25:31 +0300

    pages

--- woocommerce/includes/wc-function.php
+++ woocommerce/includes/wc-function.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! function_exists( 'ast_breadcrumbs_yoast' ) ) {
 	add_action( 'woocommerce_before_main_content', 'ast_breadrumbs_yoast', 10 );
 	function ast_breadrumbs_yoast() {
-		if ( !is_product() || !is_product_category() || !is_shop() ||!is_product_tag() ) {
+		if ( !is_product() && !is_product_category() && !is_shop() && !is_product_tag() ) {
 			if ( function_exists( 'yoast_breadcrumb' ) ) {
 				$before      = '<div id="breadcrumbs">';
 				$after       = '</div>';
