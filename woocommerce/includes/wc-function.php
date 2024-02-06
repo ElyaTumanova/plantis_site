399d3b890af3a4e8f9133717fb3e7db0a2f89561
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-06 18:20:56 +0300

    pages

--- woocommerce/includes/wc-function.php
+++ woocommerce/includes/wc-function.php
@@ -3,20 +3,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 //Хлебные крошки
-// if ( ! function_exists( 'ast_breadrumbs_yoast' ) ) {
-// 	add_action( 'woocommerce_before_main_content', 'ast_breadrumbs_yoast', 10 );
-// 	function ast_breadrumbs_yoast() {
-// 		if ( is_product() || is_product_category() || is_shop() ) {
-// 			if ( function_exists( 'yoast_breadcrumb' ) ) {
-// 				$before      = '<div id="breadcrumbs">';
-// 				$after       = '</div>';
-// 				$breadcrumbs = yoast_breadcrumb( $before, $after, true );
+if ( ! function_exists( 'ast_breadrumbs_yoast' ) ) {
+	add_action( 'woocommerce_before_main_content', 'ast_breadrumbs_yoast', 10 );
+	function ast_breadrumbs_yoast() {
+		if ( is_product() || is_product_category() || is_shop() ) {
+			if ( function_exists( 'yoast_breadcrumb' ) ) {
+				$before      = '<div id="breadcrumbs">';
+				$after       = '</div>';
+				$breadcrumbs = yoast_breadcrumb( $before, $after, true );
 				
-// 				return $breadcrumbs;
-// 			}
-// 		}
-// 	}
-// }
+				return $breadcrumbs;
+			}
+		}
+	}
+}
 
 //lazy load for images
 // function plnt_add_lazy_class ($attr) {
