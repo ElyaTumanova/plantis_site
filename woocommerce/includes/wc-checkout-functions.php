ccd8f87f1d20f6a9bab00b87468425c63c2d55b9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-07 12:42:28 +0300

    pages

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -35,8 +35,8 @@ function my_delivery_small_oder_info () {
     $min_free_delivery = carbon_get_theme_option('min_free_delivery');
     $min_small_delivery = carbon_get_theme_option('min_small_delivery');
     echo str_replace(" ","",$min_free_delivery);
-	if ( WC()->cart->subtotal < str_replace(" ","",$min_free_delivery)) {
-		$cart = str_replace(" ","",$min_free_delivery) - WC()->cart->subtotal;
+	if ( WC()->cart->subtotal < str_replace(" ","",$min_small_delivery)) {
+		$cart = str_replace(" ","",$min_small_delivery) - WC()->cart->subtotal;
 		echo '<div class="checkout__free-delivery-text">
         Добавьте товаров на <span>'.$cart,'</span> рублей, чтобы стоимость доставки уменьшилась!</div>';
 	} else {
