7da40a0cb231ed8101b89ca4ccbe887d42ef3a98
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-07 12:17:33 +0300

    pages

--- template-parts/delivery-info.php
+++ template-parts/delivery-info.php
@@ -4,6 +4,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
+<?php 
+    $in_mkad = carbon_get_theme_option('in_mkad');
+    $out_mkad = carbon_get_theme_option('out_mkad');
+    $in_mkad_urg = carbon_get_theme_option('in_mkad_urg');
+    $out_mkad_urg = carbon_get_theme_option('out_mkad_urg');
+    $in_mkad_small = carbon_get_theme_option('in_mkad_small');
+    $out_mkad_small = carbon_get_theme_option('out_mkad_small');
+    $in_mkad_small_urg = carbon_get_theme_option('in_mkad_small_urg');
+    $out_mkad_small_urg = carbon_get_theme_option('out_mkad_small_urg');
+    $min_free_delivery = carbon_get_theme_option('min_free_delivery');
+    $min_small_delivery = carbon_get_theme_option('min_small_delivery');
+?>
+
 <div class="delivery-info">
     <h4 class="delivery_table__header">Доставка</h4>
     <div class="delivery_table">
@@ -13,24 +26,24 @@ if ( ! defined( 'ABSPATH' ) ) {
             <div>бесплатно</div>
         </div>
         <div class="delivery_table__row">
-            <div>При заказе от 15000₽</div>
+            <div>При заказе от <?php echo $min_free_delivery?>₽</div>
             <div>бесплатно</div>
         </div>
         <div class="delivery_table__row">
             <div>В пределах МКАД</div>
-            <div>590₽</div>
+            <div><?php echo $in_mkad?>₽</div>
         </div>
         <div class="delivery_table__row">
             <div>За пределы МКАД (до 5км)</div>
-            <div>790₽</div>
+            <div><?php echo $out_mkad?>₽</div>
         </div>
         <div class="delivery_table__row">
             <div>В пределах МКАД в день заказа</div>
-            <div>790₽</div>
+            <div><?php echo $in_mkad_urg?>₽</div>
         </div>
         <div class="delivery_table__row">
             <div>За пределы МКАД (до 5км) в день заказа</div>
-            <div>990₽</div>
+            <div><?php echo $out_mkad_urg?>₽</div>
         </div>
         <div class="delivery_table__row">
             <div>За пределы МКАД (от 5км)</div>
