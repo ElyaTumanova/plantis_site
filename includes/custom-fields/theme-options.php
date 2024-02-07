cc39b11801a225d50e0203e701249f6d64c58f3e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-07 11:57:36 +0300

    pages

--- includes/custom-fields/theme-options.php
+++ includes/custom-fields/theme-options.php
@@ -53,7 +53,18 @@ Container::make('theme_options', 'Настройки темы')
 			 ->set_value_type( 'url' ),
 			Field::make( 'text', 'main_banner_4_name', 'Banner 4 Name' ),
 		))
-		 ;
+		->add_tab( __('Delivery'), array(
+			Field::make( 'text', 'inMKAD', 'Доставка в пределах МКАД на след день более мин суммы'),
+			Field::make( 'text', 'outMKAD', 'Доставка за пределы МКАД на след день более мин суммы'),
+			Field::make( 'text', 'inMKAD_urg', 'Доставка в пределах МКАД срочная более мин суммы'),
+			Field::make( 'text', 'outMKAD_urg', 'Доставка за пределы МКАД срочная более мин суммы'),
+			Field::make( 'text', 'inMKAD_small', 'Доставка в пределах МКАД на след день до мин суммы'),
+			Field::make( 'text', 'outMKAD_small', 'Доставка за пределы МКАД на след день до мин суммы'),
+			Field::make( 'text', 'inMKAD_small_urg', 'Доставка в пределах МКАД срочная до мин суммы'),
+			Field::make( 'text', 'outMKAD_small_urg', 'Доставка за пределы МКАД срочная до мин суммы'),
+			Field::make( 'text', 'minFreeDelivery', 'Минимальная сумма заказа для бесплатной доставки'),
+			Field::make( 'text', 'minSmallDelivery', 'Сумма заказа для более дорогой доставки'),
+	   ));
 
 // Add second options page under 'Basic Options'
 Container::make('theme_options', 'Social Links')
