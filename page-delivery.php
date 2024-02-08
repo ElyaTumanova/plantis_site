facb14ef11614ca4f3cb1c12cb96388d13c10449
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-08 20:37:43 +0300

    account

--- page-delivery.php
+++ page-delivery.php
@@ -17,42 +17,42 @@ get_header(); ?>
 <div class="content-area">
 	<main id="main" class="site-main" role="main">
         <div class="delivery">
-            <div class="delivery__wrap">
+            <div class="info__wrap">
                 <h1 class="entry-header">Доставка</h1>
 
-                <h3 class="delivery__heading heading-2">Если ваш заказ <b>от <?php echo $min_small_delivery ?></b> рублей:</h3>
-                <div class="delivery__text">
-                    <p class="delivery__subheading"><strong>Доставка на следующий день или позже:</strong></p>
+                <h3 class="info__heading heading-2">Если ваш заказ <b>от <?php echo $min_small_delivery ?></b> рублей:</h3>
+                <div class="info__text">
+                    <p class="info__subheading"><strong>Доставка на следующий день или позже:</strong></p>
                         <ul>
                             <li>в пределах МКАД — <?php echo $in_mkad ?> рублей;</li>
                             <li>за пределы МКАД (до 5 км) — <?php echo $out_mkad ?> рублей.</li>
                         </ul>
-                    <p class="delivery__subheading"><strong>Срочная “день в день”</strong>. Можно оформить до 18:00:</p>
+                    <p class="info__subheading"><strong>Срочная “день в день”</strong>. Можно оформить до 18:00:</p>
                     <ul>
                         <li>в пределах МКАД — <?php echo $in_mkad_urg ?> рублей;</li>
                         <li>за пределы МКАД (до 5 км) — <?php echo $out_mkad_urg ?> рублей.</li>
                     </ul>				
                 </div>
                 
-                <h3 class="delivery__heading heading-2">Если ваш заказ <b>до <?php echo $min_small_delivery ?></b> рублей:</h3>
-                <div class="delivery__text">
-				    <p class="delivery__subheading"><strong>Доставка на следующий день или позже:</strong></p>
+                <h3 class="info__heading heading-2">Если ваш заказ <b>до <?php echo $min_small_delivery ?></b> рублей:</h3>
+                <div class="info__text">
+				    <p class="info__subheading"><strong>Доставка на следующий день или позже:</strong></p>
                     <ul>
                         <li>в пределах МКАД — <?php echo $in_mkad_small ?> рублей;</li>
                         <li>за пределы МКАД (до 5 км) — <?php echo $out_mkad_small ?> рублей.</li>
                     </ul>
-                    <p class="delivery__subheading"><strong>Срочная “день в день”</strong>. Можно оформить до 18:00:</p>
+                    <p class="info__subheading"><strong>Срочная “день в день”</strong>. Можно оформить до 18:00:</p>
                     <ul>
                         <li>в пределах МКАД — <?php echo $in_mkad_small_urg ?> рублей;</li>
                         <li>за пределы МКАД (до 5 км) — <?php echo $out_mkad_small_urg ?> рублей.</li>
                     </ul>
-                    <p class="delivery__note">В итоговой стоимости заказа не учитывается цена доставки!</p>
+                    <p class="info__note">В итоговой стоимости заказа не учитывается цена доставки!</p>
                     <p>Мы работаем без выходных, поэтому <strong>доставка осуществляется каждый день.</strong></p>
                     <p>При заказе товаров на сумму <strong>свыше <?php echo $min_free_delivery ?> рублей</strong> доставка осуществляется бесплатно.</p>					
                 </div>
 
-                <h3 class="delivery__heading heading-2">Интервалы доставки</h3>
-                <div class="delivery__text">
+                <h3 class="info__heading heading-2">Интервалы доставки</h3>
+                <div class="info__text">
                     <ul>
                         <li>с 11:00 до 21:00;</li>
                         <li>с 11:00 до 16:00;</li>
@@ -62,22 +62,22 @@ get_header(); ?>
                     <p>При оформлении срочной доставки “день в день” менеджер согласует с вами удобный интервал доставки.</p>					
                 </div>
 
-                <h3 class="delivery__heading heading-2">Связаться с нами</h3>
+                <h3 class="info__heading heading-2">Связаться с нами</h3>
                 <p>Ничего страшного, если вы не можете принять заказ в согласованные дату и время. В таком случае просим связаться с нами удобным для вас способом.</p>
 
                 <?php get_template_part('template-parts/contacts-part');?>
 
             </div>
-            <div class="delivery__wrap">
+            <div class="info__wrap">
                 <h2 class="entry-header">Самовывоз</h2>
                 
-                <h3 class="delivery__heading heading-2">Где</h3>
+                <h3 class="info__heading heading-2">Где</h3>
                 <p>Вы можете бесплатно забрать товары из нашего шоурума по адресу г. Москва, ул. Мещерякова, д.3 (от м. Тушинская или м. Сокол).</p>
-                <div class="delivery__map">
+                <div class="info__map">
 					<iframe src="https://yandex.ru/map-widget/v1/?z=12&ol=biz&oid=237252555639" width="560" height="400" frameborder="0"></iframe>
 				</div>
 
-                <h3 class="delivery__heading heading-2">Когда</h3>
+                <h3 class="info__heading heading-2">Когда</h3>
                     <p>Мы ждем Вас ежедневно в рабочие часы.</p>
                     <p>Необходимо предварительно связаться с нами и договорится о времени Вашего прибытия.</p>
             </div>				
