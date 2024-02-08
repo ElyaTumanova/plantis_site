78a789d564ad57c76626f1bb6bf78ae1ffb65bbb
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-08 19:31:54 +0300

    account

--- page-partners.php
+++ page-partners.php
@@ -7,14 +7,12 @@ get_header(); ?>
 			<h1 class="entry-title">Поставщикам и партнерам</h1>                
 		</header>
 		<div class="entry-content">
-        <div class="elementor-text-editor elementor-clearfix">
 			<p>Уважаемые поставщики и партнёры!</p>
             <p>Мы всегда готовы рассмотреть ваши предложения по сотрудничеству с нашей компанией.</p>
             <p>Для этого свяжитесь с нами удобным способом.<br>Мы не оставим вас без ответа.</p>					
-        </div>
 
             <?php get_template_part('template-parts/contacts-part');?>
-            
+        
 		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
