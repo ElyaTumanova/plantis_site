e6f9f148f27a389bab2dd7dd21eff96c08c7ce68
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-10 14:15:09 +0300

    pages

--- template-parts/popups/pricelist-popup.php
+++ template-parts/popups/pricelist-popup.php
@@ -3,15 +3,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 ?>
-<div class="pricelist-popup popup">
-    <div class="pricelist container">
-        <div class="pricelist__wrap">
+<div class="preorder-popup popup">
+    <div class="preorder container">
+        <div class="preorder__wrap">
             <h2 class="preopder__heading heading-2">Предзаказ товара</h2>
-            <span class="pricelist__close heading-2">&#10006;</span>
-            <div class="pricelist__form">ТУТ БУДЕТ ФОРМА</div>
-            <p class="pricelist__text">Нажимая кнопку "Отправить", вы даете согласие на обработку своих персональных данных и соглашаетесь с положениями, 
-                описанными в нашей <a class="pricelist__link" target="blank" href="<?php get_site_url()?>/privacy-policy/">политике конфиденциальности</a>.</p>
+            <span class="preorder__close heading-2">&#10006;</span>
+            <div class="preorder__form">ТУТ БУДЕТ ФОРМА</div>
+            <p class="preorder__text">Нажимая кнопку "Отправить", вы даете согласие на обработку своих персональных данных и соглашаетесь с положениями, 
+                описанными в нашей <a class="preorder__link" target="blank" href="<?php get_site_url()?>/privacy-policy/">политике конфиденциальности</a>.</p>
         </div>
     </div>
-    <div class="pricelist__popup-overlay popup-overlay"></div>
+    <div class="preorder__popup-overlay popup-overlay"></div>
 </div>	
