7b665dd711570acfa39085efbd1a92af0193b80b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-11 10:59:39 +0300

    metrika

---
+++ assets/js/metrikaGoal.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function() {
+	// jQuery('.ajax_add_to_cart').click(function(){
+	// 	//ym(87781741, 'reachGoal', 'click-button-cart-listing'); return true;
+	// });
+	jQuery('.card__price-wrap .add_to_cart_button').click(function(){
+		ym(87781741, 'reachGoal', 'click-button-cart-detalka-new'); return true;
+	});
+	// jQuery('.wc-proceed-to-checkout .checkout-button').click(function(){
+	// 	ym(87781741, 'reachGoal', 'click-button-order'); return true;
+	// });
+	// jQuery('header .jet-blocks-cart__heading-link').click(function(){
+	// 	ym(87781741, 'reachGoal', 'click-button-cart'); return true;
+	// });
+
+	// jQuery('.jet-wishlist-button__state').click(function(){
+	// 	ym(87781741, 'reachGoal', 'click-button-favorites'); return true;
+	// });
+});
+
+// document.addEventListener( 'wpcf7mailsent', function( event ) {
+// 	if ( '27633' == event.detail.contactFormId ) {
+// 		ym(87781741, 'reachGoal', 'form-predzakaz');
+// 	}
+// }, false );
--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -43,6 +43,9 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 		wp_enqueue_script( 'catalog-menu', get_template_directory_uri() .
 		                                     '/assets/js/catalog-menu.js', array( 'jquery' ), null, true );
 
+		wp_enqueue_script( 'metrikaGoal', get_template_directory_uri() .
+		                                     '/assets/js/metrikaGoal.js', array( 'jquery' ), null, true );  //metrikaGoal Яндекс Метрика Yandex Metrika
+
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
