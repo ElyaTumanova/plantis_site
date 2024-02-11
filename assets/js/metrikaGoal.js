afba2123aaa3457ff4477278b0818323d2c368d4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-11 13:04:37 +0300

    metrika

--- assets/js/metrikaGoal.js
+++ assets/js/metrikaGoal.js
@@ -11,22 +11,22 @@ document.addEventListener("DOMContentLoaded", function() {
         const productName = document.querySelector ('.card__grid .product_title').innerHTML;
         console.log(productName);
         ym(87781741, 'reachGoal', 'click-button-cart-detalka-new');
-        // window.dataLayer.push(
-        //     {
-        //         "ecommerce": {
-        //             "currencyCode": "RUB",
-        //             "add": {
-        //                 "products" : [
-        //                     {
-        //                         "name":,
-        //                         "quantity":,
-        //                         "price":
-        //                     }
-        //                 ]
-        //             }
-        //         }
-        //     }
-        // )
+        window.dataLayer.push(
+            {
+                "ecommerce": {
+                    "currencyCode": "RUB",
+                    "add": {
+                        "products" : [
+                            {
+                                "name":productName,
+                                // "quantity":,
+                                // "price":
+                            }
+                        ]
+                    }
+                }
+            }
+        )
         console.log(JSON.stringify(window.dataLayer));
         
         return true; 
