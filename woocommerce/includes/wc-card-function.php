595f58d5deb142004aee3ca68db99dd6dbeb0cbc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-11 12:45:23 +0300

    metrika

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -355,7 +355,7 @@ function plnt_get_product () {
     global $product;
     ?> 
     <script>
-        console.log(<?php . $product . ?>)
+        console.log(<?php $product?>)
     </script>
     <?php
 }
