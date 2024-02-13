8d85c0958cbf375c27b7ea1ec22876dacaffffaf
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-13 17:55:40 +0300

    metrika

--- functions.php
+++ functions.php
@@ -27,6 +27,8 @@ require get_template_directory() . '/includes/navigation.php';
 require get_template_directory() . '/includes/ajax.php';
 /** Add constants */
 require get_template_directory() . '/includes/constants.php';
+/** Add Ynadex metrika */
+require get_template_directory() . '/includes/metrika.php';
 
 /** Add Woocommerce files */
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
---
+++ includes/metrika.php
@@ -0,0 +1,107 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+/*--------------------------------------------------------------
+# METRIKA E-COMMERCE 
+--------------------------------------------------------------*/
+add_action('woocommerce_after_main_content','plnt_metrika_card', 30);
+
+function plnt_metrika_card () {
+    global $product;
+    $productName = $product->get_title();
+    $price = $product->get_price();
+    $parentCatId = check_category();
+    $catName = get_the_category_by_ID($parentCatId);
+
+        if (is_product()) {
+
+            ?> 
+            <script>
+                console.log('hi product');
+                document.addEventListener("DOMContentLoaded", function() {
+                    window.dataLayer.push(
+                            {
+                                "ecommerce": {
+                                    "currencyCode": "RUB",
+                                    "detail": {
+                                        "products" : [
+                                            {
+                                                "name":'<?php echo $productName?>',
+                                                "price":'<?php echo $price?>',
+                                                "category":'<?php echo $catName?>'
+                                            }
+                                        ]
+                                    }
+                                }
+                            }
+                        )
+                        console.log(JSON.stringify(window.dataLayer));
+                        
+                        return true; 
+                })
+            </script>
+            <?php
+        }
+    }
+
+// добавляем доп атрибуты для кнопки Добавить в корзину для использования в Yandex Metrika e-commerce
+add_filter( 'woocommerce_loop_add_to_cart_args', 'filter_woocommerce_loop_add_to_cart_args', 10, 2 );
+function filter_woocommerce_loop_add_to_cart_args( $args, $product ) {
+    $productName = $product->get_title();
+    $price = $product->get_price();
+    $parentCatId = check_category();
+    $catName = get_the_category_by_ID($parentCatId);
+    $args['attributes']['data-product-name'] = $productName;
+    $args['attributes']['data-product-price'] = $price;
+    $args['attributes']['data-category-name'] = $catName;
+
+    return $args;
+}
+
+
+add_action('woocommerce_before_cart', 'plnt_metrika_cart_remove', 30);
+
+function plnt_metrika_cart_remove () {
+    // global $product;
+    // $productName = $product->get_title();
+    // $price = $product->get_price();
+    // $parentCatId = check_category();
+    // $catName = get_the_category_by_ID($parentCatId);
+
+    if ( ! WC()->cart->is_empty() ) {
+            ?> 
+            <script>
+                console.log('hi cart');
+                document.addEventListener("DOMContentLoaded", function() {
+                    $removes = document.querySelectorAll('a .remove');
+                    console.log($removes);
+
+                    // window.dataLayer.push(
+                    //     {
+                    //         "ecommerce": {
+                    //             "currencyCode": "RUB",
+                    //             "detail": {
+                    //                 "products" : [
+                    //                     {
+                    //                         "name":'<?php echo $productName?>',
+                    //                         "price":'<?php echo $price?>',
+                    //                         "category":'<?php echo $catName?>'
+                    //                     }
+                    //                 ]
+                    //             }
+                    //         }
+                    //     }
+                    // )
+                    // console.log(JSON.stringify(window.dataLayer));
+                    
+                    // return true; 
+                })
+            </script>
+            <?php
+
+    }
+
+        
+    }
