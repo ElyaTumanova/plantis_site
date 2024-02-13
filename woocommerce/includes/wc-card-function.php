0873aefde55ca86a7d47fdaecf1891d6353bbdab
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-13 13:28:48 +0300

    metrika

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -481,8 +481,8 @@ function plnt_metrika_card () {
 
 
 
-add_action('wp_ajax_woocommerce_add_to_cart', 'plnt_woocommerce_add_to_cart_action_callback');
-add_action('wp_ajax_nopriv_woocommerce_add_to_cart', 'plnt_woocommerce_add_to_cart_action_callback');
+add_action('wc_ajax_add_to_cart', 'plnt_woocommerce_add_to_cart_action_callback');
+//add_action('wp_ajax_nopriv_woocommerce_add_to_cart', 'plnt_woocommerce_add_to_cart_action_callback');
 
 function plnt_woocommerce_add_to_cart_action_callback (){
     $prodId = $_POST['product_id'];
