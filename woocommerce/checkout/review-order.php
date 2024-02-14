ccb363866076d058f384ae5858ffc6a6b7587b55
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-14 22:35:25 +0300

    checkout metrika

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -40,7 +40,7 @@ defined( 'ABSPATH' ) || exit;
 				data-product_name="<?php echo $_product->get_name()?>" 
 				data-product_category="<?php echo get_the_category_by_ID($parentCatId)?>" 
 				data-product_quantity="<?php echo $cart_item['quantity']?>"
-				data-product_price="<?php echo WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] )?>"
+				data-product_price="<?php echo $cart_item?>"
 				>
 					<td class="product-name">
 						<?php echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) ) . '&nbsp;'; ?>
