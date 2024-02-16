a39f0ebb323908357420d12f9d8c686ae2141df5
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-16 18:54:57 +0300

    adaptive

--- includes/ajax.php
+++ includes/ajax.php
@@ -26,6 +26,9 @@ function plnt_search_ajax_action_callback (){
     $query_ajax = new WP_Query($arg);
     $json_data['out'] = ob_start(PHP_OUTPUT_HANDLER_CLEANABLE);
     if ($query_ajax->have_posts()) {
+        echo '<pre>';
+        print_r( $query_ajax );
+        echo '</pre>';
         while ($query_ajax->have_posts()){
             $query_ajax->the_post();
             $product = wc_get_product( get_the_ID() );
@@ -33,7 +36,6 @@ function plnt_search_ajax_action_callback (){
             $short_descr = $product->get_short_description();
             $sale = get_post_meta( get_the_ID(), '_sale_price', true);
             ?>
-            <script>console.log(<?php echo $query_ajax;?>)</script>
             <div class="search-result__item">
                 <a href="<?php echo get_permalink();?>" class="search-result__link" target="blank">
                     <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );?>" class="search-result__image" alt="<?php echo get_the_title();?>">
