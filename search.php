134660cea4a07f2938bfea4a9b00ea051abd24b7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-16 19:10:01 +0300

    adaptive

--- search.php
+++ search.php
@@ -62,7 +62,7 @@ get_header(); ?>
                     // the_posts_pagination();
 
                     the_posts_pagination( array(
-                        'class' => 'woocommerce-pagination',
+                        // 'class' => 'woocommerce-pagination',
                         'prev_text'    => __('←'),
 	                    'next_text'    => __('→'),
                     ));
