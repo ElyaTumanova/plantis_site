3c2fed675154579dc45f7dc9558c44a3902254d8
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-17 17:11:47 +0300

    adaptive

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -87,10 +87,10 @@ function plnt_catalog_filters_main_area() {
 		<div class="catalog__filter-metki">
 			<?php echo do_shortcode('[br_filter_single filter_id=10989]') ?>  
 		</div>
-		<div class="catalog__filter-active">
-			<?php echo do_shortcode('[br_filter_single filter_id=6057]') ?>
-		</div>
     </div>
+	<div class="catalog__filter-active">
+		<?php echo do_shortcode('[br_filter_single filter_id=6057]') ?>
+	</div>
     <?php 	
 };
 
