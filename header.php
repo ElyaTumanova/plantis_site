9e150e3bddacc594175456067714e7419d677b4f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-17 13:51:23 +0300

    adaptive

--- header.php
+++ header.php
@@ -7,28 +7,25 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!-- Yandex.Metrika counter -->
 		<script type="text/javascript" >
-		(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-		m[i].l=1*new Date();
-		for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-		k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+			(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+			m[i].l=1*new Date();
+			for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+			k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+			(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-		ym(96498108, "init", {
-				clickmap:true,
-				trackLinks:true,
-				accurateTrackBounce:true,
-				webvisor:true,
-				ecommerce:"dataLayer"
-		});
-
-		window.dataLayer = window.dataLayer || [];
+			ym(96498108, "init", {
+					clickmap:true,
+					trackLinks:true,
+					accurateTrackBounce:true,
+					ecommerce:"dataLayer"
+			});
 		</script>
 	<!-- /Yandex.Metrika counter -->
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<noscript><div><img src="https://mc.yandex.ru/watch/87781741" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/96498108" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <div id="page" class="site">
 
 	<header id="header" class="header" role="banner">
