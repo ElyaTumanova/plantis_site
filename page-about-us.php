fa1d04a3138b463513165068116cc5e369d7c4ea
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-18 13:24:35 +0300

    adaptive

--- page-about-us.php
+++ page-about-us.php
@@ -104,7 +104,7 @@ get_header(); ?>
                 <script>
                     jQuery(function($){
                         $('.nivo-about-feedback').nivoSlider({
-                            effect: 'fade',               // эффекты, например: 'fold, fade, sliceDown, sliceDownLeft, sliceUp, sliceUpLeft, sliceUpDown, sliceUpDownLeft, slideInRight, slideInLeft'
+                            effect: 'slideInRight',               // эффекты, например: 'fold, fade, sliceDown, sliceDownLeft, sliceUp, sliceUpLeft, sliceUpDown, sliceUpDownLeft, slideInRight, slideInLeft'
                             animSpeed: 100,                 // скорость анимации
                             pauseTime: 3000,                // пауза между сменой слайдов
                             directionNav: true,             // нужно ли отображать кнопки перехода на следующий и предыдущий слайд
