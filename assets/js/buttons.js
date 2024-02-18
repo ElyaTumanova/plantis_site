d2ec75c8e6f05b4a6bb57fe596c0a043e8048701
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-18 18:26:45 +0300

    adaptive

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -87,26 +87,3 @@ if (filtersMob) {
         contentArea.classList.toggle ('no-padding');
     };
 }
-
-
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    // // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
-});
--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -26,7 +26,10 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 		                                     '/assets/js/swiper.js', array( 'jquery' ), null, true ); //swiper
 
 		wp_enqueue_script( 'buttons', get_template_directory_uri() .
-											 '/assets/js/buttons.js', array( 'jquery', 'swiper' ), null, true );	
+											 '/assets/js/buttons.js', array( 'jquery' ), null, true );	
+											 
+		wp_enqueue_script( 'slider-init', get_template_directory_uri() .
+											 '/assets/js/slider-init.js', array( 'jquery', 'swiper' ), null, true );	
 
 		// wp_enqueue_script( 'lazy-load', get_template_directory_uri() .
 		//                                      '/assets/js/lazy-load.js', array( 'jquery' ), null, true ); // for lazy load
