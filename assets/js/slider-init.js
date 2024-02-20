daf8deaf1e12293e4ca10f2efa20984fb8d6776c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-20 16:47:51 +0300

    swiper

--- assets/js/slider-init.js
+++ assets/js/slider-init.js
@@ -1,3 +1,31 @@
+const swiper_about_photo = new Swiper('.about__swiper-photo', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 10,
+    // breakpoints: {
+    //     320: {
+    //     slidesPerView: 1,
+    //     slidesPerGroup: 1,
+    //     },
+    //     768: {
+    //     slidesPerView: 2,
+    //     slidesPerGroup: 2,
+    //     },
+    //     1023: {
+    //     slidesPerView: 1,
+    //     slidesPerGroup: 1,
+    //     }
+    // }
+});
+
 const swiper_about_feedback = new Swiper('.about__swiper-feedback', {
     pagination: {
         el: '.swiper-pagination',
@@ -22,34 +50,6 @@ const swiper_about_feedback = new Swiper('.about__swiper-feedback', {
         1023: {
         slidesPerView: 3,
         slidesPerGroup: 3,
-        }
-    }
-});
-
-const swiper_about_photo = new Swiper('.about__swiper-photo', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    spaceBetween: 10,
-    breakpoints: {
-        320: {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
-        },
-        768: {
-        slidesPerView: 2,
-        slidesPerGroup: 2,
-        },
-        1023: {
-        slidesPerView: 1,
-        slidesPerGroup: 1,
         }
     }
 });
