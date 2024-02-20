9a4ae579d13f308b8b12ae356137f5352e897407
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-20 16:03:12 +0300

    swiper

--- assets/js/slider-init.js
+++ assets/js/slider-init.js
@@ -1,21 +1,23 @@
 const swiper_about_us_mob = new Swiper('.swiper_about-us', {
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
-
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    slidesPerView: 3,
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+        slidesPerView: 1,
+        },
+        640: {
+        slidesPerView: 2,
+        },
+        768: {
+        slidesPerView: 3,
+        }
+    }
 
-    // // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
 });
