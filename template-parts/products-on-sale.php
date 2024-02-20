de3690033d9094e69088b5069e0391b7aca5f7d0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-20 22:53:28 +0300

    swiper

--- template-parts/products-on-sale.php
+++ template-parts/products-on-sale.php
@@ -2,68 +2,122 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-?>
 
-<?php
-global $wp_query;
 $product_ids_on_sale = wc_get_product_ids_on_sale();
-$arg = array(
-    'post_type' => 'product', // если нужен поиск по постам - доавляем в массив 'post'
-    'post_status' => 'publish',
-    'post__in' => array_merge( array( 0 ), $product_ids_on_sale ),
-    // 'meta_query'     => array(
-    //     'relation' => 'AND',
-    //     array( // Simple products type
-    //         'key'           => '_sale_price',
-    //         'value'         => 0,
-    //         'compare'       => '>',
-    //         'type'          => 'numeric'
-    //     ),
-    //     array(
-    //         'key'       => '_stock_status',
-    //         'value'     => 'outofstock',
-    //         'compare'   => 'NOT IN'
-    //     )),
-    // 'tax_query' => array(
-	// 	array(
-	// 		'taxonomy' => 'category',
-	// 		'field' => 'slug',
-	// 		'terms' => 'komnatnye-rasteniya'
-	// 	)
-    // ),
-    // 'posts_per_page' => -1,
-    'meta_query' => array( 
-        array(
-            'key'       => '_stock_status',
-            'value'     => 'outofstock',
-            'compare'   => 'NOT IN'
-        )
-    ),
-    'orderby' => 'rand',
-);
-$on_sale_query = new WP_Query($arg);
-$wp_query = $on_sale_query;
-// echo '<pre>';
-// print_r( $on_sale_query);
-// echo '</pre>';
+    echo '<pre>';
+	print_r( $var );
+	echo '</pre>';
+
+// if( !empty ($product_ids_on_sale) ){
+
+//     $crosssell_ids = $crosssell_ids[0];
+
+//     if(count($crosssell_ids)>0){
+
+//         $args = array(
+//             'post_type' => 'product',
+//             'ignore_sticky_posts' => 1,
+//             'no_found_rows' => 1,
+//             'posts_per_page' => 8,
+//             'orderby' => $rand,
+//             'post__in' => $crosssell_ids,
+//             'meta_query' => array( 
+//                 array(
+//                     'key'       => '_stock_status',
+//                     'value'     => 'outofstock',
+//                     'compare'   => 'NOT IN'
+//                 )
+//             )
+//         );
+
+//         $products = new WP_Query( $args );
+//         if ( $products->have_posts() ) : ?>
+
+//             <div class="cross-sells">
+
+//                 <h2 class="heading-2"><?php _e( 'Похожие растения', 'woocommerce' ) ?></h2>       
+//                 <div class="cross-upsells-swiper swiper">
+//                     <ul class="products columns-3 swiper-wrapper"> 
+//                         <?php while ( $products->have_posts() ) : $products->the_post(); ?>
+
+//                         <?php wc_get_template_part( 'content', 'product' ); ?>
+
+//                         <?php endwhile; // end of the loop. ?>
+//                     </ul>
+//                     <div class="swiper-pagination"></div>
+// 	                <div class="swiper-button-prev"></div>
+// 	                <div class="swiper-button-next"></div>
+//                 </div>
+//             </div>
+
+//         <?php endif;
+
+//     }
+
+//     wp_reset_query();
+
+// }
+
+// <?php
+// global $wp_query;
+// $product_ids_on_sale = wc_get_product_ids_on_sale();
+// $arg = array(
+//     'post_type' => 'product', // если нужен поиск по постам - доавляем в массив 'post'
+//     'post_status' => 'publish',
+//     'post__in' => array_merge( array( 0 ), $product_ids_on_sale ),
+//     // 'meta_query'     => array(
+//     //     'relation' => 'AND',
+//     //     array( // Simple products type
+//     //         'key'           => '_sale_price',
+//     //         'value'         => 0,
+//     //         'compare'       => '>',
+//     //         'type'          => 'numeric'
+//     //     ),
+//     //     array(
+//     //         'key'       => '_stock_status',
+//     //         'value'     => 'outofstock',
+//     //         'compare'   => 'NOT IN'
+//     //     )),
+//     // 'tax_query' => array(
+// 	// 	array(
+// 	// 		'taxonomy' => 'category',
+// 	// 		'field' => 'slug',
+// 	// 		'terms' => 'komnatnye-rasteniya'
+// 	// 	)
+//     // ),
+//     // 'posts_per_page' => -1,
+//     'meta_query' => array( 
+//         array(
+//             'key'       => '_stock_status',
+//             'value'     => 'outofstock',
+//             'compare'   => 'NOT IN'
+//         )
+//     ),
+//     'orderby' => 'rand',
+// );
+// $on_sale_query = new WP_Query($arg);
+// $wp_query = $on_sale_query;
+// // echo '<pre>';
+// // print_r( $on_sale_query);
+// // echo '</pre>';
 
 
-// if( $on_sale_query->have_posts() ) :
+// // if( $on_sale_query->have_posts() ) :
 
-    ?><ul class="products columns-2"><?php
-        // затем запускаем цикл
-        while( have_posts() ) : the_post();
-            // echo '<pre>';
-            // print_r( $on_sale_query->the_post() );
-            // echo '</pre>';
-            $product = wc_get_product( get_the_ID() );
-            if ($product->is_in_stock()) {
-                wc_get_template_part( 'content', 'product' );
-            } 
-        endwhile;
-    ?></ul><?php
-// endif;
+//     ?><ul class="products columns-2"><?php
+//         // затем запускаем цикл
+//         while( have_posts() ) : the_post();
+//             // echo '<pre>';
+//             // print_r( $on_sale_query->the_post() );
+//             // echo '</pre>';
+//             $product = wc_get_product( get_the_ID() );
+//             if ($product->is_in_stock()) {
+//                 wc_get_template_part( 'content', 'product' );
+//             } 
+//         endwhile;
+//     ?></ul><?php
+// // endif;
 
-// восстанавливаем глобальную переменную $post
-wp_reset_postdata();
+// // восстанавливаем глобальную переменную $post
+// wp_reset_postdata();
  
