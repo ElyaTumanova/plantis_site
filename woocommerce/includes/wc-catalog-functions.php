d602fd40855752093b5c326a6f9cb45f3b2674aa
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-20 18:58:19 +0300

    swiper

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -176,22 +176,6 @@ function plnt_catalog_gallery() {
 		</div>';
 
 		?>
-		<!-- <script>
-			jQuery(function($){
-				$('.nivo-catalog-gallery').nivoSlider({
-					effect: 'fade',               // эффекты, например: 'fold, fade, sliceDown, sliceDownLeft, sliceUp, sliceUpLeft, sliceUpDown, sliceUpDownLeft, slideInRight, slideInLeft'
-					animSpeed: 200,                 // скорость анимации
-					pauseTime: 3000,                // пауза между сменой слайдов
-					directionNav: true,             // нужно ли отображать кнопки перехода на следующий и предыдущий слайд
-					controlNav: true,               // 1,2,3... навигация (например в виде точек)
-					pauseOnHover: true,             // останавливать прокрутку слайдов при наведении мыши
-					manualAdvance: true,           // true - отключить автопрокрутку
-					prevText: '&#10094;',               // текст перехода на предыдущий слайд
-					nextText: '&#10095;',               // текст кнопки перехода на следующий слайд
-					randomStart: false,             // начинать со случайного слайда
-				});
-			});
-		</script> -->
 		<?php
 	} else {
 		woocommerce_template_loop_product_thumbnail();
