3dc40bfc60a24cb9579b502ebe7049005fc141c4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-20 16:11:08 +0300

    swiper

--- assets/js/slider-init.js
+++ assets/js/slider-init.js
@@ -7,16 +7,16 @@ const swiper_about_us = new Swiper('.swiper_about-us', {
         prevEl: '.swiper-button-prev',
     },
     slidesPerView: 3,
-    // spaceBetween: 10,
-    // breakpoints: {
-    //     320: {
-    //     slidesPerView: 1,
-    //     },
-    //     640: {
-    //     slidesPerView: 2,
-    //     },
-    //     768: {
-    //     slidesPerView: 3,
-    //     }
-    // }
+    spaceBetween: 10,
+    breakpoints: {
+        320: {
+        slidesPerView: 1,
+        },
+        640: {
+        slidesPerView: 2,
+        },
+        768: {
+        slidesPerView: 3,
+        }
+    }
 });
