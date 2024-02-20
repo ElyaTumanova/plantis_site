236e1b933f40b012d7790bea2dca75b23d51bcda
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-20 22:54:04 +0300

    swiper

--- template-parts/products-on-sale.php
+++ template-parts/products-on-sale.php
@@ -8,55 +8,7 @@ $product_ids_on_sale = wc_get_product_ids_on_sale();
 	print_r( $var );
 	echo '</pre>';
 
-// if( !empty ($product_ids_on_sale) ){
 
-//     $crosssell_ids = $crosssell_ids[0];
-
-//     if(count($crosssell_ids)>0){
-
-//         $args = array(
-//             'post_type' => 'product',
-//             'ignore_sticky_posts' => 1,
-//             'no_found_rows' => 1,
-//             'posts_per_page' => 8,
-//             'orderby' => $rand,
-//             'post__in' => $crosssell_ids,
-//             'meta_query' => array( 
-//                 array(
-//                     'key'       => '_stock_status',
-//                     'value'     => 'outofstock',
-//                     'compare'   => 'NOT IN'
-//                 )
-//             )
-//         );
-
-//         $products = new WP_Query( $args );
-//         if ( $products->have_posts() ) : ?>
-
-//             <div class="cross-sells">
-
-//                 <h2 class="heading-2"><?php _e( 'Похожие растения', 'woocommerce' ) ?></h2>       
-//                 <div class="cross-upsells-swiper swiper">
-//                     <ul class="products columns-3 swiper-wrapper"> 
-//                         <?php while ( $products->have_posts() ) : $products->the_post(); ?>
-
-//                         <?php wc_get_template_part( 'content', 'product' ); ?>
-
-//                         <?php endwhile; // end of the loop. ?>
-//                     </ul>
-//                     <div class="swiper-pagination"></div>
-// 	                <div class="swiper-button-prev"></div>
-// 	                <div class="swiper-button-next"></div>
-//                 </div>
-//             </div>
-
-//         <?php endif;
-
-//     }
-
-//     wp_reset_query();
-
-// }
 
 // <?php
 // global $wp_query;
