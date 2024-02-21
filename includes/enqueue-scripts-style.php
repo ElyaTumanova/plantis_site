6eb0fd20d9a175e004acca402801764880e29a63
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-21 13:10:07 +0300

    swiper

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -68,10 +68,10 @@ if ( ! function_exists( 'ast_styles' ) ) {
 		                             '/assets/css/magnific-popup.css', array(), null, 'all' );
 		wp_enqueue_style( 'owl-css', get_template_directory_uri() .
 		                             '/assets/css/owl.carousel.min.css', array(), null, 'all' );
+		wp_enqueue_style( 'swiper', get_template_directory_uri() .
+									'/assets/css/swiper.css', array(), null, 'all' ); //swiper
 		wp_enqueue_style( 'general', get_template_directory_uri() .
 		                             '/assets/css/general.css', array(), null, 'all' );
-		wp_enqueue_style( 'swiper', get_template_directory_uri() .
-		                             '/assets/css/swiper.css', array(), null, 'all' ); //swiper
 		wp_enqueue_style( 'main', get_template_directory_uri() .
 		                             '/assets/css/main.css', array(), null, 'all' );
 		wp_enqueue_style( 'header', get_template_directory_uri() .
