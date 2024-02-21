0694a6572f26e039abbcae1a369a4b43048a9ecc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-21 18:38:01 +0300

    styles

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -302,7 +302,7 @@ add_action('woocommerce_after_single_product_summary','plnt_card_ukhod_loop',40)
 
 function plnt_card_ukhod_loop() {
     // echo do_shortcode ('[products category="ukhod"]');
-    // get_template_part('template-parts/products-ukhod');
+    get_template_part('template-parts/products-ukhod');
     // get_template_part('template-parts/products-on-sale');
 }
 
