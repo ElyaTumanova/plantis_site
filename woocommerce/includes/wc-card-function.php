d04e0d852fe0637b238b118c5ee6cf554ee13de0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-21 18:17:28 +0300

    styles

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -301,8 +301,7 @@ function plnt_get_cross_sells(){
 add_action('woocommerce_after_single_product_summary','plnt_card_ukhod_loop',40);
 
 function plnt_card_ukhod_loop() {
-    get_template_part('template-parts/plnt-cross-sells');
-    // echo do_shortcode ('[products category="ukhod"]');
+    echo do_shortcode ('[products category="ukhod"]');
     // get_template_part('template-parts/products-ukhod');
 }
 
