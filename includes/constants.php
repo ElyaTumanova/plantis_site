86f3afdeb9fb0b359970d500f23276960eb24a8d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-21 14:13:25 +0300

    styles

--- includes/constants.php
+++ includes/constants.php
@@ -8,6 +8,20 @@ $plants_cat_id = 90;
 $gorshki_cat_id = 86;
 $treez_cat_id = 802;
 
-$tags_exeptions = array(66,106);
+$tags_exeptions = array(
+	374,
+	300,
+	275,
+	276,
+	277,
+	293,
+	283,
+	278,
+	294,
+	295,
+	347);
+
+
+
 
 
