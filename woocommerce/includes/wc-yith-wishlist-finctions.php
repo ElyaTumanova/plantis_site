bceb47206297760c75598af5adce0f7808ad78b7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-22 12:45:06 +0300

    metrika

--- woocommerce/includes/wc-yith-wishlist-finctions.php
+++ woocommerce/includes/wc-yith-wishlist-finctions.php
@@ -69,39 +69,47 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
   }
 
 
-  // перевод текстов
+// перевод текстов
 
-  function plnt_change_text_wish_1( $translated_text ) {
-    if ( $translated_text == 'Add all to cart' ) {
-      $translated_text = 'Добавить все в корзину';
-    }
-    return $translated_text;
+function plnt_change_text_wish_1( $translated_text ) {
+  if ( $translated_text == 'Add all to cart' ) {
+    $translated_text = 'Добавить все в корзину';
   }
-  add_filter( 'gettext', 'plnt_change_text_wish_1', 20 );
+  return $translated_text;
+}
+add_filter( 'gettext', 'plnt_change_text_wish_1', 20 );
 
-  function plnt_change_text_wish_2( $translated_text ) {
-    if ( $translated_text == 'Edit title' ) {
-      $translated_text = 'Редактировать название';
-    }
-    return $translated_text;
+function plnt_change_text_wish_2( $translated_text ) {
+  if ( $translated_text == 'Edit title' ) {
+    $translated_text = 'Редактировать название';
   }
-  add_filter( 'gettext', 'plnt_change_text_wish_2', 20 );
+  return $translated_text;
+}
+add_filter( 'gettext', 'plnt_change_text_wish_2', 20 );
 
-  function plnt_change_text_wish_3( $translated_text ) {
-    if ( $translated_text == 'Items correctly added to the cart' ) {
-      $translated_text = 'Товары добавлены в корзину';
-    }
-    return $translated_text;
+function plnt_change_text_wish_3( $translated_text ) {
+  if ( $translated_text == 'Items correctly added to the cart' ) {
+    $translated_text = 'Товары добавлены в корзину';
   }
-  add_filter( 'gettext', 'plnt_change_text_wish_3', 20 );
+  return $translated_text;
+}
+add_filter( 'gettext', 'plnt_change_text_wish_3', 20 );
 
-  function plnt_change_text_wish_4( $translated_text ) {
-    if ( $translated_text == 'Product added to cart successfully' ) {
-      $translated_text = 'Товары добавлены в корзину';
-    }
-    return $translated_text;
+function plnt_change_text_wish_4( $translated_text ) {
+  if ( $translated_text == 'Product added to cart successfully' ) {
+    $translated_text = 'Товары добавлены в корзину';
   }
-  add_filter( 'gettext', 'plnt_change_text_wish_4', 20 );
+  return $translated_text;
+}
+add_filter( 'gettext', 'plnt_change_text_wish_4', 20 );
+
+function plnt_change_text_wish_5( $translated_text ) {
+  if ( $translated_text == 'Product successfully removed.' ) {
+    $translated_text = 'Товар удален из избранного';
+  }
+  return $translated_text;
+}
+add_filter( 'gettext', 'plnt_change_text_wish_5', 20 );
 
 
   
