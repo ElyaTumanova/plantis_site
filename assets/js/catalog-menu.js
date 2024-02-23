89d51a9da7c9b39ee578dd05f9fa0c15cd76122d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-23 10:43:45 +0300

    styles

--- assets/js/catalog-menu.js
+++ assets/js/catalog-menu.js
@@ -2,7 +2,9 @@ const dropdown = document.querySelectorAll('.catalog__dropdown');
 
 dropdown.forEach((el) => {
 	const menu = el.querySelector('.catalog__dropdown-menu');
-	el.addEventListener('click', function (event) {
+	const btn = el.querySelector('.menu__dropdown-arrow');
+	console.log(btn);
+	btn.addEventListener('click', function (event) {
 		menu.classList.toggle('catalog__dropdown-menu_show');
 		el.classList.toggle('catalog__dropdown_open');
 		event.stopPropagation();
