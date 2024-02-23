259972de14b353c5ca6c9fc1a73428dde55c4fc8
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-23 14:31:02 +0300

    remove from cart

--- woocommerce/includes/wc-cart-functions.php
+++ woocommerce/includes/wc-cart-functions.php
@@ -55,9 +55,7 @@ function plnt_woocommerce_mini_cart_fragment( $fragments ) {
 add_filter( 'woocommerce_add_to_cart_fragments', 'plnt_woocommerce_mini_cart_fragment', 25 );
 
 function plnt_remove_from_cart(){
-	?>
-	<div class="newbtn">
-	<?php
+
 	global $product;
 
 	if ( $product ) {
@@ -96,15 +94,13 @@ function plnt_remove_from_cart(){
 
 		wc_get_template( 'loop/add-to-cart.php', $args );
 	}
-	?>
-	</div>
-	<?php
+
 }
 
 function plnt_woocommerce_add_to_cart_btn( $fragments ) {
 	ob_start();
 	plnt_remove_from_cart();
-	$fragments[ '.newbtn'] = ob_get_clean();
+	$fragments[ '.product_type_simple'] = ob_get_clean();
 	return $fragments;
 }
 
