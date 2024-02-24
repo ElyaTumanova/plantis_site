a0c4e25ed099607dd174592b7d02b66d509e4a94
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-24 16:11:52 +0300

    swiper

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -152,7 +152,7 @@ add_action('woocommerce_before_shop_loop_item_title','woocommerce_template_loop_
 
 remove_action('woocommerce_before_shop_loop_item_title','woocommerce_template_loop_product_thumbnail', 10);
 add_action('woocommerce_before_shop_loop_item_title','plnt_catalog_gallery', 10);
-add_action('woocommerce_before_shop_loop_item_title','swiper_init', 10);
+add_action('woocommerce_after_shop_loop','swiper_init', 30);
 
 
 function plnt_catalog_gallery() {
