c3707e91a085f0ad3f5322988122a6ec3b1aa5a7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-25 16:01:35 +0300

    styles

--- template-parts/plants-cats-grid.php
+++ template-parts/plants-cats-grid.php
@@ -17,40 +17,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="cats-grid">
-    <div class="cats-grid__inner cats-grid__inner_palms">
+    <a class="cats-grid__inner cats-grid__inner_palms">
         <h3 class="cats-grid__title">Пальмы</h3>
         <img src="<?php echo $cats_palms ?>" class="cats-grid__img" alt="Пальмы">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_fikus">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_fikus">
         <h3 class="cats-grid__title">Фикусы</h3>
         <img src="<?php echo $cats_fikus ?>" class="cats-grid__img" alt="Фикусы">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_lisv">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_lisv">
         <h3 class="cats-grid__title">Декоративно-лиственные</h3>
         <img src="<?php echo $cats_lisv ?>" class="cats-grid__img" alt="Декоративно-лиственные">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_napol">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_napol">
         <h3 class="cats-grid__title">Напольные</h3>
         <img src="<?php echo $cats_napol ?>" class="cats-grid__img" alt="Напольные">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_cvetush">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_cvetush">
         <h3 class="cats-grid__title">Цветущие</h3>
         <img src="<?php echo $cats_cvetush ?>" class="cats-grid__img" alt="Цветущие">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_lianas">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_lianas">
         <h3 class="cats-grid__title">Лианы</h3>
         <img src="<?php echo $cats_lianas ?>" class="cats-grid__img" alt="Лианы">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_neprikhotliv">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_neprikhotliv">
         <h3 class="cats-grid__title">Неприхотливые</h3>
         <img src="<?php echo $cats_neprikhotliv ?>" class="cats-grid__img" alt="Неприхотливые растения">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_succulent">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_succulent">
         <h3 class="cats-grid__title">Суккуленты</h3>
         <img src="<?php echo $cats_succulent ?>" class="cats-grid__img" alt="Суккуленты">
-    </div>
-    <div class="cats-grid__inner cats-grid__inner_petfriendly">
+    </a>
+    <a class="cats-grid__inner cats-grid__inner_petfriendly">
         <h3 class="cats-grid__title">Pet Friendly</h3>
         <img src="<?php echo $cats_petfriendly ?>" class="cats-grid__img" alt="Pet Friendly">
-    </div>
+    </a>
 </div>
