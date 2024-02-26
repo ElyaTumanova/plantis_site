5ebb9f53771957227fc3b34a9c692a479a7d0ce1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-26 13:47:55 +0300

    side cart

---
+++ assets/js/side-cart.js
@@ -0,0 +1,43 @@
+//переменные для управления попапом
+const sideCartOpenPopupBtn = document.querySelector('.side-cart__open-btn');
+const sideCartPopup = document.querySelector('.side-cart__popup');
+// const preorderClosePopupBtn = document.querySelector('.page-popup__close');
+const sideCartPopupOverlay = document.querySelector('.side-cart__popup-overlay');
+
+if (sideCartPopup != null && sideCartOpenPopupBtn != null) {
+    
+    sideCartOpenPopupBtn.addEventListener ("click", (evt)=>{
+        toggle_side_cart_popup ();
+    });
+
+    // if (preorderClosePopupBtn) {
+    //     preorderClosePopupBtn.addEventListener ("click", (evt)=>{
+    //         toggle_side_cart_popup ();
+    //     });
+    // }
+
+    sideCartPopupOverlay.addEventListener ("click", (evt)=>{
+        toggle_side_cart_popup ();
+    });
+
+    document.addEventListener('keydown', function(e){
+        if((e.key=='Escape'||e.key=='Esc')){
+            if(sideCartPopup.classList.contains('popup_active')) {
+                toggle_side_cart_popup ();
+            } 
+        }
+    }, true);
+}
+
+function toggle_side_cart_popup () {
+    sideCartPopup.classList.toggle ('popup_active');
+    // for side cart
+    if (sideCartOpenPopupBtn) {
+        sideCartOpenPopupBtn.classList.toggle ('side-cart__open-btn_active');
+    }
+};
+
+
+
+
+
--- footer.php
+++ footer.php
@@ -30,10 +30,10 @@
 </div>
 
 <div class="side-cart__wrap">
-    <button class="side-cart__open-btn page-popup-open-btn">hello</button>
-    <div class="side-cart__wrap page-popup popup">
+    <button class="side-cart__open-btn">hello</button>
+    <div class="side-cart__popup popup">
         <div class="side-cart">        </div>
-        <div class="page-popup__popup-overlay popup-overlay"></div>
+        <div class="side-cart__popup-overlay popup-overlay"></div>
     </div>
 </div>
 
