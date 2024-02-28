372e65320972d846de67bef7d7a68d01212fefc1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-28 12:32:26 +0300

    styles

--- template-parts/card-banners.php
+++ template-parts/card-banners.php
@@ -4,12 +4,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<div class="card__banners">
-    <div class="card__banner" id="card__banner_photo">
-        <p>Напишите нам, и мы пришлём вам “живые” фотографии этого растения</p>
-        <?php get_template_part('template-parts/social-media-btns');?>
-    </div>
-    <div class="card__banner" id="card__banner_peresadka">
-        <p>При покупке <a href="<?php echo get_site_url()?>/product-category/gorshki_i_kashpo/" target="_blank" rel="noopener">горшка </a>— пересадка в подарок!</p>
-    </div>
+<div class="card_banner" id="card_banner_photo">
+    <p>Напишите нам, и мы пришлём вам “живые” фотографии этого растения</p>
+    <?php get_template_part('template-parts/social-media-btns');?>
+</div>
+<div class="card_banner" id="card_banner_peresadka">
+    <p>При покупке <a href="<?php echo get_site_url()?>/product-category/gorshki_i_kashpo/" target="_blank" rel="noopener">горшка </a>— пересадка в подарок!</p>
 </div>
