a3ee50a35f0798bbb85fe036ba1cb49f78708002
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-28 13:40:23 +0300

    side cart

--- footer.php
+++ footer.php
@@ -31,8 +31,8 @@
 
 <div class="side-cart__wrap">
     <?php $cart_icon = carbon_get_theme_option('cart_icon')?>
-    <span class="side-cart__count"><?php echo wp_kses_data(WC()->cart->get_cart_contents_count())?></span>
     <button class="side-cart__open-btn">
+        <span class="side-cart__count"><?php echo wp_kses_data(WC()->cart->get_cart_contents_count())?></span>
         <img class="side-cart__icon" src="<?php echo $cart_icon ?>" alt="cart" width="35" height="35">
     </button>
     <div class="side-cart__popup popup">
