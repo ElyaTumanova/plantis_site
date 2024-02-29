169310773f671857cc7323ce107c6baf58175710
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-29 11:22:58 +0300

    yandex

--- includes/xml/create-test-xml.php
+++ includes/xml/create-test-xml.php
@@ -4,6 +4,21 @@
   $dom->appendChild($root);
   $shop = $dom->createElement("shop");
   $root->appendChild($shop);
+
+  $name = $dom->createElement("name",'Plantis');
+  $company = $dom->createElement("company",'Plantis');
+  $url = $dom->createElement("url",'https://plantis.shop/');
+  $platform = $dom->createElement("platform",'WordPress - Yml for Yandex Market');
+  $version = $dom->createElement("version",'6.4.3');
+
+  $shop->appendChild($name);
+  $shop->appendChild($company);
+  $shop->appendChild($url);
+  $shop->appendChild($platform);
+  $shop->appendChild($version);
+  
+
+
 //   $logins = array("User1", "User2", "User3"); // Логины пользователей
 //   $passwords = array("Pass1", "Pass2", "Pass3"); // Пароли пользователей
 //   for ($i = 0; $i < count($logins); $i++) {
@@ -18,3 +33,11 @@
 //   }
   $dom->save("wp-content/yandex-xml/users1.xml"); // Сохраняем полученный XML-документ в файл
 ?>
+
+
+<!-- 
+<name>Plantis</name>
+<company>Plantis</company>
+<url>https://plantis.shop/</url>
+<platform>WordPress - Yml for Yandex Market</platform>
+<version>6.4.3</version> -->
