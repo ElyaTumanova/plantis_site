a31bc2710a621f595062a60e770e758e35715edd
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-02-29 11:15:39 +0300

    yandex

--- includes/xml/create-test-xml.php
+++ includes/xml/create-test-xml.php
@@ -1,18 +1,18 @@
 <?php
   $dom = new domDocument("1.0", "utf-8"); // Создаём XML-документ версии 1.0 с кодировкой utf-8
-  $root = $dom->createElement("users"); // Создаём корневой элемент
+  $root = $dom->createElement("yml_catalog"); // Создаём корневой элемент
   $dom->appendChild($root);
-  $logins = array("User1", "User2", "User3"); // Логины пользователей
-  $passwords = array("Pass1", "Pass2", "Pass3"); // Пароли пользователей
-  for ($i = 0; $i < count($logins); $i++) {
-    $id = $i + 1; // id-пользователя
-    $user = $dom->createElement("user"); // Создаём узел "user"
-    $user->setAttribute("id", $id); // Устанавливаем атрибут "id" у узла "user"
-    $login = $dom->createElement("login", $logins[$i]); // Создаём узел "login" с текстом внутри
-    $password = $dom->createElement("password", $passwords[$i]); // Создаём узел "password" с текстом внутри
-    $user->appendChild($login); // Добавляем в узел "user" узел "login"
-    $user->appendChild($password);// Добавляем в узел "user" узел "password"
-    $root->appendChild($user); // Добавляем в корневой узел "users" узел "user"
-  }
+//   $logins = array("User1", "User2", "User3"); // Логины пользователей
+//   $passwords = array("Pass1", "Pass2", "Pass3"); // Пароли пользователей
+//   for ($i = 0; $i < count($logins); $i++) {
+//     $id = $i + 1; // id-пользователя
+//     $user = $dom->createElement("user"); // Создаём узел "user"
+//     $user->setAttribute("id", $id); // Устанавливаем атрибут "id" у узла "user"
+//     $login = $dom->createElement("login", $logins[$i]); // Создаём узел "login" с текстом внутри
+//     $password = $dom->createElement("password", $passwords[$i]); // Создаём узел "password" с текстом внутри
+//     $user->appendChild($login); // Добавляем в узел "user" узел "login"
+//     $user->appendChild($password);// Добавляем в узел "user" узел "password"
+//     $root->appendChild($user); // Добавляем в корневой узел "users" узел "user"
+//   }
   $dom->save("wp-content/yandex-xml/users1.xml"); // Сохраняем полученный XML-документ в файл
 ?>
