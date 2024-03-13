01fc65713db7c5664dff5c56dae6d3f2d068848b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-13 22:39:11 +0300

    initial set up

--- template-parts/plnt-cross-sells.php
+++ template-parts/plnt-cross-sells.php
@@ -20,7 +20,7 @@ if( !empty ($crosssell_ids) ){
             'ignore_sticky_posts' => 1,
             'no_found_rows' => 1,
             'posts_per_page' => 8,
-            'orderby' => $rand,
+            'orderby' => 'rand',
             'post__in' => $crosssell_ids,
             'meta_query' => array( 
                 array(
