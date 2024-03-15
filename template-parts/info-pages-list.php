4eb69eb009655c73dbb987f4664c8ac9e8251dfa
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-15 14:11:06 +0300

    styles

--- template-parts/info-pages-list.php
+++ template-parts/info-pages-list.php
@@ -11,3 +11,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="menu__item"> <a href="<?php echo site_url()?>/refund_returns/">Возврат и обмен</a></div>
 <div class="menu__item"> <a href="<?php echo site_url()?>/faq/">Вопросы и ответы</a></div>
 <div class="menu__item"> <a href="<?php echo site_url()?>/partners/">Поставщикам и партнерам</a></div>
+<div class="menu__item"> <a href="<?php echo site_url()?>/vakansii/">Вакансии</a></div>
