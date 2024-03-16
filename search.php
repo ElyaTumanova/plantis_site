68abb0e3f105eab294bcf6d6ccb9e82758e57e52
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-16 12:31:45 +0300

    search

--- search.php
+++ search.php
@@ -1,86 +1,43 @@
 <?php
 /**
- * The template for displaying search results pages
+ * Template for displaying Search Results pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package estore
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
  */
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<div id="container">
+			<div id="content" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
+<?php if ( have_posts() ) : ?>
+				<h1 class="page-title">
+				<?php
+				/* translators: %s: Search query. */
+				printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' );
+				?>
+				</h1>
+				<?php
+				/*
+				 * Run the loop for the search to output the results.
+				 * If you want to overload this in a child theme then include a file
+				 * called loop-search.php and that will be used instead.
+				 */
+				get_template_part( 'loop', 'search' );
+				?>
+<?php else : ?>
+				<div id="post-0" class="post no-results not-found">
+					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
+					<div class="entry-content">
+						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-0 -->
+<?php endif; ?>
+			</div><!-- #content -->
+		</div><!-- #container -->
 
-			<header class="page-header">
-				<h1 class="page-title"><?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Результаты поиска: %s', 'estore' ), '<span>' . get_search_query() . '</span>' );
-				?></h1>
-			</header><!-- .page-header -->
-
-            <?php
-            global $wp_query;
-            $arg = array(
-                'post_type' => 'product', // если нужен поиск по постам - доавляем в массив 'post'
-                'post_status' => 'publish',
-                's' => get_search_query(),
-                'posts_per_page' => -1,
-                'meta_query' => array( 
-                    array(
-                        'key'       => '_stock_status',
-                        'value'     => 'outofstock',
-                        'compare'   => 'NOT IN'
-                    )
-                )
-            );
-            $search_query = new WP_Query($arg);
-            $wp_query = $search_query;
-            ?>
-
-            <div class="catalog__grid">
-                <div class="catalog__sidebar">
-                    <?php plnt_catalog_menu() ?>
-                </div>
-                <div class="catalog__products-wrap">
-                    <ul class="products columns-2"> 
-                        <?php
-                        /* Start the Loop */
-                        while ( have_posts() ) : the_post();
-                        $product = wc_get_product( get_the_ID() );
-                        if ($product->is_in_stock()) {
-                            wc_get_template_part( 'content', 'product' );
-                        }                      
-                        endwhile;
-                        ?>
-                    </ul> 
-                    
-                    <?php
-
-                    // the_posts_pagination();
-
-                    the_posts_pagination( array(
-                        'class' => 'woocommerce-pagination',
-                        'prev_text'    => __('←'),
-	                    'next_text'    => __('→'),
-                    ));
-
-                    else :
-
-                        get_template_part( 'template-parts/content', 'none' );
-
-                    endif; ?>
-                     
-                </div>
-            </div>
-			
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
