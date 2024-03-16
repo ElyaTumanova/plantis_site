c279052e9bea45f8237ea5591ca75751a7d86ca9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-16 13:23:17 +0300

    search

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ) ?>" >
-	<label class="screen-reader-text" for="s">Поиск: </label>
-	<input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" />
+<form role="search" method="get" id="searchform" class="search-form" action="<?php echo home_url( '/' ) ?>" >
+	<label class="screen-reader-text" for="s">Найти: </label>
+	<input type="search" placeholder="Поиск…" value="<?php echo get_search_query() ?>" name="s" id="s" />
     <input type="hidden" value="product" name="post_type" />
-	<input type="submit" id="searchsubmit" value="найти" />
+	<input class="search-submit" type="submit" id="searchsubmit" value="Поиск" />
 </form>
