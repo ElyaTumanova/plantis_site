227a9c1acb53a3133432e6e8f822bb6f14aa6a6c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-16 14:54:09 +0300

    search

--- template-parts/popups/search-popup.php
+++ template-parts/popups/search-popup.php
@@ -7,8 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="search-popup popup">
     <div class="search">
         <div class="search__wrap container">
-            <?//get_search_form();?>
-            <?php aws_get_search_form( true ); ?>
+            <?get_search_form();?>
             <div class="search__close">&#10006;</div>
         </div>
         <div class="search-result container"></div>
