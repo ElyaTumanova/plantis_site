bc29b5b9029a989a9f0b7491b1d27376d0a63ad9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-17 12:30:00 +0300

    search

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -310,32 +310,6 @@ function plnt_add_class_loop_item_swiper($clasess){
 
 // // вывод товаров в каталоге с учетом наличия - instock products first 
 
-// class iWC_Orderby_Stock_Status
-// {
- 
-// 	public function __construct()
-// 	{
-// 		// Check if WooCommerce is active
-// 		if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-// 			add_filter('posts_clauses', array($this, 'order_by_stock_status'), 2000);
-// 		}
-// 	}
- 
-// 	public function order_by_stock_status($posts_clauses)
-// 	{
-// 		global $wpdb;
-// 		// only change query on WooCommerce loops
-// 		if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag()) || is_search()) {
-// 			$posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
-// 			$posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
-// 			$posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
-// 		}
-// 		return $posts_clauses;
-// 	}
-// }
- 
-// new iWC_Orderby_Stock_Status;
-
 add_filter('posts_clauses', 'order_by_stock_status', 9999);
 function order_by_stock_status($posts_clauses) {
     global $wpdb;
