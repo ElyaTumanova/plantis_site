56b8241af6c59a52f47e2be9cc39fc710f265829
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-21 13:06:17 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -430,14 +430,15 @@ function min_amount_for_category(){
     }
 }
 
-add_action( 'woocommerce_before_checkout_form', 'min_amount_for_category_info' );
+//add_action( 'woocommerce_before_checkout_form', 'min_amount_for_category_info' );
+add_action( 'woocommerce_checkout_order_review', 'min_amount_for_category_info',40 );
 
 function min_amount_for_category_info(){
     global $treez_cat_id;
     $min_treez_delivery = carbon_get_theme_option('min_treez_delivery');
-    // echo '<div>hi</div>';
-    // echo '<div class="checkout__free-delivery-text">
-    // Минимальная сумма заказа для кашпо Treez <span>'.$min_treez_delivery,'</span> (без учета стоимости других товаров)</div>';
+    echo '<div>hi</div>';
+    echo '<div class="checkout__free-delivery-text">
+    Минимальная сумма заказа для кашпо Treez <span>'.$min_treez_delivery,'</span> (без учета стоимости других товаров)</div>';
 	$qty = 0; // обязательно сначала ставим 0
  	$cat_amount = 0;
 	$products_min = false;
