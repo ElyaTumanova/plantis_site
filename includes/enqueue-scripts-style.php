345cdbadb49b8edfefe4fd9141b2d6f4e25a7efd
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-03-22 14:27:21 +0300

    styles

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -68,8 +68,8 @@ if ( ! function_exists( 'ast_styles' ) ) {
 	add_action( 'wp_enqueue_scripts', 'ast_styles' );
 	function ast_styles() {
 		wp_enqueue_style( 'ast-style', get_stylesheet_uri() );
-		wp_enqueue_style( 'magnific-css', get_template_directory_uri() .
-		                             '/assets/css/magnific-popup.css', array(), null, 'all' );
+		// wp_enqueue_style( 'magnific-css', get_template_directory_uri() .
+		//                              '/assets/css/magnific-popup.css', array(), null, 'all' );
 		// wp_enqueue_style( 'owl-css', get_template_directory_uri() .
 		//                              '/assets/css/owl.carousel.min.css', array(), null, 'all' );
 		wp_enqueue_style( 'swiper', get_template_directory_uri() .
