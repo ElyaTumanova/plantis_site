14ce54a563460275f8be046113e6ee910b6aa49a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-04 15:43:38 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -464,18 +464,12 @@ add_action ('woocommerce_cart_has_errors', 'plnt_check_cart_item_stock');
 function plnt_check_cart_item_stock() {
 
     $isOutOfStock = false;
-    echo '<pre>';
-    print_r( $isOutOfStock );
-    echo '</pre>';
 
     foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
         $product = $cart_item['data'];
 
         if ( $product->get_stock_status() ==='outofstock') {
             $isOutOfStock = true;
-            echo '<pre>';
-            print_r( $isOutOfStock );
-            echo '</pre>';
         } 
     }
     
