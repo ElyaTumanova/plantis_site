d228cc406ace81c8a5f2b9fae99ff49e5cb484fd
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-10 11:52:52 +0300

    canonical

--- functions.php
+++ functions.php
@@ -74,20 +74,13 @@ function plnt_check_page() {
 		print_r( $canon_url );
 		echo '</pre>';
 	}
+
+	if ( is_singular( 'post' )) {
+		echo 'Вы находитесь на странице поста.';
+	}
 }
 
  add_action( 'wp_footer', 'plnt_check_page' );
-
- 
- // Удалить каноническую ссылку - SEO by Yoast
-
-function at_remove_dup_canonical_link() {
-
-return false;
-
-}
-
-add_filter( 'wpseo_canonical', 'at_remove_dup_canonical_link' );
 
 
 // function get_cats() {
