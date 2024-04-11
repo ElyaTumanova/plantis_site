fe2cac1e5c6d94cdfe8e7c187b8976dc361cb15e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-11 15:58:35 +0300

    upsells

--- woocommerce/single-product/up-sells.php
+++ woocommerce/single-product/up-sells.php
@@ -30,12 +30,16 @@ if ( $upsells ) : ?>
 		if ( $heading ) :
 			?>
 			<h2 class="heading-2"><?php echo esc_html( $heading ); ?></h2>
+            <?php echo '<pre>';
+                    print_r( $upsells );
+                    echo '</pre>';?>
 		<?php endif; ?>
 
         <div class="cross-upsells-swiper swiper">
             <ul class="products columns-3 swiper-wrapper">
 
                 <?php foreach ( $upsells as $upsell ) : ?>
+                    
                     <?php if ( ! $upsell->is_in_stock() && ! $upsell->backorders_allowed() ) : continue; endif; ?>
                     <?php
                     $post_object = get_post( $upsell->get_id() );
