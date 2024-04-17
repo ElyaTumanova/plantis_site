b74238aac6a28fff5e8044c34eeca34f20f592ea
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-17 12:14:54 +0300

    notice

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -442,46 +442,48 @@ function filter_wpseo_robots( $robotsstr ) {
 
 // изменяем canonical для страниц пагинации #SEO
 
-// add_filter('wpseo_canonical', 'removeCanonicalArchivePagination');
+//убирем канонакал, который выводит Load More плагин
+function remove_my_theme_canonical() {
+    $br_aapf_paid_instance = BeRocket_AAPF_paid::getInstance();
+    remove_action('wp_head', array($br_aapf_paid_instance, 'wp_head_canonical'), 99999);
+}
+add_action('init', 'remove_my_theme_canonical');
 
-// function removeCanonicalArchivePagination($link) {
-// $link = preg_replace('#\\??/page[\\/=]\\d+#', '', $link);
-// 	return $link;
-// }
 
+function add_custom_canonical_tags() {
+    if (is_paged()) {
+        // Получаем URL первой страницы текущего архива
+        $first_page_url = get_pagenum_link(1);
+		
+		// Разбираем URL на составляющие
+		$parsedUrl = parse_url($first_page_url);
 
+		// Строим новый URL без query-параметров
+		$first_page_url = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . $parsedUrl['path'];
+				
+        // Добавляем canonical тег
+        echo '<link rel="canonical" href="' . esc_url($first_page_url) . '" />' . "\n";
 
-// add_filter( 'get_canonical_url', 'subpages_canonical', 10, 2 );
+        // Получаем номер текущей страницы
+        $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 
-// function subpages_canonical( $canonical_url, $post ){
+        // Добавляем ссылки на следующую и предыдущую страницы
+        if ($paged > 1) {
+            $prev_page_url = get_pagenum_link($paged - 1);
+            echo '<link rel="prev" href="' . esc_url($prev_page_url) . '" />' . "\n";
+        }
+        
+        // Проверяем, есть ли следующая страница
+        global $wp_query;
+        if ($paged < $wp_query->max_num_pages) {
+            $next_page_url = get_pagenum_link($paged + 1);
+            echo '<link rel="next" href="' . esc_url($next_page_url) . '" />' . "\n";
+        }
+    }
+}
 
-// 	if( $subpage = get_query_var('post_subpage') ){
-// 		$canonical_url = user_trailingslashit( rtrim( get_permalink( $post ), '/' ) ."/$subpage" );
-// 	}
-
-// 	return $canonical_url;
-// }
-
-// add_action('wp_head', 'woocommerce_pagination_canonical_tag');
-
-// function woocommerce_pagination_canonical_tag() {
-//     if (is_paged()) {
-//         $canonical_url = get_pagenum_link(1);
-//         echo '<link rel="canonical" href="' . $canonical_url . '" />' . "\n";
-//     }
-// }
-
-// set home_page canonical for each home pagination link
-// add_filter( 'get_canonical_url', 'wp_kama_get_canonical_url_filter', 10, 2 );
-// function wp_kama_get_canonical_url_filter( $canonical_url, $post ){
-
-// 	if( is_paged() ){
-// 		$canonical_url = get_pagenum_link(1);
-// 		// return $canonical_url;
-// 	}
-
-// 	return $canonical_url;
-// }
+// Добавляем действие в WordPress, чтобы выполнить функцию при выводе тегов в head
+add_action('wp_head', 'add_custom_canonical_tags');
 
 // изменяем названия меток на подборки для хлебных крошек #breadcrumb
 add_filter( 'woocommerce_get_breadcrumb', 'plnt_woocommerce_get_breadcrumb_filter', 10, 2 );
