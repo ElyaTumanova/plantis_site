304a108576dc2c6749dcaee76c0feea8bbb0098b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-18 13:12:38 +0300

    notice

--- template-parts/header-notice.php
+++ template-parts/header-notice.php
@@ -3,8 +3,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-$notice = carbon_get_theme_option('notice');?>
+$notice = carbon_get_theme_option('notice');
+$show_notice = carbon_get_theme_option('show_notice');
 
+if $show_notice === 'yes' {
+?>
 <div class="header__notice">
     <?php echo $notice ?>
-</div>
+</div> 
+<?php
+};
