dfded18df9f28fd325e928b41e771783328188ea
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-24 19:08:34 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -218,13 +218,9 @@ function new_custom_checkout_field_script() {
 			
 			// Initializing at start after checkout init (Based on the chosen shipping method)
             $( 'form.checkout' ).on( 'change', payment_method, function() {	
-                console.log(inn_field);
-                console.log(payment_inn_chekbox);
-                console.log($(payment_method_checked).val());
 				if( $(payment_method_checked).val() == payment_inn_chekbox) // Chosen "INN payment" (Show "INN")
                 {
                     inn_field.classList.remove('d-none');
-                    console.log('hi');
                 } else {
 					inn_field.classList.add('d-none');
 				};
