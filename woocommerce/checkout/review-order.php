172f3f4cd02c45f2a970b059229328bf83500d46
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-04-29 12:18:16 +0300

    ecommerce

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -33,7 +33,7 @@ defined( 'ABSPATH' ) || exit;
 		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
 			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 			$parentCatId = check_category($_product);
-			$price = WC()->cart->get_product_price( $_product );
+			$price = get_post_meta($cart_item['product_id'] , '_price', true);
 
 			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {	
 				?>
