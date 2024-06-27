9cee59cac0ec6112ff01d5049a94b7b29dbd09b5
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-06-27 18:05:47 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -44,7 +44,7 @@ function plnt_card_grid_end () {
 };
 
 // табы(убираем) и описание
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+// remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
 add_action('woocommerce_before_single_product_summary', 'plnt_product_description', 10);
 
 function plnt_product_description () {
