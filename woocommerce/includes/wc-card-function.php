075efb89230c5db362b629a6ccb81acd71229e16
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-01 16:54:35 +0300

    card

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -350,23 +350,6 @@ function plnt_product_attributes(){
 
 //upsells & cross sells
 
-// add_action('woocommerce_after_single_product_summary', 'plnt_card_crosssells_wrap_start', 6);
-
-function plnt_card_crosssells_wrap_start(){
-    ?>
-    <div class="card__crosssell-wrap">
-    
-    <?php 
-};
-
-// add_action('woocommerce_after_single_product_summary', 'plnt_card_crosssells_wrap_end', 8);
-
-function plnt_card_crosssells_wrap_end(){
-    ?>
-    </div>
-    <?php 
-};
-
 //add_action('woocommerce_after_single_product_summary','plnt_sliders_wrap_start', 10);
 
 function plnt_sliders_wrap_start() {
@@ -426,14 +409,6 @@ add_action('woocommerce_after_single_product_summary','plnt_get_cross_sells', 20
 function plnt_get_cross_sells(){
     get_template_part('template-parts/plnt-cross-sells');
 }
-
-// tabs
-
-// add_action('woocommerce_after_single_product_summary','plnt_get_card_tabs', 35);
-
-// function plnt_get_card_tabs() {
-//     get_template_part('template-parts/card-tabs');
-// }
 
 // товары для ухода
 
