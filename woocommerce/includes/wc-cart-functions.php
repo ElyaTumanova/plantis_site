d63a9c259b623e1d0b730d7d498381b87a711866
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-06 13:41:22 +0300

    cart

--- woocommerce/includes/wc-cart-functions.php
+++ woocommerce/includes/wc-cart-functions.php
@@ -18,7 +18,7 @@ remove_action( 'woocommerce_before_checkout_form', 'woocommerce_output_all_notic
 // популярные товары в корзине
 add_action( 'woocommerce_after_cart', 'plnt_cart_popular', 10);
 add_action( 'woocommerce_cart_is_empty', 'plnt_cart_popular', 30);
-add_action( 'woocommerce_before_cart', 'plnt_cart_popular_swiper_init', 30);
+add_action( 'woocommerce_cart_is_empty', 'plnt_cart_popular_swiper_init', 30);
 
 function plnt_cart_popular() {
 	get_template_part('template-parts/products-popular'); 
