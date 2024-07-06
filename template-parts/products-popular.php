5b6fb32e85cf4dd58dfc8b599d3fba529cc2c7be
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-06 11:28:15 +0300

    cart

--- template-parts/products-popular.php
+++ template-parts/products-popular.php
@@ -9,7 +9,7 @@ $args = array(
     'post_type' => 'product',
     'ignore_sticky_posts' => 1,
     'no_found_rows' => 1,
-    'posts_per_page' => 8,
+    'posts_per_page' => 6,
     'orderby' => 'rand',
     'meta_query' => array( 
         array(
