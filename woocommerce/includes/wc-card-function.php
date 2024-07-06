ec3f843857c29ad52dcdf5732e8427db90d21bbc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-06 11:45:05 +0300

    cart

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -437,6 +437,7 @@ function plnt_get_cross_sells(){
 // товары для ухода
 
 add_action('woocommerce_after_single_product_summary','plnt_card_ukhod_loop',40);
+add_action('woocommerce_after_single_product_summary','plnt_cart_popular',50);
 
 function plnt_card_ukhod_loop() {
     get_template_part('template-parts/products-ukhod');
