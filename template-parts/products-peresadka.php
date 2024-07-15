b9b6ab1838b94e57842b5cbbc99f2e45655bf5a2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-15 14:25:44 +0300

    peresadka

---
+++ template-parts/products-peresadka.php
@@ -0,0 +1,56 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $product;
+$is_out_of_stock = $product->get_stock_status() ==='outofstock';
+
+
+$crosssell_ids = get_post_meta( get_the_ID(), '_crosssell_ids' );
+
+if( !empty ($crosssell_ids) ){
+
+    $crosssell_ids = $crosssell_ids[0];
+    
+
+    if(count($crosssell_ids)>0){
+
+        $args = array(
+            'post_type' => 'product',
+            'ignore_sticky_posts' => 1,
+            'no_found_rows' => 1,
+            'posts_per_page' => 8,
+            'orderby' => 'rand',
+            'post__in' => $crosssell_ids,
+            'meta_query' => array( 
+                array(
+                    'key'       => '_stock_status',
+                    'value'     => 'outofstock',
+                    'compare'   => 'NOT IN'
+                )
+            ),
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'product_cat',
+                    'field' => 'slug',
+                    'terms' => 'peresadka'
+                )
+            )
+        );
+
+        $products = new WP_Query( $args );
+        if ( $products->have_posts() ) : $products->the_post() ?>
+
+            <div class="card__peresadka">
+                <?php wc_get_template_part( 'content', 'product' ); ?>>
+            </div>
+
+        <?php endif;
+
+    }
+
+    wp_reset_query();
+
+}
+
