ce555010190d78ce02c6e5b59ab93090a6ec72ba
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-15 21:39:14 +0300

    delivery

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -241,6 +241,7 @@ function new_custom_checkout_field_script() {
 			
 			 $( 'form.checkout' ).on( 'change', ism, function() {	
                 console.log($(ismc).val());	
+                console.log(deliveryInterval);
 				if( $(ismc).val() == localPickup || $(ismc).val() == urgentPickup1 || $(ismc).val() == urgentPickup2 || $(ismc).val() == urgentPickup3 || $(ismc).val() == urgentPickup4) // Chosen "Local pickup or Urgent pickup" (Hiding "Interval")
                 {
 					if (deliveryInterval) {deliveryInterval.classList.add('d-none')};
