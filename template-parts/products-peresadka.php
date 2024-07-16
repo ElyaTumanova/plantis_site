a143512c4e55c1256e239a0e9166de72dff1a9c7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-16 16:34:39 +0300

    peresadka

--- template-parts/products-peresadka.php
+++ template-parts/products-peresadka.php
@@ -4,4 +4,51 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // global $product;
-echo 'hihihi';
+$product_id = $args[ 'product_id' ];
+echo 'hihihi'.$product_id.'hihihi';
+$product = wc_get_product( $product_id );
+
+$crosssell_ids = get_post_meta( get_the_ID(), '_crosssell_ids' );
+
+if( !empty ($crosssell_ids) ){
+
+    $crosssell_ids = $crosssell_ids[0];
+    
+
+    if(count($crosssell_ids)>0){
+
+        $args = array(
+            'post_type' => 'product',
+            'ignore_sticky_posts' => 1,
+            'no_found_rows' => 1,
+            'posts_per_page' => 8,
+            'orderby' => 'rand',
+            'post__in' => $crosssell_ids,
+            'meta_query' => array( 
+                array(
+                    'key'       => '_stock_status',
+                    'value'     => 'outofstock',
+                    'compare'   => 'NOT IN'
+                )
+            ),
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'product_cat',
+                    'field' => 'slug',
+                    'terms' => 'peresadka'
+                )
+            )
+        );
+
+        $products = new WP_Query( $args );
+        if ( $products->have_posts() ) : $products->the_post(); 
+        ?>
+            <?php woocommerce_template_loop_add_to_cart();
+            woocommerce_quantity_input();?>
+        <?php endif;
+    }
+    wp_reset_query();
+    wp_reset_postdata();
+
+}
+
