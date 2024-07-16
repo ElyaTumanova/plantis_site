ae0025727591bea44bf4543764318c08b1cf5ef4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-16 16:22:02 +0300

    peresadka

--- template-parts/products-peresadka.php
+++ template-parts/products-peresadka.php
@@ -6,9 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // global $product;
 $product = $args[ 'product' ];
 echo $product;
-//$is_out_of_stock = $product->get_stock_status() ==='outofstock';
-
-
+    
 $crosssell_ids = get_post_meta( get_the_ID(), '_crosssell_ids' );
 
 if( !empty ($crosssell_ids) ){
