9827ce34a3685baf5598dbddfe8e2e3196dfd2db
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-16 15:57:33 +0300

    peresadka

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -41,7 +41,7 @@ else {
 		$url = $product->add_to_cart_url();
 		$text = $product->get_title();
 		$class = isset( $args['class'] ) ? $args['class'] : 'button';
-		echo 'hihi'
+		echo 'hihi';
 	} 
 	else {
 		$url = $product->add_to_cart_url();
