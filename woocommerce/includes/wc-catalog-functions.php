dac85084a940e9da354fe05b7980fd7fda1e8b39
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-30 16:10:00 +0300

    metki

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -510,7 +510,8 @@ add_filter('BeRocket_AAPF_template_full_element_content', 'some_custom_berocket_
 function some_custom_berocket_aapf_template_full_content($template_content) {
 	if ($template_content['template']['attributes']['data-name']==='Подборки') {
 
-		$template_content['template']['content']['filter']['attributes']['class'] = .'metki_swiper_wrap';
+		array_push($template_content['template']['content']['filter']['attributes']['class'],'metki_swiper_wrap')
+		//$template_content['template']['content']['filter']['attributes']['class'] = 'metki_swiper_wrap';
 		
 		$template_content['template']['content']['filter']['content']['list']['attributes']['class'] = 'plnt_filter';
 
