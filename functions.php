40c47219b07ee425d24647ea4f1edb4235f44765
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-30 10:50:53 +0300

    metki

--- functions.php
+++ functions.php
@@ -87,7 +87,7 @@ function some_custom_berocket_aapf_template_full_content($template_content) {
 
 
 add_filter('BeRocket_AAPF_template_single_item', 'some_custom_berocket_aapf_template_single_item', 4000, 1);
-function some_custom_berocket_aapf_template_single_item($element, $term, $i, $berocket_query_var_title) {
+function some_custom_berocket_aapf_template_single_item($element) {
 	// $terms_content = array();
 	// foreach( $terms as $i => $term ) {
 	// 	$terms_content['template']['content']['filter']['content']['list']['element_'.$i]['attributes']['class'] = 'hohoho';
