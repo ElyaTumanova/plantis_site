1e80003207bd4185e9c5ccf03637ada331a11134
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-30 10:53:33 +0300

    metki

--- functions.php
+++ functions.php
@@ -88,11 +88,7 @@ function some_custom_berocket_aapf_template_full_content($template_content) {
 
 add_filter('BeRocket_AAPF_template_single_item', 'some_custom_berocket_aapf_template_single_item', 4000, 1);
 function some_custom_berocket_aapf_template_single_item($element) {
-	// $terms_content = array();
-	// foreach( $terms as $i => $term ) {
-	// 	$terms_content['template']['content']['filter']['content']['list']['element_'.$i]['attributes']['class'] = 'hohoho';
-	// }
-    // return $terms_content;
+	$element['attributes']['class'] = 'hohoho';
 	echo '<pre>';
 	print_r( $element );
 	echo '</pre>';
