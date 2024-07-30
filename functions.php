ff1efcabacc75cb555bce725777f0fda67cd4754
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-30 13:27:11 +0300

    metki

--- functions.php
+++ functions.php
@@ -91,16 +91,16 @@ function some_custom_berocket_aapf_template_full_content_element($template_conte
 	$elements = $template_content['template']['content']['filter']['content']['list']['content'];
 	foreach($elements as $element) {
 		$element['attributes']['class'] = 'hohoho';
-		// echo '<pre>';
-		// print_r( $element['attributes']);
-		// echo '</pre>';
-		return $elements;
+		echo '<pre>';
+		print_r( $element);
+		echo '</pre>';
+		// return $elements;
 	}
-	echo '<pre>';
-	print_r( $elements );
-	echo '</pre>';
-	$template_content['template']['content']['filter']['content']['list']['content'] = $elements;
-    return $template_content;
+	// echo '<pre>';
+	// print_r( $elements );
+	// echo '</pre>';
+	// $template_content['template']['content']['filter']['content']['list']['content'] = $elements;
+    // return $template_content;
 }
 
 
