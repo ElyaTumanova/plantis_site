5f8de86519721393307b0e19409fcbc5e7cad425
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-07-30 12:16:13 +0300

    metki

--- functions.php
+++ functions.php
@@ -89,12 +89,12 @@ add_filter('BeRocket_AAPF_template_full_content', 'some_custom_berocket_aapf_tem
 add_filter('BeRocket_AAPF_template_full_element_content', 'some_custom_berocket_aapf_template_full_content_element', 4000, 1);
 function some_custom_berocket_aapf_template_full_content_element($template_content) {
 	$elements = $template_content['template']['content']['filter']['content']['list']['content'];
-		echo '<pre>';
-		print_r( $elements );
-		echo '</pre>';
 	foreach($elements as $element) {
 		$element['attributes']['class'] = 'hohoho';
 	}
+	echo '<pre>';
+	print_r( $elements );
+	echo '</pre>';
 	$template_content['template']['content']['filter']['content']['list']['content'] = $elements;
     return $template_content;
 }
