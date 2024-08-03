2a7b637c3ad6a9de51c11654c931752540f72a00
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-03 15:23:21 +0300

    delivery date

--- assets/js/datepicker_init.js
+++ assets/js/datepicker_init.js
@@ -1,21 +1,27 @@
-var datepicker = new Datepicker('#datepicker', {
+jQuery(document).ready(function($){
+    'use strict';
+    // настройки по умолчанию. Их можно добавить в имеющийся js файл,
+    // если datepicker будет использоваться повсеместно на проекте и предполагается запускать его с разными настройками
+    $.datepicker.setDefaults({
+        closeText: 'Закрыть',
+        prevText: '<Пред',
+        nextText: 'След>',
+        currentText: 'Сегодня',
+        monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+        monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+        dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+        dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+        dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+        weekHeader: 'Нед',
+        dateFormat: 'dd-mm-yy',
+        firstDay: 1,
+        showAnim: 'slideDown',
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    } );
 
-    min: (function(){
-      var date = new Date();
-      date.setDate(date.getDate());
-      date_string = toValue([date]);
-      console.log(date_string);
-      return date;
-    })(),
-
-    // 30 days in the future
-    max: (function(){
-      var date = new Date();
-      date.setDate(date.getDate() + 30);
-      return date;
-    })(),
-
-    openOn: "today",
-
-    // without: [deserialize('05.08.2024')]
-  });
+    // Инициализация
+    $('input[id*="datepicker"], .datepicker').datepicker({ dateFormat: 'dd/mm/yy' });
+    
+});
