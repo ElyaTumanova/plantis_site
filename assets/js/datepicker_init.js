309667faaa1211106b0a9779e0fe7c3af450fa0c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-03 12:36:42 +0300

    delivery date

--- assets/js/datepicker_init.js
+++ assets/js/datepicker_init.js
@@ -1,1 +1,15 @@
-var datepicker = new Datepicker('#datepicker');
+var datepicker = new Datepicker('#datepicker', {
+
+    min: (function(){
+      var date = new Date();
+      date.setDate(date.getDate());
+      return date;
+    })(),
+
+    // 30 days in the future
+    max: (function(){
+      var date = new Date();
+      date.setDate(date.getDate() + 30);
+      return date;
+    })()
+  });
