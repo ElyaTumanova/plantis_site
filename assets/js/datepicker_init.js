56c8c588cfbd6dd579db05dbc9bd9f7a1cf634e2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-03 18:10:53 +0300

    delivery date

--- assets/js/datepicker_init.js
+++ assets/js/datepicker_init.js
@@ -1,22 +1,38 @@
-var datepicker = new Datepicker('#datepicker', {
+jQuery(function($){
 
-    min: (function(){
-      var date = new Date();
-      date.setDate(date.getDate()-1);
-      return date;
-    })(),
+    var ism = 'input[name^="shipping_method"]', ismc = ism+':checked',;
 
-    // 30 days in the future
-    max: (function(){
-      var date = new Date();
-      date.setDate(date.getDate() + 30);
-      return date;
-    })(),
+    $( 'form.checkout' ).on( 'change', ism, function() {
+        // if( $(ismc).val() == urgentPickup1 || $(ismc).val() == urgentPickup2 || $(ismc).val() == urgentPickup3 || $(ismc).val() == urgentPickup4) // Chosen "Urgent pickup" (Hiding "Date")
+        // {
+        //     if (deliveryDate) {deliveryDate.classList.add('d-none'); deliveryDate.style.display='none'};
+        // } else {
+        // 	if (deliveryDate) {deliveryDate.classList.remove('d-none'); deliveryDate.style.display='block'};
+        // };
 
-    openOn: "today",
+        var datepicker = new Datepicker('#datepicker', {
 
-    without: [(function(){
-        var date = new Date(2024,7,5);
-        return date;
-      })(),]
-  });
+            min: (function(){
+              var date = new Date();
+              date.setDate(date.getDate()-1);
+              return date;
+            })(),
+        
+            // 30 days in the future
+            max: (function(){
+              var date = new Date();
+              date.setDate(date.getDate() + 30);
+              return date;
+            })(),
+        
+            openOn: "today",
+        
+            without: [(function(){
+                var date = new Date(2024,7,5);
+                return date;
+              })(),]
+          });
+    })
+
+})
+
