0581a6f365466f8a90d814625d4d86732f17afd0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-04 17:44:06 +0300

    delivery date

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -199,7 +199,7 @@ function new_custom_checkout_field_script() {
                 } else {
 					if (deliveryDate) {deliveryDate.classList.remove('d-none'); deliveryDate.style.display='block'};
 				}
-            }, 100);
+            }, 500);
 			
 			setTimeout(function(){
                      if( $(ismc).val() == localPickup || $(ismc).val() == urgentPickup1 || $(ismc).val() == urgentPickup2 || $(ismc).val() == urgentPickup3 || $(ismc).val() == urgentPickup4) // Chosen "Local pickup or Urgent pickup" (Hiding "Interval")
@@ -208,7 +208,7 @@ function new_custom_checkout_field_script() {
                 } else {
 					if (deliveryInterval) {deliveryInterval.classList.remove('d-none'); deliveryInterval.style.display='block'};
 				}
-            }, 100);
+            }, 500);
 
             setTimeout(function(){
                      if( $(payment_method_checked).val() == payment_inn_chekbox) // Chosen "INN payment" (Show "INN")
@@ -232,9 +232,9 @@ function new_custom_checkout_field_script() {
             $( 'form.checkout' ).on( 'change', ism, function() {
 				if( $(ismc).val() == urgentPickup1 || $(ismc).val() == urgentPickup2 || $(ismc).val() == urgentPickup3 || $(ismc).val() == urgentPickup4) // Chosen "Urgent pickup" (Hiding "Date")
                 {
-                    if (deliveryDate) {deliveryDate.classList.add('d-none')};
+                    if (deliveryDate) {deliveryDate.classList.add('d-none'); deliveryDate.style.display='none'};
                 } else {
-					if (deliveryDate) {deliveryDate.classList.remove('d-none')};
+					if (deliveryDate) {deliveryDate.classList.remove('d-none'); deliveryDate.style.display='block'};
 				};
 
                 // setTimeout(function(){
@@ -250,9 +250,9 @@ function new_custom_checkout_field_script() {
 			 $( 'form.checkout' ).on( 'change', ism, function() {
 				if( $(ismc).val() == localPickup || $(ismc).val() == urgentPickup1 || $(ismc).val() == urgentPickup2 || $(ismc).val() == urgentPickup3 || $(ismc).val() == urgentPickup4) // Chosen "Local pickup or Urgent pickup" (Hiding "Interval")
                 {
-					if (deliveryInterval) {deliveryInterval.classList.add('d-none')};
+					if (deliveryInterval) {deliveryInterval.classList.add('d-none'); deliveryInterval.style.display='none'};
                 } else {
-					if (deliveryInterval) {deliveryInterval.classList.remove('d-none')};
+					if (deliveryInterval) {deliveryInterval.classList.remove('d-none'); deliveryInterval.style.display='block'};
 				}
                 
                 // setTimeout(function(){
