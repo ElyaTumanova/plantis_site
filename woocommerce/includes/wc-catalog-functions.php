613e7f20804753437f0434ff05652b47d75e44f5
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-13 22:02:41 +0300

    metki

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -96,7 +96,7 @@ function plnt_catalog_sidebar() {
 
 // // вывод фильтров над каталогом  #filters #berocket
 add_action('woocommerce_before_shop_loop','plnt_catalog_filters_main_area', 20);
-//add_action('woocommerce_before_product_loop_end','plnt_filter_metki_swiper_init', 20);
+add_action('woocommerce_before_product_loop_end','plnt_filter_metki_swiper_init', 20);
 
 function plnt_catalog_filters_main_area() {
 	global $filter_icon;
@@ -122,28 +122,32 @@ function plnt_catalog_filters_main_area() {
 function plnt_filter_metki_swiper_init () {
 	?>
 	<script>
-		swiper_filter_metki = new Swiper('.metki_swiper_wrap', {
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-			slidesPerView: 'auto',
-			spaceBetween: 5,
-			loop: false,
-			breakpoints: {
-				320: {
-				navigation: {
-					enabled: false,
-				},
-				},
-				767: {
-				navigation: {
-					enabled: true,
-				},
-				}
-			}
-		});
-		console.log('hihihh')
+		jQuery(function($){
+			setTimeout(function(){
+				swiper_filter_metki = new Swiper('.metki_swiper_wrap', {
+					navigation: {
+						nextEl: '.swiper-button-next',
+						prevEl: '.swiper-button-prev',
+					},
+					slidesPerView: 'auto',
+					spaceBetween: 5,
+					loop: false,
+					breakpoints: {
+						320: {
+						navigation: {
+							enabled: false,
+						},
+						},
+						767: {
+						navigation: {
+							enabled: true,
+						},
+						}
+					}
+				});
+				console.log('hihihh');
+			},100)
+		})
 	</script>
 	<?php
 }
@@ -256,30 +260,29 @@ function plnt_img_gallery_swiper_init() {
 				}
 			}
 		});
-		console.log('hohohoh')
 
-		swiper_filter_metki = new Swiper('.metki_swiper_wrap', {
-			navigation: {
-				nextEl: '.swiper-button-next',
-				prevEl: '.swiper-button-prev',
-			},
-			slidesPerView: 'auto',
-			spaceBetween: 5,
-			loop: false,
-			breakpoints: {
-				320: {
-				navigation: {
-					enabled: false,
-				},
-				},
-				767: {
-				navigation: {
-					enabled: true,
-				},
-				}
-			}
-		});
-		console.log(swiper_filter_metki);
+		// swiper_filter_metki = new Swiper('.metki_swiper_wrap', {
+		// 	navigation: {
+		// 		nextEl: '.swiper-button-next',
+		// 		prevEl: '.swiper-button-prev',
+		// 	},
+		// 	slidesPerView: 'auto',
+		// 	spaceBetween: 5,
+		// 	loop: false,
+		// 	breakpoints: {
+		// 		320: {
+		// 		navigation: {
+		// 			enabled: false,
+		// 		},
+		// 		},
+		// 		767: {
+		// 		navigation: {
+		// 			enabled: true,
+		// 		},
+		// 		}
+		// 	}
+		// });
+		// console.log(swiper_filter_metki);
 	</script>
 	<?php
 }
