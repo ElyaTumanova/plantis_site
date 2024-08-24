ff355833a466a5e9c8fd56894294c2c21d51e0df
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-24 12:34:29 +0300

    search

--- woocommerce/loop/no-products-found.php
+++ woocommerce/loop/no-products-found.php
@@ -22,7 +22,7 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <div class="woocommerce-no-products-found">
 	<?php wc_print_notice( esc_html__( 'No products were found matching your selection.', 'woocommerce' ), 'notice' ); 
-    plnt_cart_popular(); //added
+  //  plnt_cart_popular(); //added
     ?>
     
 </div>
