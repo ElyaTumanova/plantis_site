19911b55e94114c55589952d107f3c16455ead03
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-08-24 12:25:51 +0300

    search

--- functions.php
+++ functions.php
@@ -83,7 +83,7 @@ add_action( 'wp_footer', 'plnt_check_page' );
 add_filter('posts_search', function( $search, \WP_Query $q ) {
     if (!is_admin() && empty($search) && $q->is_search() && $q->is_main_query())
         $search .=" AND 0=1 ";
-
+		echo ' не нашлось';
     return $search;
 }, 10, 2);
 
