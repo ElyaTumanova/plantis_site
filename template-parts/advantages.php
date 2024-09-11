c49c070e11787c6276a1eb5e82bb9daa5745878f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-11 17:13:47 +0300

    catalog

--- template-parts/advantages.php
+++ template-parts/advantages.php
@@ -6,9 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="advantages">
   <div class="advantages__wrap">
-      <img width="111" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/фото_зел.svg" class="advantages__image" alt="" loading="lazy">														
-      <div class="advantages__title">“Живые” фотографии</div>	
-      <div class="advantages__descr">Перед оплатой мы пришлём вам фотографии именно того растения, которое поедет к вам домой</div>
+    <img width="111" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/фото_зел.svg" class="advantages__image" alt="" loading="lazy">														
+    <div class="advantages__title">“Живые” фотографии</div>	
+    <div class="advantages__descr">Перед оплатой мы пришлём вам фотографии именно того растения, которое поедет к вам домой</div>
   </div>
   <div class="advantages__wrap">
     <img width="110" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/пересадка_зел.svg" class="advantages__image" alt="" loading="lazy">
@@ -18,7 +18,8 @@ if ( ! defined( 'ABSPATH' ) ) {
   <div class="advantages__wrap">									
     <img width="115" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/доставка_зел.svg" class="advantages__image" alt="" loading="lazy">														
     <div class="advantages__title">Бережная доставка</div>
-    <div class="advantages__descr">Осуществляем доставку комнатных растений до двери. Если сумма покупки будет выше 30000 рублей, то делаем это бесплатно</div>	
+    <?php $min_free_delivery = carbon_get_theme_option('min_free_delivery');?>
+    <div class="advantages__descr">Осуществляем доставку комнатных растений до двери. <?php if($min_free_delivery) { echo 'Если сумма покупки будет выше '.$min_free_delivery.' рублей, то делаем это бесплатно';}?></div>	
   </div>
   <div class="advantages__wrap">		
     <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="advantages__image" alt="" loading="lazy">												
