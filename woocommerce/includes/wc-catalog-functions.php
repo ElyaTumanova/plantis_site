e38e06b47c7c6f5916b1fd37ec5c17627c3213cf
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-11 17:03:57 +0300

    catalog

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -10,13 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // // убираем лишнее
 //remove_action('woocommerce_archive_description','woocommerce_taxonomy_archive_description',10);
-add_action('woocommerce_after_shop_loop','plnt_woocommerce_taxonomy_archive_description',15);
-
-function plnt_woocommerce_taxonomy_archive_description() {
-	if ( is_paged() ) {
-		woocommerce_taxonomy_archive_description();
-	}
-}
+add_action('woocommerce_after_shop_loop','woocommerce_taxonomy_archive_description',15);
 
 remove_action('woocommerce_before_shop_loop','woocommerce_result_count', 20);
 remove_action('woocommerce_before_shop_loop','woocommerce_output_all_notices', 10);
