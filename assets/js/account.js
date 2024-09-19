b2413f9340a5df11ae18d02f169c62bdcf7e94a6
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-19 18:08:57 +0300

    account

--- assets/js/account.js
+++ assets/js/account.js
@@ -8,15 +8,21 @@ if (customerLoginForm) {
     const registerHeader = customerLoginForm.querySelector ('.col-2').querySelector('h2');
 
     registerForm.classList.add("d-none");
+    loginHeader.classList.add("h2_opened");
+
 
     loginHeader.addEventListener('click', function (e) {
         registerForm.classList.add("d-none");
         loginForm.classList.remove("d-none");
+        loginHeader.classList.add("h2_opened");
+        registerHeader.classList.remove("h2_opened");
     })
 
     registerHeader.addEventListener('click', function (e) {
         registerForm.classList.remove("d-none");
         loginForm.classList.add("d-none");
+        registerHeader.classList.add("h2_opened");
+        loginHeader.classList.remove("h2_opened");
     })
 
     console.log(loginForm);
