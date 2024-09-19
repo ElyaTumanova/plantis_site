c2a43d7fa841afaf1aa55a5110c4c0bffb6f2814
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-19 21:39:24 +0300

    account

--- footer.php
+++ footer.php
@@ -51,6 +51,7 @@
 <?php get_template_part('template-parts/popups/search-popup');?>
 <?php get_template_part('template-parts/popups/burger-menu');?>
 <?php get_template_part('template-parts/popups/catalog-menu');?>
+<?php get_template_part('template-parts/popups/login-form');?>
 
 <?php wp_footer(); ?>
 
---
+++ template-parts/popups/login-form.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
+<div class="plnt-customer-login modal-mob">
+    hello
+    <?php wp_login_form( $args ); ?>
+</div>
