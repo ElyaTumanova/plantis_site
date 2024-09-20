c7b1f453d1a15f8aff4b708859e48759a7894b10
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-20 13:07:18 +0300

    account

--- template-parts/popups/login-form.php
+++ template-parts/popups/login-form.php
@@ -6,7 +6,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="login-popup popup">
     <div class="plnt-customer-login page-popup__containe">
-        <?php wp_signon(); ?>
+        <?php wc_get_template(
+					'auth/form-login.php',
+					array(
+						'app_name'     => wc_clean( $data['app_name'] ),
+						'return_url'   => add_query_arg(
+							array(
+								'success' => 0,
+								'user_id' => wc_clean( $data['user_id'] ),
+							),
+							$this->get_formatted_url( $data['return_url'] )
+						),
+						'redirect_url' => $this->build_url( $data, 'authorize' ),
+					)
+				); ?>
         <div class="login__close">✖</div>
     </div>
     <div class="login__popup-overlay popup-overlay"></div>
