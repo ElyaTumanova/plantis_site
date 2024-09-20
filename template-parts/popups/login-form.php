d14408c327d9908cb344eb49125b234c361720ba
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-20 13:24:49 +0300

    account

--- template-parts/popups/login-form.php
+++ template-parts/popups/login-form.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="login-popup popup">
-    <div class="plnt-customer-login page-popup__containe">
+    <div class="plnt-customer-login page-popup__container">
         <?php wc_get_template('myaccount/form-login.php'); ?>
         <div class="login__close">✖</div>
     </div>
