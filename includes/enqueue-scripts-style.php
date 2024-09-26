8cea7d9798499a61f01b7d8504ee80e27d23e5fe
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-09-26 19:47:17 +0300

    account

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -35,10 +35,10 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 		wp_enqueue_script( 'account', get_template_directory_uri() .
 											 '/assets/js/account.js', array( 'jquery' ), null, true );	
 
-		wp_enqueue_script( 'login-popup.js', get_template_directory_uri() .
+		wp_enqueue_script( 'login-popup', get_template_directory_uri() .
 											 '/assets/js/login-popup.js', array( 'jquery' ), null, true );
 
-		wp_enqueue_script( 'login-popup.js', get_template_directory_uri() .
+		wp_enqueue_script( 'register-popup', get_template_directory_uri() .
 											 '/assets/js/register-popup.js', array( 'jquery' ), null, true );	
 											 
 		wp_enqueue_script( 'slider-init', get_template_directory_uri() .
