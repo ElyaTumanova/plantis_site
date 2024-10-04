fac689f109facef6262e065b702950d5fdd39c99
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-04 22:44:35 +0300

    account

--- woocommerce/order/order-details.php
+++ woocommerce/order/order-details.php
@@ -90,13 +90,11 @@ if ( $show_downloads ) {
         <div class="plnt-order__totals">
             <?php
             foreach ( $order->get_order_item_totals() as $key => $total ) {
-                echo '<pre>';
-                print_r( $total );
-                echo '</pre>';
                 if ($total['label']==='Подытог:') {
                     ?>
-                    <div>lalal
-                        
+                    <div>
+                        <div scope="row">Товары</div>
+                        <div><?php echo wp_kses_post( $total['value'] ); ?></div>
                     </div>
                     <?php
                 } else {
