4f9bbd9db19b08f574fce0f09274e2f581ed1220
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-21 21:21:34 +0300

    account

--- woocommerce/includes/wc-account-functions.php
+++ woocommerce/includes/wc-account-functions.php
@@ -9,7 +9,7 @@ add_filter ( 'woocommerce_account_menu_items', 'misha_remove_my_account_links' )
 function misha_remove_my_account_links( $menu_links ){
  
 	unset( $menu_links['edit-address'] ); // Addresses
-	unset( $menu_links['dashboard'] ); // Dashboard
+	//unset( $menu_links['dashboard'] ); // Dashboard
 	//unset( $menu_links['payment-methods'] ); // Payment Methods
 	//unset( $menu_links['orders'] ); // Orders
 	//unset( $menu_links['downloads'] ); // Downloads
