e187dd776b8de4d3e6dd72f99dfe94356b095e62
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-24 21:20:12 +0300

    datepicker

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -359,6 +359,7 @@ function new_custom_checkout_field_script() {
                 } 
                 
                 console.log(weekend);
+                console.log(new Date ((new Date(selectedDate)).setHours(0,0,0,0)));
                 console.log((new Date(selectedDate)).setHours(0,0,0,0));
                 const first5 = weekend.find(isMyFavoriteHobby);
 
