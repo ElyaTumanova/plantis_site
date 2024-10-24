b5480d5bc394fbc67e30cd23eddbadb05d710e33
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-24 21:57:33 +0300

    datepicker

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -370,11 +370,11 @@ function new_custom_checkout_field_script() {
                 let isSelectedDayWeekend = false;
                 function checkSelectedDay (selectedDate) {
                     isSelectedDayWeekend = weekendTimeStamps.includes((new Date(selectedDate)).setHours(3,0,0,0));
-                    if (isSelectedDayWeekend) {
-                        selectedDate = date.setDate(new Date(selectedDate).getDate() + 1);
-                        console.log(selectedDate);
-                        checkSelectedDay (selectedDate);
-                    }
+                    // if (isSelectedDayWeekend) {
+                    //     selectedDate = date.setDate(new Date(selectedDate).getDate() + 1);
+                    //     console.log(selectedDate);
+                    //     checkSelectedDay (selectedDate);
+                    // }
                     return selectedDate;
                 };
                 
