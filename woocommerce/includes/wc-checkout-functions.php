43662aec0a47de314aee73b3e7b71265fd2233e7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-25 11:33:21 +0300

    datepicker

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -382,6 +382,16 @@ function new_custom_checkout_field_script() {
                 // console.log('finally');
                 // console.log(new Date(selectedDate));
 
+                //кнопка ОК
+                let button = {
+                    content: 'OK',
+                    className: 'custom-button-classname',
+                    onClick: (datepicker) => {
+                        console.log('ok');
+                        console.log(new Date(SelectedDate));
+                        datepicker.hide();
+                    }
+                }
                 
                 var datepicker = new AirDatepicker('#datepicker', {
                     selectedDates: selectedDate,
@@ -399,17 +409,6 @@ function new_custom_checkout_field_script() {
                 
                 if (weekend) {
                     datepicker.disableDate(weekend);
-                }
-
-                //кнопка ОК
-                let button = {
-                    content: 'OK',
-                    className: 'custom-button-classname',
-                    onClick: (datepicker) => {
-                        console.log('ok');
-                        console.log(new Date(SelectedDate));
-                        datepicker.hide();
-                    }
                 }
             }
 
