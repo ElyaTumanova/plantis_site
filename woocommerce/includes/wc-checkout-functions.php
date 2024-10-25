f66265c3f0c561198f063add9e65e3b2ee051a6e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-25 11:40:57 +0300

    datepicker

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -415,15 +415,13 @@ function new_custom_checkout_field_script() {
             // Datepicker init
             setTimeout(function(){
                 console.log('hi');
-                destroy();
                 datepicker_init ();
             }, 100);
 
-            $( 'form.checkout' ).on( 'change', ism, function() {
-                console.log('hello');
-                destroy();
-                datepicker_init ();
-            })
+            // $( 'form.checkout' ).on( 'change', ism, function() {
+            //     console.log('hello');
+            //     datepicker_init ();
+            // })
         });
     </script>
     <?php
