f7ddb182ac630633c2a2207a5912322e227d5b02
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-25 17:03:29 +0300

    account

--- footer.php
+++ footer.php
@@ -53,10 +53,10 @@
 <?php get_template_part('template-parts/popups/catalog-menu');?>
 
 <?php 
-if (!is_page('my-account')) {
+//if (!is_page('my-account')) {
     get_template_part('template-parts/popups/register-form');
     get_template_part('template-parts/popups/login-form');
-}
+//}
 ?>
 
 <?php wp_footer(); ?>
