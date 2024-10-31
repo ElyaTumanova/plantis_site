b362f8a2e5aa8b6c71e13320f50c6f8a6bb393e8
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-10-31 12:23:38 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -421,13 +421,11 @@ function new_custom_checkout_field_script() {
             setTimeout(function(){
                 var datePickerOpts = datepicker_init ();
                 datepickerCal.update(datePickerOpts);
-                console.log(datePickerOpts.selectedDates);
             }, 100);
 
             $( 'form.checkout' ).on( 'change', ism, function() {
                 var datePickerOpts = datepicker_init ();
                 datepickerCal.update(datePickerOpts);
-                console.log(datePickerOpts.selectedDates);
             })
         });
     </script>
