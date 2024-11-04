600e7f5581489e800cd6aac82ef0eb92ade8f374
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-04 10:33:03 +0300

    delivery

--- assets/js/delivery-dropdown.js
+++ assets/js/delivery-dropdown.js
@@ -2,7 +2,7 @@ const del_dropdown = document.querySelectorAll('.delivery__block');
 
 del_dropdown.forEach((el) => {
 	const dropdown = el.querySelector('.delivery__dropdown');
-	const header = el.querySelector('..delivery__header');
+	const header = el.querySelector('.delivery__header');
 	const btn = el.querySelector('.delivery__dropdown-arrow');
 	btn.addEventListener('click', function (event) {
 		dropdown.classList.toggle('delivery__dropdown_show');
