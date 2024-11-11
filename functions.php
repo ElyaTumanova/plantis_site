151cf6433d82396622d5e8ff9ef3b52edcbad53c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-11 16:01:30 +0300

    api dev

--- functions.php
+++ functions.php
@@ -81,15 +81,6 @@ function plnt_check_page() {
 //add_action( 'wp_footer', 'plnt_check_page' );
 
 
-//add_action( 'woocommerce_product_set_stock', 'stock_changed' );
-//add_action( 'woocommerce_variation_set_stock', 'stock_changed' );
-function stock_changed( $product ) {
-    echo '<pre>';
-	print_r( "hi" );
-	echo '</pre>';
-}
-
-
 
 
 
