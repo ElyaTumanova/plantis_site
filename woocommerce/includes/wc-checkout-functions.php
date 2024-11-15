da69137895a4c235c59e8358fd8f7574a930be78
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-15 19:34:13 +0300

    datepicker

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -211,7 +211,7 @@ function new_custom_checkout_field_script() {
             function diasable_pickup_method () {
                 console.log(urgentPickup3);
                 let element = document.querySelector(`[value="${urgentPickup3}"]`);
-                //element.setAttribute("disabled", "");
+                element.setAttribute("disabled", "disabled");
                 //element.disabled = true;
                 console.log(element);
             }
