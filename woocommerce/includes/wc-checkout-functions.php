54ced4a6a97806ea696de21adcec41cfc17866b6
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-17 15:15:50 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -182,7 +182,7 @@ function new_custom_checkout_field_script() {
     $required_html = '<abbr class="required" title="' . $required_text . '">*</abbr>';
     ?>
     <script>
-        let shipingMethods = document.querySelector('.woocommerce-shipping-methods');
+        let shipingMethods = document.querySelectorAll('.shipping_method');
     
 		let deliveryDate = document.querySelector('#datepicker_field');
 		let deliveryInterval = document.querySelector('#additional_delivery_interval_field');
@@ -225,7 +225,9 @@ function new_custom_checkout_field_script() {
         plnt_hide_checkout_fields();
         
 
-        shipingMethods.addEventListener('change', plnt_hide_checkout_fields);
+        shipingMethods.forEach(element => {
+            element.addEventListener('change', plnt_hide_checkout_fields);
+        });
         
 
         jQuery(function($){
