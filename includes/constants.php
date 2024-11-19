d7cf4464cc4ba921db59a2bae433d755b710cfef
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-19 15:56:41 +0300

    checkout

--- includes/constants.php
+++ includes/constants.php
@@ -136,11 +136,10 @@ if($site === 'http://new.plantis.shop') {
 	$urgent_delivery_inMKAD_small = 'flat_rate:11'; 
 	$urgent_delivery_outMKAD_small = 'flat_rate:12';
 
-	// TO BE UPDATED
-	$urgent_delivery_inMKAD_late = 'flat_rate:19'; 
-	$urgent_delivery_outMKAD_late = 'flat_rate:20'; 
-	$urgent_delivery_inMKAD_small_late = 'flat_rate:21'; 
-	$urgent_delivery_outMKAD_small_late = 'flat_rate:22';
+	$urgent_delivery_inMKAD_late = 'flat_rate:13'; 
+	$urgent_delivery_outMKAD_late = 'flat_rate:14'; 
+	$urgent_delivery_inMKAD_small_late = 'flat_rate:15'; 
+	$urgent_delivery_outMKAD_small_late = 'flat_rate:16';
 	
 	$delivery_free = 'free_shipping:4';
 
