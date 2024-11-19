fd8408c5b5e9aed44f78196bbcaed2b2745d13cd
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-19 15:10:22 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -477,7 +477,7 @@ function new_truemisha_remove_shipping_on_hour( $rates, $package ) {
  
 	date_default_timezone_set('Europe/Moscow');
 	//echo date('H');
-	if ( date('H') < 18 ) {
+	if ( date('H') < 15 ) {
 		unset( $rates[ $urgent_delivery_inMKAD_late ] );
 		unset( $rates[ $urgent_delivery_outMKAD_late ] );		
 		unset( $rates[ $urgent_delivery_inMKAD_small_late ] );		
