cf71fef481bee24e06985d3fbee30ace44d7867f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-19 16:15:09 +0300

    treez plants

--- template-parts/card-banners.php
+++ template-parts/card-banners.php
@@ -13,6 +13,6 @@ $min_treez_delivery = carbon_get_theme_option('min_treez_delivery');
     <p>При покупке <a href="<?php echo get_site_url()?>/product-category/gorshki_i_kashpo/" target="_blank" rel="noopener">горшка </a>— пересадка в подарок!</p>
 </div>
 <div class="card_banner" id="card_banner_treez">
-    <p> Минимальная сумма заказа для кашпо Treez <span><?php echo $min_treez_delivery?></span> рублей (без учета стоимости других товаров)</p>
+    <p> Минимальная сумма заказа для кашпо Treez и/или искусственных растений Treez<span><?php echo $min_treez_delivery?></span> рублей (без учета стоимости других товаров)</p>
     <p> Оплатить заказ с кашпо Treez можно будет после подтверждения наличия кашпо. Наш менеджер свяжется с Вами после оформления заказа.</p>
 </div>
