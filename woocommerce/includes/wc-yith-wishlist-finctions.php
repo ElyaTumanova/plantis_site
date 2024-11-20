c1aac819ff3e99cf6bf81f4c2869cade0f7707b1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-20 18:19:51 +0300

    wish

--- woocommerce/includes/wc-yith-wishlist-finctions.php
+++ woocommerce/includes/wc-yith-wishlist-finctions.php
@@ -128,7 +128,7 @@ function plnt_get_wishlist_script() {
     let wishBtns = document.querySelectorAll('.yith-wcwl-add-button .add_to_wishlist');
     
     wishBtns.forEach(button => {
-    //console.log(button.dataset.product_id);
+    console.log(button.dataset.product_id);
     if(wishListItems.includes(button.dataset.product_id)) {
       console.log(button);
       button.setAttribute('href', `?remove_from_wishlist=${button.dataset.product_id}`);
