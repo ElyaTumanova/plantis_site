c4e0a20e7fcbeaaa46b411cd9603ffa4229bce20
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-22 18:31:25 +0300

    header cart mob

--- footer.php
+++ footer.php
@@ -37,7 +37,8 @@
     </button>
     <div class="side-cart__popup popup">
         <div class="side-cart">
-            <h4 class="side-cart__title heading-2">Корзина</h4>      
+            <h4 class="side-cart__title heading-2">Корзина</h4>
+            <div class="modal-mob__close burger-menu__close button">&#10006;</div>      
             <?php plnt_woocommerce_mini_cart();?> 
         </div>
         <div class="side-cart__popup-overlay popup-overlay"></div>
