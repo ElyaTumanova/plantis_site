7f615de4295a132085d21cfcc49932487618c0ca
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-23 11:13:20 +0300

    checkout

--- functions.php
+++ functions.php
@@ -97,9 +97,11 @@ function plnt_check_page() {
  add_action( 'wp_footer', 'plnt_get_checkout_fields' );
 
 function plnt_get_checkout_fields() {
-	$order = wc_get_order( $the_order );
+	$order = WC()->session->get('order_awaiting_pay');
+	$order = new WC_Order($order);
+	$first_name = $order->get_meta('billing_first_name');
 	echo '<pre>';
-	print_r( $order );
+	print_r( $first_name );
 	echo '</pre>';
 }
 
