83ef37ba0f01282dbbe99a3516a9325331d7a326
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-23 10:55:57 +0300

    checkout

--- functions.php
+++ functions.php
@@ -81,3 +81,15 @@ function plnt_check_page() {
 }
 
 //add_action( 'wp_footer', 'plnt_check_page' );
+
+
+
+add_action( 'woocommerce_checkout_update_order_review', 'refresh_shipping_methods', 10, 1 );
+function refresh_shipping_methods( $post_data ){
+	echo '<pre>';
+	print_r( $post_data );
+	echo '</pre>';
+
+    
+    WC()->cart->calculate_shipping();
+}
