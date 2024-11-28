fb356c7d7202daf1e2744609f12b421fde51357e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-28 11:22:25 +0300

    delivery

--- assets/js/ajax-urgent-delivery.js
+++ assets/js/ajax-urgent-delivery.js
@@ -2,7 +2,7 @@ let isUrgent = '0';
 
 function plntChekUrgentDelivery() {
   let deliveryDates = document.querySelectorAll('.delivery_dates input');
-  console.log(deliveryDates.value);
+  console.log(deliveryDates[0]);
   deliveryDates.forEach((date) => {
     date.addEventListener('click', function(event){
       console.log(event.target.value);
