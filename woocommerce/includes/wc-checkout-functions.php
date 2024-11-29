eedd0510eb7e1a784f6c95385de9d84c2fff2068
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-29 18:52:40 +0300

    delivery

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -73,7 +73,7 @@ function plnt_add_delivery_interval_field() {
 
 // FOR DEV
 
-add_action( 'woocommerce_review_order_after_shipping', 'plnt_checkout_before_order_total_callback', 10 );
+add_action( 'woocommerce_review_order_before_order_total', 'plnt_checkout_before_order_total_callback', 10 );
 
 function plnt_checkout_before_order_total_callback() {
     echo 'here!';
