89dca815b04e7dc1a16fa65b203c3c9ef546ad03
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-11-29 16:50:53 +0300

    delivery

--- assets/js/slider-init.js
+++ assets/js/slider-init.js
@@ -282,7 +282,7 @@ console.log(deliveryWrapper);
 
 const swiper_delivery_dates = new Swiper('#delivery_dates_field', {
 
-    slidesPerView: 'auto',
+    slidesPerView: 6,
     slidesPerGroup: 1,
     spaceBetween: 0,
     //loop: false,
