aa454c56ce3e527c4930ddb9e35a15c8735a7f3f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-01 13:31:49 +0300

    delivery

--- woocommerce/checkout/review-order.php
+++ woocommerce/checkout/review-order.php
@@ -109,8 +109,7 @@ defined( 'ABSPATH' ) || exit;
 		<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
 
 		<tr class="plnt_before-order-total">
-			<th>Delivery</th>
-			<td><?php do_action( 'plnt_woocommerce_review_order_in_order_total' ); ?></td>
+			<td colspan="2"><?php do_action( 'plnt_woocommerce_review_order_in_order_total' ); ?></td>
 		</tr>
 
 		<tr class="order-total">
