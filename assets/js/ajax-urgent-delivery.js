a6d9835f61fb9ee9f5147ee68481cc395fd96eb9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-01 15:30:56 +0300

    delivery

--- assets/js/ajax-urgent-delivery.js
+++ assets/js/ajax-urgent-delivery.js
@@ -113,7 +113,7 @@ function plntAjaxGetUrgent() {
                   // Trigger refresh checkout
                   $('body').trigger('update_checkout');
                   //console.log(isUrgent);
-                  plntAjaxGetTotal();
+                  // plntAjaxGetTotal();
               }
           });
     });
