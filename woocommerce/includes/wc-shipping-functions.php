fdf6c5da43da65f95b647e16d2952a0efc7ecaa3
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-01 12:37:09 +0300

    delivery

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -36,8 +36,6 @@ function plnt_refresh_shipping_methods_for_urgent( $post_data ){
         WC()->session->set( 'shipping_for_package_' . $package_key, $bool );
     }
     WC()->cart->calculate_shipping();
-
-    wc_cart_totals_order_total_html();
 }
 
 add_filter( 'woocommerce_package_rates', 'plnt_shipping_rates_for_urgent', 100, 2 );
