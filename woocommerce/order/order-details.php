9456c86a840d60f920374749557ff2091d9ab040
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-01 14:09:47 +0300

    delivery

--- woocommerce/order/order-details.php
+++ woocommerce/order/order-details.php
@@ -98,18 +98,14 @@ if ( $show_downloads ) {
                     <div class='plnt-order__totals-label' scope="row">Товары:</div>
                     <div class='plnt-order__totals-value'><?php echo wp_kses_post( $plnt_order_totals['cart_subtotal']['value'] ); ?></div>
                 </div>
-                <?php if( $plnt_order_totals['shipping']['value'] ) : ?>
                 <div class='plnt-order__totals-row'>
                     <div class='plnt-order__totals-label' scope="row">Доставка:</div>
                     <div class='plnt-order__totals-value'><?php echo wp_kses_post( $plnt_order_totals['shipping']['value'] ); ?></div>
                 </div>
-                <?php endif; ?>
-                <?php if( $plnt_order_totals['order_total']['value'] ) : ?>
                 <div class='plnt-order__totals-row'>
                     <div class='plnt-order__totals-label' scope="row">Итого:</div>
                     <div class='plnt-order__totals-value plnt-order__totals-value_total'><?php echo wp_kses_post( $plnt_order_totals['order_total']['value'] ); ?></div>
-                </div>
-                <?php endif; ?>                
+                </div>               
                 <?php
            // foreach ( $order->get_order_item_totals() as $key => $total ) {
                ?>
@@ -136,14 +132,18 @@ if ( $show_downloads ) {
  */
 do_action( 'woocommerce_after_order_details', $order );
 ?>
+<?php if( $plnt_order_totals['delivery_dates']['value'] ) : ?>
 <div class='plnt-order__delivery-row'>
     <div class='plnt-order__delivery-label' scope="row">Дата доставки (самовывоза):</div>
     <div class='plnt-order__delivery-value'><?php echo wp_kses_post( $plnt_order_totals['delivery_dates']['value'] ); ?></div>
 </div>
+<?php endif; ?> 
+<?php if( $plnt_order_totals['additional_delivery_interval']['value'] ) : ?>
 <div class='plnt-order__delivery-row'>
     <div class='plnt-order__delivery-label' scope="row">Интервал доставки:</div>
     <div class='plnt-order__delivery-value'><?php echo wp_kses_post( $plnt_order_totals['additional_delivery_interval']['value'] ); ?></div>
 </div>
+<?php endif; ?> 
 <?php
 if ( $show_customer_details ) {
 	wc_get_template( 'order/order-details-customer.php', array( 'order' => $order ) );
