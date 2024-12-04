b76b8f45db1e9b2cb74e2eaf56e180bbe639aace
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-04 17:54:31 +0300

    delivery

--- includes/constants.php
+++ includes/constants.php
@@ -159,5 +159,3 @@ if($site === 'http://new.plantis.shop') {
 /* Изображения и иконки */
 
 $filter_icon = "https://plantis.shop/wp-content/uploads/2024/07/filter_new.svg";
-
-$in_mkad = carbon_get_theme_option('in_mkad');
