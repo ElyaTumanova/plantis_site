e7966256acb37b4fdc8e933f46056cf3339e3ddc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-04 18:51:17 +0300

    delivery

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -53,3 +53,87 @@ function plnt_shipping_rates_for_urgent( $rates, $package ) {
 
     return $rates;
 }
+
+/* стоимость доставки в зависимости от суммы заказа*/
+
+//убираем способ онлайн-оплаты, если маленькая сумма заказа или далекая доставка
+add_filter( 'woocommerce_available_payment_gateways', 'plnt_disable_payment_small_order' );
+
+function plnt_disable_payment_small_order( $available_gateways ) {
+    $min_small_delivery = carbon_get_theme_option('min_small_delivery');
+    global $delivery_courier;
+    global $delivery_long_dist;
+
+    if( is_admin() ) {
+		return $available_gateways;
+	}
+
+    if( is_wc_endpoint_url( 'order-pay' ) ) {
+		return $available_gateways;
+	}
+
+    $chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
+
+    // стоимость товаров в корзине
+    if (WC()->cart->subtotal < $min_small_delivery && $delivery_courier == $chosen_methods[0]) {
+        unset( $available_gateways['tinkoff'] ); //to be updated - change to tinkoff
+    }
+
+    // дальняя доставка
+    if ( $delivery_long_dist == $chosen_methods[0]) {
+        unset( $available_gateways['tinkoff'] ); //to be updated - change to tinkoff
+    }
+
+    return $available_gateways;
+}
+	
+add_filter( 'woocommerce_package_rates', 'new_truemisha_remove_shipping_on_price', 25, 2 );
+ 
+function new_truemisha_remove_shipping_on_price( $rates, $package ) {
+
+    //переменные
+    global $local_pickup;
+        
+    global $delivery_inMKAD;
+    global $delivery_outMKAD;
+    global $delivery_courier;
+    global $delivery_long_dist;
+
+    $small_delivery_markup = carbon_get_theme_option('small_delivery_markup');
+    $min_small_delivery = carbon_get_theme_option('min_small_delivery');
+
+	// если сумма всех товаров в корзине меньше min_small_delivery, увеличиваем стоимость доставки
+    if ($small_delivery_markup) { //если наценка не задана, то будет запущен второй вариант алгоритма с отключением способов доставки
+        // отключаем опцию доставики по тарифам курьерской службы
+        unset( $rates[ $delivery_courier ] );
+        // стоимость товаров в корзине
+        if (WC()->cart->subtotal < $min_small_delivery) {
+           foreach( $rates as $rate) {
+            
+            if ( 'local_pickup' === $rate->method_id ) {
+                $rate->cost = $rate->cost;
+            }
+            else if ('free_shipping' === $rate->method_id) {
+                $rate->cost = $rate->cost ;
+            } else {
+                $rate->cost = $rate->cost + $small_delivery_markup;
+            }
+            
+           }
+        }
+    } else {
+        // если сумма всех товаров в корзине меньше min_small_delivery, отключаем способ доставки
+        
+            if ( WC()->cart->subtotal < $min_small_delivery ) {
+                unset( $rates[ $delivery_inMKAD ] );
+                unset( $rates[ $delivery_outMKAD ] );
+                unset( $rates[ $delivery_long_dist ] );
+                isset($rates[ $delivery_courier ]);		
+            } 
+            else {
+                unset( $rates[ $delivery_courier ] );
+            }
+    }
+ 
+	return $rates;
+}
