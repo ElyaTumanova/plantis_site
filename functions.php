1c1a880a20c37871db7753b04ca3bdbc5aacacda
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-05 18:06:54 +0300

    delivery

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ function plnt_set_constants_script() {
 	<?php
 }
 
-add_action( 'wp_footer', 'plnt_get_shiping_costs' );
+add_action( 'wp_head', 'plnt_get_shiping_costs' );
 
 function plnt_get_shiping_costs() {
     $shipping_costs = [];
