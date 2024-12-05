0c151e1876689b5186ae7af1fee778b956cb03a7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-05 12:20:13 +0300

    delivery

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -147,6 +147,8 @@ function new_truemisha_remove_shipping_on_price( $rates, $package ) {
                 unset( $rates[ $delivery_outMKAD_small ] );
                 unset( $rates[ $urgent_delivery_inMKAD_small ] );
                 unset( $rates[ $urgent_delivery_outMKAD_small ] );
+                unset( $rates[ $delivery_inMKAD_large ] );
+                unset( $rates[ $delivery_outMKAD_large ] );
             }
     }
  
