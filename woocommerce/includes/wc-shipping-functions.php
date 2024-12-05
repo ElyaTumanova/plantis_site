7632bf1bdd317398b8e80ee2f7c1375f9ee709f4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-05 16:54:02 +0300

    delivery

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -180,7 +180,7 @@ function plnt_get_shiping_costs() {
  
 			if( $shipping_methods ) {
 				foreach ( $shipping_methods as $shipping_method_id => $shipping_method ) {
-                    echo $shipping_method;
+                    print_r($shipping_method_id);
                     array_push($shipping_costs, array($shipping_method->id => $shipping_method->cost));
 				}
 			}
