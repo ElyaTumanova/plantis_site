481f1596bd5e0a598da5599c3d9b06556b2a324d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-07 19:37:38 +0300

    delivery

--- assets/js/hide-chekout-fields.js
+++ assets/js/hide-chekout-fields.js
@@ -2,7 +2,7 @@
 
 //let deliveryDate = document.querySelector('#datepicker_field');
 let deliveryInterval = document.querySelector('#additional_delivery_interval_field');
-let deliveryIntervalInput = document.querySelector('input[name=additional_delivery_interval]');
+let deliveryIntervalInput = document.querySelectorAll('input[name=additional_delivery_interval]');
 console.log(deliveryIntervalInput);
 
 let addressFields = document.querySelector('#billing_address_1_field');
@@ -17,27 +17,18 @@ let innField = document.querySelector('#additional_inn');
 function plnt_hide_checkout_fields(event){
     
     //console.log('hi plnt_hide_checkout_fields');
-    //console.log(deliveryIntervalInput)
-    // if (event) {console.log(event)};
     if(event && event.target.className == "shipping_method") {
         // console.log(event);
         checkedShippingMethod = event.target.value;
     }
     
-    //TO BE DELETED
-    // if (urgentPickups.includes(checkedShippingMethod))  
-    // {
-    //     if (deliveryDate) {deliveryDate.classList.add('d-none')};
-    //     if (deliveryInterval) {deliveryInterval.classList.add('d-none')};
-    //     if (deliveryIntervalInput) {deliveryIntervalInput.checked = false};
-    //     if (addressFields) {addressFields.classList.remove('d-none');}
-    //     if (additionalAddress) {additionalAddress.classList.remove('d-none');}
-    // } else 
-
+  
     if ( checkedShippingMethod == localPickupId) {
-        //if (deliveryDate) {deliveryDate.classList.remove('d-none')};
         if (deliveryInterval) {deliveryInterval.classList.add('d-none')};
-        if (deliveryIntervalInput) {deliveryIntervalInput.checked = false};
+        if (deliveryIntervalInput) {
+            deliveryIntervalInput.forEach((input)=>{
+                input.checked = false;
+            })};
         if (addressFields) {addressFields.classList.add('d-none');}
         if (additionalAddress) {additionalAddress.classList.add('d-none');}
     } else {
@@ -45,13 +36,15 @@ function plnt_hide_checkout_fields(event){
         console.log(isUrgent);
         if (isUrgent == '1') {
             if (deliveryInterval) {deliveryInterval.classList.add('d-none')};
-            if (deliveryIntervalInput) {deliveryIntervalInput.checked = false};
+            if (deliveryIntervalInput) {
+                deliveryIntervalInput.forEach((input)=>{
+                    input.checked = false;
+                })};
         }
     
         if (isUrgent == '0') {
             if (deliveryInterval) {deliveryInterval.classList.remove('d-none')};
         }
-        //if (deliveryDate) {deliveryDate.classList.remove('d-none')};
         if (addressFields) {addressFields.classList.remove('d-none');}
         if (additionalAddress) {additionalAddress.classList.remove('d-none');}
     }
