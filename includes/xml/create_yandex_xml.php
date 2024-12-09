4243d6e3f29f2710f4496a83363c2c8451361dd8
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-09 17:08:34 +0300

    xml feed

--- includes/xml/create_yandex_xml.php
+++ includes/xml/create_yandex_xml.php
@@ -107,10 +107,7 @@ function create_yandex_xml_btn () {
             $allproductscount = count($allproducts);
 
             foreach($allproducts as $allproduct){
-                //$prodid = $allproduct->ID;
-                ?>
-                //var prodid = <?php echo $prodid; ?>;
-                <?php
+               
                 // Определяем последую категорию в дереве, к которой присвоен конкретный товар в текущем цикле. В примере участвует кастомная таксономия 'products_category', её замените на ту, которая создана у вас.
                 $lastcateg='';
                 if($categorys=get_the_terms($allproduct->ID,'product_cat')){
