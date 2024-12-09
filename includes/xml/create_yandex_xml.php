0e0a7ef7d55c2c0abc0bcb9047f6f5427505090d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-09 14:29:26 +0300

    xml feed

--- includes/xml/create_yandex_xml.php
+++ includes/xml/create_yandex_xml.php
@@ -225,7 +225,7 @@ function create_yandex_xml_btn () {
 	<?php
 
     
-     print_r($allproducts[0]);
+     print_r($allproducts[0]->get_weight());
 }
 
 add_action( 'wp_footer', 'create_yandex_xml_btn' );
