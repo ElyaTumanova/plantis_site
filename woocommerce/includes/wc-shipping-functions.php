b236ac17b1e2c716ac2747f55b365887db148090
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-15 18:10:46 +0300

    urgent delivery

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -11,7 +11,7 @@ add_action('wp_head','plnt_set_urgent');
 function plnt_set_urgent() {
     date_default_timezone_set('Europe/Moscow');
     $hour = date("H");
-    if ($hour >= 18 && $hour <20) {
+    if ($hour >= 19 && $hour <20) {
         WC()->session->set('isUrgent', '0' );
     } else {
         WC()->session->set('isUrgent', '1' );
