4d09a9ea23741cff56215343ba6e0224c1da2c31
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-16 11:14:10 +0300

    catalog

--- assets/js/header-catalog-menu.js
+++ assets/js/header-catalog-menu.js
@@ -1,11 +1,13 @@
-let majorCats = document.querySelectorAll('.menu--main .menu-node_lvl_1');
+let majorCats = document.querySelectorAll('.menu--main .menu-item-has-children.menu-node_lvl_1');
 
 function showSubmenu(event) {
     console.log(event.target);
+    let menu = event.target;
+    let subMenues = menu.querySelectorAll('.sub-menu');
 
 }
 
 majorCats.forEach((el) => {
-	//console.log(el);
+	console.log(el);
     el.addEventListener('mouseenter',showSubmenu);
 })
