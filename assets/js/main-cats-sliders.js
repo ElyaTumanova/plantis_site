14a47dada1a4d36de6934789a4fa695374f0d781
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-18 19:50:16 +0300

    main cats

--- assets/js/main-cats-sliders.js
+++ assets/js/main-cats-sliders.js
@@ -5,10 +5,10 @@ console.log(navItems);
 console.log(catsSliders);
 
 navItems.forEach((el,index) => {
-    el.addEventListener('click',showSlider);
+    el.addEventListener('click',() => showSlider(event,index));
 });
 
-function showSlider(index) {
-    //console.log(event.target);
+function showSlider(event,index) {
+    console.log(event.target);
     console.log(index);
 }
