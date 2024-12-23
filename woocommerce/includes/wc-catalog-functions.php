af21e007117c1acd5392d843c24caa10d40e27be
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-23 18:31:48 +0300

    regular products

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -415,21 +415,10 @@ function order_by_stock_status($posts_clauses) {
     if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag() || is_product_taxonomy())) {
         $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
         $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
-        $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value = 'onbackorder' " . $posts_clauses['where'];
+        $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
     }
     return $posts_clauses;
 }
-// function order_by_stock_status($posts_clauses) {
-//     global $wpdb;
-//     // only change query on WooCommerce loops
-//     if (is_woocommerce() && (is_shop() || is_product_category() || is_product_tag() || is_product_taxonomy())) {
-//         $posts_clauses['join'] .= " INNER JOIN $wpdb->postmeta istockstatus ON ($wpdb->posts.ID = istockstatus.post_id) ";
-//         $posts_clauses['orderby'] = " istockstatus.meta_value ASC, " . $posts_clauses['orderby'];
-//         $posts_clauses['where'] = " AND istockstatus.meta_key = '_stock_status' AND istockstatus.meta_value <> '' " . $posts_clauses['where'];
-//     }
-//     return $posts_clauses;
-// }
-
 
 // FOR DEV
 // add_action('wp_footer','plnt_wpdb');
