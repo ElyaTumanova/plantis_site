39882172316cd45f449ec379aa653295a23f47a0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-23 21:55:31 +0300

    regular products

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -246,10 +246,15 @@ function plnt_get_add_to_card() {
         if ($product->get_stock_status() ==='instock' || $product->backorders_allowed()) {
             woocommerce_template_loop_add_to_cart(); //заменили обычную не яакс кнопку на аякс кнопку из каталога
         }
-        if ($quantity > 1 || !$product->get_manage_stock() || $product->backorders_allowed()) {
+        if ($quantity > 1 || !$product->get_manage_stock()) {
             woocommerce_quantity_input(array(
                 'min_value' => 1,
                 'max_value'    => $quantity,    // почему-то пришлось передавать заново, проверить на PLANTIS #TODO
+            ),);           // добавили поля ввода. чтобы кнопка "в корзину" работала я полем ввода и кнопками +- см скрипт quantity-buttons.js
+        }
+        if ($product->backorders_allowed()) {
+            woocommerce_quantity_input(array(
+                'min_value' => 1,
             ),);           // добавили поля ввода. чтобы кнопка "в корзину" работала я полем ввода и кнопками +- см скрипт quantity-buttons.js
         }
         if ($product->get_stock_status() ==='instock' || $product->backorders_allowed()) {
