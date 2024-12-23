9c21363e6427182b33d44fccc38f270295bf6980
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-23 17:43:15 +0300

    regular products

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -247,7 +247,6 @@ function plnt_get_add_to_card() {
             woocommerce_template_loop_add_to_cart(); //заменили обычную не яакс кнопку на аякс кнопку из каталога
         }
         if ($quantity > 1 || !$product->get_manage_stock() || $product->backorders_allowed()) {
-            echo 'hihi';
             woocommerce_quantity_input(array(
                 'min_value' => 1,
                 'max_value'    => $quantity,    // почему-то пришлось передавать заново, проверить на PLANTIS #TODO
@@ -274,7 +273,7 @@ function plnt_check_stock_status() {
     global $plants_cat_id;
     $parentCatId = check_category ($product);
     if ($parentCatId === $plants_cat_id) {
-        if ( $product->get_stock_status() ==='instock') {
+        if ( $product->get_stock_status() ==='instock' || $product->backorders_allowed()) {
             ?>
             <div class="card__stockstatus card__stockstatus_in">В наличии</div>
             <?php
