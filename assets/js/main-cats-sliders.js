7be7b06343975b534ec234189bcebd565f477de1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-24 12:51:04 +0300

    main cats

--- assets/js/main-cats-sliders.js
+++ assets/js/main-cats-sliders.js
@@ -4,12 +4,31 @@ let catsSliders = document.querySelectorAll('.main__cats-slider');
 console.log(navItems);
 console.log(catsSliders);
 
-navItems.forEach((el,index) => {
-    el.addEventListener('click',() => showSlider(event,index));
-});
+function closeSlider() {
+    navItems.forEach((el,index) => {
+        if(index === 0) {
+            el.classList.add('main__cats-nav-title_active');
+        } else {
+            el.classList.add('main__cats-nav-title');
+        }
+    });
+    catsSliders.forEach((el,index) => {
+        if(index === 0) {
+            el.classList.add('main__cats-slider_open');
+        } else {
+            el.classList.add('main__cats-slider');
+        }
+    });
+}
+
 
 function showSlider(event,index) {
     console.log(event.target);
     console.log(index);
     catsSliders[index].classList.add('main__cats-slider_open');
 }
+
+closeSlider();
+navItems.forEach((el,index) => {
+    el.addEventListener('click',() => showSlider(event,index));
+});
