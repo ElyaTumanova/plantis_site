3e1ed29504d7b93a59f9aa41c6e6b1e0274b4a33
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-24 13:15:51 +0300

    main cats

--- assets/js/main-cats-sliders.js
+++ assets/js/main-cats-sliders.js
@@ -1,39 +1,14 @@
 let navItems = document.querySelectorAll('.main__cats-nav-title');
 let catsSliders = document.querySelectorAll('.main__cats-slider');
 
-// console.log(navItems);
-// console.log(catsSliders);
-
-// function closeSliders(sliderNmber) {
-//     console.log(sliderNmber);
-//     navItems.forEach((el,index) => {
-//         if(index === sliderNmber) {
-//             el.classList.add('main__cats-nav-title_active');
-//         } else {
-//             el.classList.remove('main__cats-nav-title_active');
-//             //el.classList.add('main__cats-nav-title');
-//         }
-//     });
-//     catsSliders.forEach((el,index) => {
-//         if(index === sliderNmber) {
-//             el.classList.add('main__cats-slider_open');
-//         } else {
-//             el.classList.remove('main__cats-slider_open');
-//             //el.classList.add('main__cats-slider');
-//         }
-//     });
-// }
-
-
 function showSlider(sliderNmber) {
-    console.log(event.target);
-    console.log(sliderNmber);
+    // console.log(event.target);
+    // console.log(sliderNmber);
     navItems.forEach((el,index) => {
         if(index === sliderNmber) {
             el.classList.add('main__cats-nav-title_active');
         } else {
             el.classList.remove('main__cats-nav-title_active');
-            //el.classList.add('main__cats-nav-title');
         }
     });
     catsSliders.forEach((el,index) => {
@@ -41,12 +16,9 @@ function showSlider(sliderNmber) {
             el.classList.add('main__cats-slider_open');
         } else {
             el.classList.remove('main__cats-slider_open');
-            //el.classList.add('main__cats-slider');
         }
     });
-    //closeSliders(index);
-    // event.target.classList.add('main__cats-nav-title_active')
-    // catsSliders[index].classList.add('main__cats-slider_open');
+
 }
 
 showSlider(0);
