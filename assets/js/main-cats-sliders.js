b7460849e926493e02cab674c6847da13a0f7a91
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-24 13:03:39 +0300

    main cats

--- assets/js/main-cats-sliders.js
+++ assets/js/main-cats-sliders.js
@@ -4,17 +4,17 @@ let catsSliders = document.querySelectorAll('.main__cats-slider');
 // console.log(navItems);
 // console.log(catsSliders);
 
-function closeSliders(all) {
-    console.log(all);
+function closeSliders(sliderNmber) {
+    console.log(sliderNmber);
     navItems.forEach((el,index) => {
-        if(index === 0 && !all) {
+        if(index === sliderNmber) {
             el.classList.add('main__cats-nav-title_active');
         } else {
             el.classList.add('main__cats-nav-title');
         }
     });
     catsSliders.forEach((el,index) => {
-        if(index === 0 && !all) {
+        if(index === sliderNmber) {
             el.classList.add('main__cats-slider_open');
         } else {
             el.classList.add('main__cats-slider');
@@ -26,12 +26,12 @@ function closeSliders(all) {
 function showSlider(event,index) {
     console.log(event.target);
     console.log(index);
-    closeSliders(true);
-    event.target.classList.add('main__cats-nav-title_active')
-    catsSliders[index].classList.add('main__cats-slider_open');
+    closeSliders(index);
+    // event.target.classList.add('main__cats-nav-title_active')
+    // catsSliders[index].classList.add('main__cats-slider_open');
 }
 
-closeSliders(false);
+closeSliders(0);
 navItems.forEach((el,index) => {
     el.addEventListener('click',() => showSlider(event,index));
 });
