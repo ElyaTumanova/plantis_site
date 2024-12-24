2a81c16e0e02bc5b17f4c8dfcbe0dd6f06a306c4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-24 12:55:26 +0300

    main cats

--- assets/js/main-cats-sliders.js
+++ assets/js/main-cats-sliders.js
@@ -4,16 +4,16 @@ let catsSliders = document.querySelectorAll('.main__cats-slider');
 console.log(navItems);
 console.log(catsSliders);
 
-function closeSlider() {
+function closeSliders(all) {
     navItems.forEach((el,index) => {
-        if(index === 0) {
+        if(index === 0 && !all) {
             el.classList.add('main__cats-nav-title_active');
         } else {
             el.classList.add('main__cats-nav-title');
         }
     });
     catsSliders.forEach((el,index) => {
-        if(index === 0) {
+        if(index === 0 && !all) {
             el.classList.add('main__cats-slider_open');
         } else {
             el.classList.add('main__cats-slider');
@@ -25,10 +25,12 @@ function closeSlider() {
 function showSlider(event,index) {
     console.log(event.target);
     console.log(index);
+    closeSliders(true);
+    event.target.classList.add('main__cats-nav-title_active')
     catsSliders[index].classList.add('main__cats-slider_open');
 }
 
-closeSlider();
+closeSliders(false);
 navItems.forEach((el,index) => {
     el.addEventListener('click',() => showSlider(event,index));
 });
