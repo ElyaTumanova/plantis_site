7ccf9bcf8232c76fd6571935597e6de3f641f7d4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2024-12-28 14:35:42 +0300

    main cats

--- assets/js/slider-init.js
+++ assets/js/slider-init.js
@@ -159,53 +159,53 @@ const swiper_card_ukhod = new Swiper('.card-ukhod-swiper', {
 /*--------------------------------------------------------------
 # Products slider
 --------------------------------------------------------------*/
-// function swiper_product_slider_init() {
-//     console.log('hohooh');
-//     const swiper_product_slider = new Swiper('.product-slider-swiper', {
-//         pagination: {
-//             el: '.swiper-pagination',
-//             clickable: true,
-//             type: 'progressbar'
-//         },
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//         // scrollbar: {
-//         //     el: '.swiper-scrollbar',
-//         //     draggable: true,
-//         // },
-//         slidesPerView: 5,
-//         slidesPerGroup: 1,
-//         spaceBetween: 30,
-//         loop: true,
-//         freeMode: true,
-//         breakpoints: {
-//             320: {
-//                 slidesPerView: 2,
-//                 spaceBetween: 10,
-//                 navigation: {
-//                     enabled: false,
-//                 },
-//                 freeMode: true,
-//             },
-//             768: {
-//                 slidesPerView: 4,
-//                 spaceBetween: 10,
-//                 navigation: {
-//                     enabled: true,
-//                 },
-//             },
-//             1024: {
-//                 slidesPerView: 5,
-//                 spaceBetween: 30,
-//                 navigation: {
-//                     enabled: true,
-//                 },
-//             }
-//         }
-//     });
-// }
+function swiper_product_slider_init() {
+    console.log('hello');
+    const swiper_product_slider = new Swiper('.product-slider-swiper', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            type: 'progressbar'
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        //     draggable: true,
+        // },
+        slidesPerView: 5,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+        loop: true,
+        freeMode: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+                navigation: {
+                    enabled: false,
+                },
+                freeMode: true,
+            },
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 10,
+                navigation: {
+                    enabled: true,
+                },
+            },
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 30,
+                navigation: {
+                    enabled: true,
+                },
+            }
+        }
+    });
+}
 
 /*--------------------------------------------------------------
 # About Us
