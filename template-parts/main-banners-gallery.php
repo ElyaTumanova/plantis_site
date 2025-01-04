9145eb5132873fe57133a9bd98de1571f04f74f4
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-04 23:22:17 +0300

    main banners

--- template-parts/main-banners-gallery.php
+++ template-parts/main-banners-gallery.php
@@ -27,7 +27,7 @@ $banners_arr = carbon_get_theme_option('banners');
 <div class="main__banners_mob main__banners-swiper swiper">
 	<div class="swiper-wrapper">
 	<?php
-		foreach ($banners_arr as &$banner) {
+		foreach ($banners_arr as $banner) {
 			?>
 			<a class="swiper-slide <?php if(!$banner['banner_link']) : ?> main__banners_no-link <?php endif; ?>" href="<?php echo $banner['banner_link'] ?>"><img src="<?php echo $banner['banner_mob'] ?>" class="main__banner-img" alt="<?php echo $banner['banner_name']?>" loading=" lazy"></a>
 			<?php 
