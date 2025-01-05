dde77a7ec41901fbc038afe1557a77c1bf5d004f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-05 19:20:52 +0300

    icons

--- woocommerce/includes/wc-yith-wishlist-finctions.php
+++ woocommerce/includes/wc-yith-wishlist-finctions.php
@@ -63,40 +63,6 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
     add_action( 'wp_ajax_nopriv_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
   }
   
-  // if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_ajax_update_count' ) ) {
-  //   function yith_wcwl_ajax_update_count() {
-  //     wp_send_json( array(
-  //       'count' => yith_wcwl_get_items_count()
-  //     ) );
-  //   }
-  
-  //   add_action( 'wp_ajax_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
-  //   add_action( 'wp_ajax_nopriv_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
-  // }
-  
-
-
-  // if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_enqueue_custom_script' ) ) {
-  //   function yith_wcwl_enqueue_custom_script() {
-  //     wp_add_inline_script(
-  //       'jquery-yith-wcwl',
-  //       "
-  //         jQuery( function( $ ) {
-  //           $( document ).on( 'added_to_wishlist removed_from_wishlist', function() {
-  //             $.get( yith_wcwl_l10n.ajax_url, {
-  //               action: 'yith_wcwl_update_wishlist_count'
-  //             }, function( data ) {
-  //               $('.header__wishlist').html( data.count );
-  //               console.log (data.count);
-  //             } );
-  //           } );
-  //         } );
-  //       "
-  //     );
-  //   }
-  
-  //   add_action( 'wp_enqueue_scripts', 'yith_wcwl_enqueue_custom_script', 20 );
-  // }
 
   if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_enqueue_custom_script' ) ) {
     function yith_wcwl_enqueue_custom_script() {
