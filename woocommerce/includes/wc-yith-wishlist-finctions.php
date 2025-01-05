abbc450187b56762253f1e582b18811fe0f9fefe
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-05 17:50:00 +0300

    icons

--- woocommerce/includes/wc-yith-wishlist-finctions.php
+++ woocommerce/includes/wc-yith-wishlist-finctions.php
@@ -52,17 +52,20 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
     add_shortcode( 'yith_wcwl_items_count', 'yith_wcwl_get_items_count' );
   }
   
-  if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_ajax_update_count' ) ) {
-    function yith_wcwl_ajax_update_count() {
-      wp_send_json( array(
-        'count' => yith_wcwl_count_all_products()
-      ) );
-    }
+  // if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_ajax_update_count' ) ) {
+  //   function yith_wcwl_ajax_update_count() {
+  //     wp_send_json( array(
+  //       'count' => yith_wcwl_count_all_products()
+  //     ) );
+  //   }
   
-    add_action( 'wp_ajax_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
-    add_action( 'wp_ajax_nopriv_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
-  }
+  //   add_action( 'wp_ajax_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
+  //   add_action( 'wp_ajax_nopriv_yith_wcwl_update_wishlist_count', 'yith_wcwl_ajax_update_count' );
+  // }
   
+  add_action( 'wp_ajax_yith_wcwl_update_wishlist_count', 'yith_wcwl_get_items_count' );
+  add_action( 'wp_ajax_nopriv_yith_wcwl_update_wishlist_count', 'yith_wcwl_get_items_count' );
+
   if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_enqueue_custom_script' ) ) {
     function yith_wcwl_enqueue_custom_script() {
       wp_add_inline_script(
@@ -73,8 +76,8 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
               $.get( yith_wcwl_l10n.ajax_url, {
                 action: 'yith_wcwl_update_wishlist_count'
               }, function( data ) {
-                $('.yith-wcwl-items-count').children('i').html( data.count );
-                test (data.count);
+                $('.header__wishlist').html( data );
+                console.log (data);
               } );
             } );
           } );
@@ -85,9 +88,28 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
     add_action( 'wp_enqueue_scripts', 'yith_wcwl_enqueue_custom_script', 20 );
   }
 
-  function test (count) {
-    print_r(count);
-  }
+  // if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_enqueue_custom_script' ) ) {
+  //   function yith_wcwl_enqueue_custom_script() {
+  //     wp_add_inline_script(
+  //       'jquery-yith-wcwl',
+  //       "
+  //         jQuery( function( $ ) {
+  //           $( document ).on( 'added_to_wishlist removed_from_wishlist', function() {
+  //             $.get( yith_wcwl_l10n.ajax_url, {
+  //               action: 'yith_wcwl_update_wishlist_count'
+  //             }, function( data ) {
+  //               $('.yith-wcwl-items-count').children('i').html( data.count );
+  //             } );
+  //           } );
+  //         } );
+  //       "
+  //     );
+  //   }
+  
+  //   add_action( 'wp_enqueue_scripts', 'yith_wcwl_enqueue_custom_script', 20 );
+  // }
+
+ 
 
 
 // получаем ИД товаров в wishlist для аякса
