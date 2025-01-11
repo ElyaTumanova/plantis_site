18e52496e864bda34bde3bc3dd435e573dc0ff2d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-11 12:35:24 +0300

    burger menu

--- template-parts/popups/catalog-menu.php
+++ template-parts/popups/catalog-menu.php
@@ -5,8 +5,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 $close_icon = carbon_get_theme_option('close_icon')
 ?>
 
-<div class="modal-mob catalog-menu__wrap">
-    <div class="modal-mob__close catalog-menu__close button"><?php echo $close_icon ?></div>
+<div class="catalog-menu__wrap">
+    <!-- <div class="modal-mob__close catalog-menu__close button"><?php //echo $close_icon ?></div> -->
     <!-- <div class="menu__item_accent ">Каталог</div> -->
     <?php plnt_catalog_menu() ?>
 </div>
