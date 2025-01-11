f14006edb70913cf363a3f91d3dd2a681e3ce0e2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-11 15:51:57 +0300

    social media btns

--- template-parts/social-media-btns.php
+++ template-parts/social-media-btns.php
@@ -23,6 +23,10 @@ if ( ! defined( 'ABSPATH' ) ) {
         </svg>
     </a>
     <a class="social-media__button button social-media__button-telegram" href="https://t.me/+79647687944" target="_blank">
-        <svg class="social-media__button-icon" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></svg>					
+        <!-- <svg class="social-media__button-icon" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></svg>					 -->
+        <svg class="social-media__button-icon" xmlns="http://www.w3.org/2000/svg" width="65" height="64" viewBox="0 0 65 64" fill="none">
+            <path d="M25.9046 39.6721L41.8637 23.4855L19.0316 36.2355L1.45312 31.4022L60.5155 7.51562L48.041 50.9466L25.9046 39.6721Z" stroke="#292929" stroke-width="2" stroke-linejoin="bevel"/>
+            <path d="M19.1562 36.4814L24.3109 54.8916L31.3069 47.5275" stroke="#292929" stroke-width="2" stroke-linejoin="bevel"/>
+        </svg>
     </a>
 </div>
