2368ae09f197b314b2fcbc4d82ea1c6b0ab56679
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-13 15:06:12 +0300

    regular products

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -26,7 +26,7 @@ function plnt_set_urgent() {
 
 //for dev
 
-//add_action('woocommerce_review_order_before_shipping','plnt_check');
+add_action('woocommerce_review_order_before_shipping','plnt_check');
 //add_action('wp_head','plnt_check');
 
 function plnt_check() {
@@ -37,15 +37,15 @@ function plnt_check() {
     $chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
     //print_r( $packages);
     //echo '<br>';
-    echo $chosen_methods[0];
-    echo '<br>';
+    // echo $chosen_methods[0];
+    // echo '<br>';
 
     // if($local_pickup === $chosen_methods[0]) {
     //     echo 'hi';
     //     WC()->session->set('isLate', '0' );  
     // }
     echo (WC()->session->get('isUrgent' ));
-    echo (WC()->session->get('isLate' ));
+    //echo (WC()->session->get('isLate' ));
     // date_default_timezone_set('Europe/Moscow');
     // $hour = date("H");
     // if ( is_checkout() && ($hour<18 || $hour>=20)) {
