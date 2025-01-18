a006ef10e3058735e9b546ca52b03e84536989ce
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-18 15:39:15 +0300

    regular products card

--- assets/js/quantity-buttons.js
+++ assets/js/quantity-buttons.js
@@ -52,7 +52,7 @@ jQuery(function ($){
 	//уведомление для backorder
 
 	var backorderInfo = qty.parent().parent().parent().parent().find(".backorder-info");
-	console.log(backorderInfo[0]);
+	console.log(backorderInfo);
 	if (newVal == (stock + 1)) {
 		backorderInfo.innerHTML='hohoho';
 	}
