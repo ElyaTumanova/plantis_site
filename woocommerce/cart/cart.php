a75e70d6606f46c4447af7117b15e37f429ad5ed
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-20 15:38:22 +0300

    quantity btns

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -144,6 +144,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 						</td>
 
 						<td class="product-quantity" data-title="<?php esc_attr_e( 'Quantity', 'woocommerce' ); ?>">
+						<div class="quantity">
 						<div class="plus">&#43;</div>
 						<?php
 						if ( $_product->is_sold_individually() ) {
@@ -169,6 +170,7 @@ do_action( 'woocommerce_before_cart' ); ?>
 						echo apply_filters( 'woocommerce_cart_item_quantity', $product_quantity, $cart_item_key, $cart_item ); // PHPCS: XSS ok.
 						?>
 						<div class="minus">&#8722;</div>
+						</div>
 						</td>
 
 						<td class="product-subtotal" data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>">
