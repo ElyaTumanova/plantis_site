aef4b4755e697780e354482f5c82702586aee076
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-23 12:44:50 +0300

    card__ukhod-wrap

--- assets/js/metrikaGoal.js
+++ assets/js/metrikaGoal.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function() {
 	});
 
     // Клик по кнопке «Положить в корзину», товары для ухода
-	jQuery('.card__grid .card__ukhod-wrap .add_to_cart_button').click(function(){
+	jQuery('.card__ukhod-wrap .add_to_cart_button').click(function(){
 		//yaCounter87781741.reachGoal('click-button-cart-ukhod-loop');
 		 ym(87781741, 'reachGoal', 'click-button-cart-ukhod-loop'); 
         return true;
