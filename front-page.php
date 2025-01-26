8625f2ec8fd2c1b6d5562a78c71a3bc90e8b0951
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-01-26 17:46:01 +0300

    feedbacks

--- front-page.php
+++ front-page.php
@@ -71,6 +71,7 @@ global $skidki_cat_id;
 		<p class="main__about-text">Благодаря такому отношению к делу более 30% наших клиентов возвращаются к нам за покупкой вновь.</p>
 		<p class="main__about-text">Для нас это важно, ведь так приятно делать сервис, после которого вы будете вспоминать нас с теплотой и смотреть на своего нового друга.</p>					
 	</div>
+	<div style="width:560px;height:800px;overflow:hidden;position:relative;"><iframe style="width:100%;height:100%;border:1px solid #e6e6e6;border-radius:8px;box-sizing:border-box" src="https://yandex.ru/maps-reviews-widget/237252555639?comments"></iframe><a href="https://yandex.ru/maps/org/plentis/237252555639/" target="_blank" style="box-sizing:border-box;text-decoration:none;color:#b3b3b3;font-size:10px;font-family:YS Text,sans-serif;padding:0 20px;position:absolute;bottom:8px;width:100%;text-align:center;left:0;overflow:hidden;text-overflow:ellipsis;display:block;max-height:14px;white-space:nowrap;padding:0 16px;box-sizing:border-box">Плэнтис на карте Москвы — Яндекс Карты</a></div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
