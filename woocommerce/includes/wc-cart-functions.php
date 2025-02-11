f541192e9f593a17ec6cec155a07e7e9b138b83d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-11 17:48:50 +0300

    backorder upsale

--- woocommerce/includes/wc-cart-functions.php
+++ woocommerce/includes/wc-cart-functions.php
@@ -181,9 +181,8 @@ add_filter( 'wp_ajax_plnt_update_header_cart_count', 'plnt_update_header_cart_co
 /*--------------------------------------------------------------
 # CART FUNCTIONS 
 --------------------------------------------------------------*/
-// доп функции для корзины
 
-// // изменяем кнопку "в корзину" после добавления товара в корзину
+// изменяем кнопку "в корзину" после добавления товара в корзину
 
 
 add_filter( 'woocommerce_product_single_add_to_cart_text', 'truemisha_single_product_btn_text' ); // текст для страницы самого товара
@@ -196,58 +195,15 @@ function truemisha_single_product_btn_text( $text ) {
 	return $text;
 }
 
-// add_filter( 'woocommerce_product_add_to_cart_text', 'truemisha_product_btn_text', 20, 2 ); //текст для страниц каталога товаров, категорий товаров и т д
- 
-// function truemisha_product_btn_text( $text, $product ) {
-// 	if( 
-// 	   $product->is_type( 'simple' )
-// 	   && $product->is_purchasable()
-// 	   && $product->is_in_stock()
-// // 		&& !wp_is_mobile()
-// 	   && WC()->cart->find_product_in_cart( WC()->cart->generate_cart_id( $product->get_id() ) )
-// 	) {
-// 		$text = 'Добавлен1';
-// 	}
-// 	return $text;
-// }
+// аналоги для товаров в статусе backorder в корзине
 
-//add_action( 'wp_footer', 'ajax_button_text_js_script' );
-function ajax_button_text_js_script() {
-	if (!(is_product() )){
-		$text = __('Добавлен', 'woocommerce');
-		$svg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path fill="#fff" d="M8.305 20.982.179 12.856l2.031-2.031 6.095 6.094 13.08-13.08 2.03 2.032-15.11 15.111Z"/></svg>' 
-		?>
-	   <script>
-		  jQuery(function($) {
-			    var text = '<?php echo $text; ?>',      $this;
-				var svg = '<?php echo $svg; ?>',      $this;
-				$(document.body).on('click', '.ajax_add_to_cart', function(event){
-					$this = $(this); 
-					console.log(this);
-				});
+add_action('plnt_cart_item','plnt_backorder_upsale', 10)
 
-				$(document.body).on('added_to_cart', function(event,b,data){
-					
-					var buttonContent = '<span class="jet-woo-button-content"> <span class="button-label">'+text+'</span> <span class="button-icon"> '+svg+'</span> </span>'
-					var buttonText = '<span class="button-label">'+text+'</span>';
-					var buttonIcon = '<span class="button-icon"> '+svg+'</span>';
-					//console.log ($this)
-					//$this.html(buttonText).attr('data-tip',text);
-					$this.html(buttonText);
+function plnt_backorder_upsale() {
+	echo 'hi';
+}
 
-					// <?php  
-					// $cart_item_key = WC()->cart->generate_cart_id( $product->get_id() );
-					// $remove_cart_url = wc_get_cart_remove_url( $cart_item_key );
-					// ?>;
 
-					// var remove_cart_url = <?php echo $remove_cart_url; ?>;
-					// console.log (remove_cart_url);
-				});
-			});
-		</script>
-	  <?php 
-	}
- }
 
 /*--------------------------------------------------------------
 # HELPERS 
