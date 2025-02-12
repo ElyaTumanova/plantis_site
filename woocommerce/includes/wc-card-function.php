af4649ecc545a4fd3241fcd8fac44df67ebd1f53
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-12 17:16:08 +0300

    peresadka

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -212,7 +212,7 @@ function plnt_price_wrap(){
 function plnt_get_peresadka_add_to_cart() {
     global $product; 
     $product_id = $product->get_id();
-    ?><div class="card__peresadka"><?php 
+    ?><div class="cart__peresadka"><?php 
     get_template_part('template-parts/products/products-peresadka',null, 
         array( // массив с параметрами
             'product_id' => $product_id
