d89fbda7a407e7795b36936b67daae61c129b350
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-12 15:39:15 +0300

    backorder upsale

--- assets/js/cart-backorder-crossell.js
+++ assets/js/cart-backorder-crossell.js
@@ -2,6 +2,9 @@ let backorderWrap = document.querySelectorAll('.product-backorder-upsells');
 
 backorderWrap.forEach((el) =>{
     let dropDown = el.querySelector('.backorder-crossells__preview-down');
+    console.log(el);
+    console.log(dropDown);
+
     dropDown.addEventListener('click','toggleBackorderDropdown');
 });
 
