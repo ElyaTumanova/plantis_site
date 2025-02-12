7be45ad777e378f4b9fd030c950ee035f836b797
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-12 17:28:05 +0300

    peresadka

--- woocommerce/includes/wc-cart-functions.php
+++ woocommerce/includes/wc-cart-functions.php
@@ -185,7 +185,7 @@ add_filter( 'wp_ajax_plnt_update_header_cart_count', 'plnt_update_header_cart_co
 // изменяем кнопку "в корзину" после добавления товара в корзину
 
 
-add_filter( 'woocommerce_product_single_add_to_cart_text', 'truemisha_single_product_btn_text' ); // текст для страницы самого товара
+//add_filter( 'woocommerce_product_single_add_to_cart_text', 'truemisha_single_product_btn_text' ); // текст для страницы самого товара
  
 function truemisha_single_product_btn_text( $text ) {
 	if( WC()->cart->find_product_in_cart( WC()->cart->generate_cart_id( get_the_ID() ) ) ) {
