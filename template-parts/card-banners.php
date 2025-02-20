57064a9dacbbae4182fe7395230ed19115297bba
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-20 11:15:51 +0300

    card banners

--- template-parts/card-banners.php
+++ template-parts/card-banners.php
@@ -11,7 +11,9 @@ $parentCatId = check_category ($product);
 $min_treez_delivery = carbon_get_theme_option('min_treez_delivery');
 
 $isTreez = check_is_treez($product);
-echo $isTreez;
+if ($isTreez) {
+    echo 'hi';
+}
 ?>
 
 
