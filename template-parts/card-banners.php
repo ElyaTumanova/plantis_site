474d8704e84705d31ebbf7f247c3859ac5e52806
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-24 12:59:49 +0300

    lechuza treez backorder

--- template-parts/card-banners.php
+++ template-parts/card-banners.php
@@ -9,8 +9,10 @@ global $treez_cat_id;
 global $plants_treez_cat_id;
 $parentCatId = check_category ($product);
 $min_treez_delivery = carbon_get_theme_option('min_treez_delivery');
+$min_lechuza_delivery = carbon_get_theme_option('min_lechuza_delivery');
 
 $isTreez = check_is_treez($product);
+$isLechuza = check_is_lechuza($product)
 ?>
 
 
@@ -28,7 +30,7 @@ $isTreez = check_is_treez($product);
         </div>
     <?php endif; ?>
 <?php endif; ?>
-<?php if (!$isTreez) :?>
+<?php if (!$isTreez || !$isLechuza) :?>
     <div class="card_banner" id="card_banner_peresadka">
         <p>При покупке <a href="<?php echo get_site_url()?>/product-category/gorshki_i_kashpo/" target="_blank" rel="noopener">горшка </a>— пересадка в подарок!*</p>
         <cite>*кроме кашпо Treez и Lechuza диаметром больше 26 см</cite>
@@ -39,6 +41,12 @@ $isTreez = check_is_treez($product);
     <div class="card_banner" id="card_banner_treez">
         <p> Минимальная сумма заказа для кашпо и искусственных растений Treez <span><?php echo $min_treez_delivery?></span> рублей (без учета стоимости других товаров)</p>
         <p> Оплатить заказ с кашпо и/или искусственными растениями Treez можно будет после подтверждения их наличия. Наш менеджер свяжется с Вами после оформления заказа.</p>
+    </div>
+<?php endif; ?>
+<?php if ($isLechuza) :?>
+    <div class="card_banner" id="card_banner_treez">
+        <p> Минимальная сумма заказа для кашпо Lechuza <span><?php echo $min_lechuza_delivery?></span> рублей (без учета стоимости других товаров)</p>
+        <p> Оплатить заказ с кашпо Lechuza можно будет после подтверждения их наличия. Наш менеджер свяжется с Вами после оформления заказа.</p>
     </div>
 <?php endif; ?>
 
