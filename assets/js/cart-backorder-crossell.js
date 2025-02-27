399d431ee59d978d113374e1bba8fc727cac3e53
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-27 14:25:45 +0300

    backorder upsale

--- assets/js/cart-backorder-crossell.js
+++ assets/js/cart-backorder-crossell.js
@@ -22,31 +22,31 @@ function replaceBackorderProduct(evt, btn) {
     let prodId = btn.getAttribute('data-product_id');
     let cartItem = btn.getAttribute('data-cart_item');
     let addToCartBtn = btn.nextElementSibling;
-    console.log(addToCartBtn)
+    console.log(addToCartBtn);
     // console.log(prodId);
     // console.log(cartItem);
 
-    jQuery( function($){
-        $.ajax({
-            type: 'POST',
-            url: woocommerce_params.ajax_url,
-            data: {
-                'action': 'replace_backorder_product',
-                'backorder_replace_prodId': prodId,
-                'backorder_replace_cart_item': cartItem,
-            },
-            success: function (result) {
-                // Trigger refresh checkout
-                //console.log('hi update cart');
-                //console.log(result);
-                //plntAjaxUpdateCartCount();
-                //plntAjaxGetMiniCart();
-                // $( '[name="update_cart"]' ).removeAttr("disabled").trigger( 'click' ); // автообновление корзины без перезагрузки 
-                console.log(addToCartBtn);
+    // jQuery( function($){
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: woocommerce_params.ajax_url,
+    //         data: {
+    //             'action': 'replace_backorder_product',
+    //             'backorder_replace_prodId': prodId,
+    //             'backorder_replace_cart_item': cartItem,
+    //         },
+    //         success: function (result) {
+    //             // Trigger refresh checkout
+    //             //console.log('hi update cart');
+    //             //console.log(result);
+    //             //plntAjaxUpdateCartCount();
+    //             //plntAjaxGetMiniCart();
+    //             // $( '[name="update_cart"]' ).removeAttr("disabled").trigger( 'click' ); // автообновление корзины без перезагрузки 
+    //             console.log(addToCartBtn);
                 
-                //$('.backorder_replace_btn').next().trigger( 'click' );
-            }
-        });
-    });
+    //             //$('.backorder_replace_btn').next().trigger( 'click' );
+    //         }
+    //     });
+    // });
     
 }
