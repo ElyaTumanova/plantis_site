1dd3aca107c0e689a5fdb020833551a4bd1ed1a1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-02-27 14:33:29 +0300

    backorder upsale

--- assets/js/cart-backorder-crossell.js
+++ assets/js/cart-backorder-crossell.js
@@ -21,7 +21,7 @@ function toggleBackorderDropdown(evt, el) {
 function replaceBackorderProduct(evt, btn) {
     let prodId = btn.getAttribute('data-product_id');
     let cartItem = btn.getAttribute('data-cart_item');
-    let addToCartBtn = document.querySelector(`.ajax_add_to_cart [data-product_id=${prodId}]`);
+    let addToCartBtn = document.querySelector(`.ajax_add_to_cart[data-product_id=${prodId}]`);
     console.log(btn);
     console.log(addToCartBtn);
     // console.log(prodId);
