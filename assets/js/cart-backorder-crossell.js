6bc1d9d0216ca8aa56552078e7b7ee74254e5dc2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-05 17:44:56 +0300

    backorder upsale

--- assets/js/cart-backorder-crossell.js
+++ assets/js/cart-backorder-crossell.js
@@ -3,14 +3,17 @@ let backorderWrap = document.querySelectorAll('.product-backorder-upsells');
 if(backorderWrap) {
     backorderWrap.forEach((el) =>{
         let dropDownBtn = el.querySelector('.backorder-crossells__preview-down');
-        dropDownBtn.addEventListener('click', (evt)=>{toggleBackorderDropdown(evt,el)});
-        console.log('hi event')
+        if(dropDownBtn) {
+            dropDownBtn.addEventListener('click', (evt)=>{toggleBackorderDropdown(evt,el)});
+            console.log('hi event')
+        }
 
         let replaceBtns = el.querySelectorAll('.backorder_replace_btn');
-        
-        replaceBtns.forEach((btn)=>{
-            btn.addEventListener('click', (evt)=>{replaceBackorderProduct(evt,btn)});
-        })
+        if(replaceBtns) {
+            replaceBtns.forEach((btn)=>{
+                btn.addEventListener('click', (evt)=>{replaceBackorderProduct(evt,btn)});
+            })
+        }
     }); 
 }    
 
