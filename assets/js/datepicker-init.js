f1bdeab4983ddc891457c310c7c680a0ba7a05d9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-21 11:09:35 +0300

    datepicker

--- assets/js/datepicker-init.js
+++ assets/js/datepicker-init.js
@@ -76,7 +76,7 @@ function datepicker_options () {
     let selectedDate = startDate;
     // datepicker options
     let datePickerOpts = {
-        selectedDates: [selectedDate],
+        selectedDates: [startDate],
         minDate: startDate,
         maxDate: (function(){
             let date = new Date();
