bd08ef0e899a502f7c7788e56605f9cf4ae6964a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-23 14:36:21 +0300

    search

--- includes/ajax.php
+++ includes/ajax.php
@@ -76,24 +76,25 @@ function plnt_search_ajax_action_callback (){
         }
         if ($product_sku_id) {
             $product = wc_get_product( $product_sku_id );
-            // print_r($product);
+            print_r($product);
             $short_descr = $product->get_short_description();
-            $sale = get_post_meta( get_the_ID(), '_sale_price', true);
+            $title = $product->get_the_title();
+            $sale = get_post_meta( $product_sku_id, '_sale_price', true);
             ?>
             <div class="search-result__item">
                 <a href="<?php echo get_permalink();?>" class="search-result__link" target="blank">
-                    <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );?>" class="search-result__image" alt="<?php echo get_the_title();?>">
+                    <img src="<?php echo get_the_post_thumbnail_url( $product_sku_id, 'thumbnail' );?>" class="search-result__image" alt="<?php echo get_the_title();?>">
                     <div class="search-result__info">
-                        <span class="search-result__title"><?php echo get_the_title();?></span>
+                        <span class="search-result__title"><?php echo $title?></span>
                         <span class="search-result__descr"><?php echo $short_descr?></span>
                         <?php if ($sale) {
                             ?>
-                            <span class="search-result__reg-price"><?php echo get_post_meta( get_the_ID(), '_regular_price', true);?>&#8381;</span>
-                            <span class="search-result__price"><?php echo get_post_meta( get_the_ID(), '_sale_price', true);?>&#8381;</span>
+                            <span class="search-result__reg-price"><?php echo get_post_meta( $product_sku_id, '_regular_price', true);?>&#8381;</span>
+                            <span class="search-result__price"><?php echo get_post_meta( $product_sku_id, '_sale_price', true);?>&#8381;</span>
                             <?php
                         } else {
                             ?>
-                            <span class="search-result__price"><?php echo get_post_meta( get_the_ID(), '_price', true);?>&#8381;</span>
+                            <span class="search-result__price"><?php echo get_post_meta( $product_sku_id, '_price', true);?>&#8381;</span>
                             <?php 
                         }
                         ?>
