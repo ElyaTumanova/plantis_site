4879b0943a48e355600f124a908fd6465810ecda
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-23 14:33:47 +0300

    search

--- includes/ajax.php
+++ includes/ajax.php
@@ -41,13 +41,41 @@ function plnt_search_ajax_action_callback (){
     print_r($product_sku_id);
     print_r($query_ajax->query_vars[ 's' ]);
     $json_data['out'] = ob_start(PHP_OUTPUT_HANDLER_CLEANABLE);
-    if ($query_ajax->have_posts()) {
+    if ($query_ajax->have_posts() || $product_sku_id) {
         // echo '<pre>';
         // print_r( $query_ajax );
         // echo '</pre>';
         while ($query_ajax->have_posts()){
             $query_ajax->the_post();
             $product = wc_get_product( get_the_ID() );
+            // print_r($product);
+            $short_descr = $product->get_short_description();
+            $sale = get_post_meta( get_the_ID(), '_sale_price', true);
+            ?>
+            <div class="search-result__item">
+                <a href="<?php echo get_permalink();?>" class="search-result__link" target="blank">
+                    <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );?>" class="search-result__image" alt="<?php echo get_the_title();?>">
+                    <div class="search-result__info">
+                        <span class="search-result__title"><?php echo get_the_title();?></span>
+                        <span class="search-result__descr"><?php echo $short_descr?></span>
+                        <?php if ($sale) {
+                            ?>
+                            <span class="search-result__reg-price"><?php echo get_post_meta( get_the_ID(), '_regular_price', true);?>&#8381;</span>
+                            <span class="search-result__price"><?php echo get_post_meta( get_the_ID(), '_sale_price', true);?>&#8381;</span>
+                            <?php
+                        } else {
+                            ?>
+                            <span class="search-result__price"><?php echo get_post_meta( get_the_ID(), '_price', true);?>&#8381;</span>
+                            <?php 
+                        }
+                        ?>
+                    </div>
+                </a>  
+            </div>
+            <?php
+        }
+        if ($product_sku_id) {
+            $product = wc_get_product( $product_sku_id );
             // print_r($product);
             $short_descr = $product->get_short_description();
             $sale = get_post_meta( get_the_ID(), '_sale_price', true);
