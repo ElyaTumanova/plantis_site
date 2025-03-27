cb495c6e7409027ac2d42924f2c73749da128046
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-27 12:56:29 +0300

    pochta delivery

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -577,8 +577,10 @@ Contents
 
         //Доставка Почтой России
         if ( $delivery_pochta == $chosen_methods[0]) {
+            $shipping_costs = plnt_get_shiping_costs();
+            $delivery_pochta_cost = $shipping_costs[$delivery_pochta]
             echo '<div class="checkout__text checkout__text_long-dist">
-                Доставка Почтой России. 
+                Доставка Почтой России '.$delivery_pochta_cost,'. рублей.
                 Мы свяжемся с Вами после оформления заказа в рабочее время с 10:00 до 20:00 и рассчитаем стоимость доставки.
                 <a href="https://plantis.shop/delivery/">Подробнее об условиях доставки и самовывоза.</a>
                 </div>';
