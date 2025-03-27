7385b90a67d3d24731bc36975ec9eef2376138b7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-27 17:11:31 +0300

    seo

--- includes/custom-fields/term-meta.php
+++ includes/custom-fields/term-meta.php
@@ -7,7 +7,7 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 Container::make( 'term_meta', __( 'Category Properties' ) )
-    ->where( 'term_taxonomy', '=', 'category' )
+    ->where( 'term_taxonomy', '=', 'product_cat' )
     ->add_fields( array(
         Field::make( 'color', 'crb_title_color', __( 'Title Color' ) ),
         Field::make( 'image', 'crb_thumb', __( 'Thumbnail' ) ),
