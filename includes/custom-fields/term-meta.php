1d0bd2b6a3d6ee03a98688be9c247a72dee32907
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-03-27 17:16:48 +0300

    seo

--- includes/custom-fields/term-meta.php
+++ includes/custom-fields/term-meta.php
@@ -9,5 +9,5 @@ use Carbon_Fields\Field;
 Container::make( 'term_meta', __( 'Category Properties' ) )
     ->where( 'term_taxonomy', '=', 'product_cat' )
     ->add_fields( array(
-        Field::make( 'text', 'SEO-title', __( 'SEO-title' ) ),
+        Field::make( 'text', 'seo-title', __( 'seo-title' ) ),
     ) );
