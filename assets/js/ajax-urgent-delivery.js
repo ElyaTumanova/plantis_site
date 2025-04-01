87b25dcccc76fd95595c9d1073cdc52094971e2d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-04-01 22:33:39 +0300

    Revert "datepicker"
    This reverts commit 9da955c2e4cb98cfb13bb4987aa0b59ca33e4a60.

--- assets/js/ajax-urgent-delivery.js
+++ assets/js/ajax-urgent-delivery.js
@@ -42,8 +42,8 @@ function ajaxGetUrgent() {
     urgentComent.append('Это срочная доставка');
     deliveryWrap.append(urgentComent);
   } else {
-    let urgentComentEl = document.querySelector('.checkout__comment_urgent');
-    urgentComentEl.remove();
+    console.log(urgentComent);
+    urgentComent.remove();
   }
 
   jQuery( function($){
