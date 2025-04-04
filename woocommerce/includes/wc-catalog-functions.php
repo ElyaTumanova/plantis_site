34bf7829e9782bcc0a2d50aa31e8e0b2886884b0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-04-04 15:52:04 +0300

    canonical

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -503,11 +503,10 @@ function remove_canonical() {
 
 //add_action( 'wpseo_head', 'remove_canonical', 4);
 
-add_filter( 'wpseo_canonical', '__return_false', 20);
+add_filter( 'wpseo_canonical', '__return_false', 9999);
 
 add_filter( 'wpseo_next_rel_link', '__return_false' );
 add_filter( 'wpseo_prev_rel_link', '__return_false' );
-add_filter( 'wpseo_canonical_rel_link', '__return_false' );
 
 
 //убирем канонакал, который выводит Load More плагин
