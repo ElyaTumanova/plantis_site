814fc0fc872fdff5c256eab29a97ecba2a8a532b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-04-14 21:50:51 +0300

    antibot

--- footer.php
+++ footer.php
@@ -86,13 +86,12 @@ if (!is_account_page()) {
 <div class="site-row">
     <div class="site-popup-inner welcome-pt-message" style="display: none;">
         <form method="post" enctype="multipart/form-data" action="">
-            <div class="site-form-title">Добро пожаловать</div>
             <div class="site-row">
-                <p class="site-form-text">Благодарим за посещение нашего ресурса.</p>
+                <p class="site-form-text">Мы используем куки, чтобы пользоваться сайтом было удобно.</p>
             </div>
             <div class="site-form-buttons site-form--center">
                 <div class="site-form-button">
-                    <a href="javascript:void(0);" class="site-btn--submit green welcome-pt-message-btn" onclick="$.fancybox.close();">Продолжить</a>
+                    <a href="javascript:void(0);" class="site-btn--submit green button welcome-pt-message-btn" onclick="$.fancybox.close();">Отлично</a>
                 </div>
             </div>
         </form>
