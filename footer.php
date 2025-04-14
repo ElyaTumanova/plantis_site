551c95022f4c275b4a62fb249a298a150bd65a4e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-04-14 11:41:11 +0300

    antibot

--- footer.php
+++ footer.php
@@ -105,7 +105,7 @@ if (!is_account_page()) {
         data: {
             ya_counter: {
                 status: false,
-                html: '<!-- Yandex.Metrika counter --> <script type="text/javascript" >	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};	m[i].l=1*new Date();	for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}	k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");	ym(87781741, "init", {			clickmap:true,			trackLinks:true,			accurateTrackBounce:true,			webvisor:true,			ecommerce:"dataLayer"	});	window.dataLayer = window.dataLayer || [];	</script>	<noscript><div><img src="https://mc.yandex.ru/watch/87781741" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->',
+                html: '<script type="text/javascript" >	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};	m[i].l=1*new Date();	for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}	k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");	ym(87781741, "init", {			clickmap:true,			trackLinks:true,			accurateTrackBounce:true,			webvisor:true,			ecommerce:"dataLayer"	});	window.dataLayer = window.dataLayer || [];	</script>	<noscript><div><img src="https://mc.yandex.ru/watch/87781741" style="position:absolute; left:-9999px;" alt="" /></div></noscript>',
                 area: 'head'
             }
         }
