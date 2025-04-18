a6cc4db2ff80ac14ad115a9110a2d93eed914431
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-04-18 13:11:14 +0300

    canonical debug

--- functions.php
+++ functions.php
@@ -33,6 +33,8 @@ require get_template_directory() . '/includes/metrika.php';
 /** Create Yandex XML */
 require get_template_directory() . '/includes/xml/create_yandex_xml.php';
 
+require get_template_directory() . '/includes/canonical-test.php';
+
 /** Add Woocommerce files */
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	require get_template_directory() . '/woocommerce/includes/wc-cart-functions.php';
