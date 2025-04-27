70039435e784368aa010f386a09aa3e4345f23aa
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-04-27 13:40:51 +0300

    treez stock status

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -418,7 +418,7 @@ function shop_only_instock_products( $meta_query, $query ) {
 			// ),
 			array(
 				'key' => '_manage_stock',
-				'value' => 'true',
+				'value' => true,
 				'compare' => '='
 			)
 		);
