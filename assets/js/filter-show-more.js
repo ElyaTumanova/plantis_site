ecc58cd5ac67d59ae7a60246c5d14d05b2eb08b3
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-05-12 16:45:10 +0300

    filter show more

--- assets/js/filter-show-more.js
+++ assets/js/filter-show-more.js
@@ -4,14 +4,31 @@ let diametrFilterItems = Array.from(diametrFilter.querySelectorAll('li'));
 
 console.log(diametrFilterItems);
 
-const newBtn = document.createElement("button");
+const showMoreBtn = document.createElement("button");
 
-newBtn.textContent = 'Показать все';
+showMoreBtn.textContent = 'Показать все';
 
 diametrFilter.appendChild(newBtn);
 
-diametrFilterItems.forEach((item, index, arr) => {
-if (index > 5) {
-   item.classList.add('d-none');
-  }
-})
+
+function hideFilterItems() {
+    diametrFilterItems.forEach((item, index, arr) => {
+        if (index > 5) {
+            item.classList.add('d-none');
+        }
+    });
+    showMoreBtn.addEventListener('click', showAllFilterItems, {once:true});
+}
+
+function showAllFilterItems() {
+    console.log('hi showAllFilterItems')
+    diametrFilterItems.forEach((item, index, arr) => {
+        item.classList.remove('d-none');
+    })
+    showMoreBtn.addEventListener('click', hideFilterItems, {once:true});
+}
+
+
+
+
+hideFilterItems();
