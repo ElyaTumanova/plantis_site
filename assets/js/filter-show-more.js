8e6ba1ba247bef6391e8557989b56fe3bf01b79b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-05-12 17:15:17 +0300

    filter show more

--- assets/js/filter-show-more.js
+++ assets/js/filter-show-more.js
@@ -1,3 +1,4 @@
+let filterValuesDefault = ['D22', 'D15', 'D18', 'D26', 'D30', 'D34', 'D37', '20x20', '30x30'];
 let diametrFilter = document.querySelector('.filter_diametr_gorshka');
 
 let diametrFilterItems = Array.from(diametrFilter.querySelectorAll('li'));
@@ -10,9 +11,8 @@ diametrFilter.appendChild(showMoreBtn);
 function hideFilterItems() {
     diametrFilterItems.forEach((item, index, arr) => {
         let filterValue = item.querySelector('input').value;
-        if (index > 5) {
+        if (!filterValuesDefault.includes(filterValue)) {
             item.classList.add('d-none');
-            console.log (filterValue);
         }
     });
     showMoreBtn.addEventListener('click', showAllFilterItems, {once:true});
