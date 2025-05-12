8929f7626bc82fc5844bfed2b09f14962dacd329
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-05-12 17:31:30 +0300

    filter show more

--- assets/js/filter-show-more.js
+++ assets/js/filter-show-more.js
@@ -1,13 +1,6 @@
 let filterValuesDefault = ['d22', 'd15', 'd18', 'd26', 'd30', 'd34', 'd37', '20x20', '30x30'];
 let diametrFilter = document.querySelector('.filter_diametr_gorshka');
 
-let diametrFilterItems = Array.from(diametrFilter.querySelectorAll('li'));
-
-let showMoreBtn = document.createElement("button");
-showMoreBtn.classList.add('filter-show-more-btn');
-diametrFilter.appendChild(showMoreBtn);
-
-
 function hideFilterItems() {
     diametrFilterItems.forEach((item, index, arr) => {
         let filterValue = item.querySelector('input').value;
@@ -26,9 +19,18 @@ function showAllFilterItems() {
     })
     showMoreBtn.addEventListener('click', hideFilterItems, {once:true});
     showMoreBtn.addEventListener('click', (event) => {
-        diametrFilter.scrollIntoView(true)
+        let sidebar = document.querySelector('.catalog__sidebar-filters');
+        sidebar.scrollIntoView(true);
     }, {once:true});
     showMoreBtn.textContent = 'Свернуть';
 }
 
-hideFilterItems();
+if(diametrFilter) {
+    let diametrFilterItems = Array.from(diametrFilter.querySelectorAll('li'));
+
+    let showMoreBtn = document.createElement("button");
+    showMoreBtn.classList.add('filter-show-more-btn');
+    diametrFilter.appendChild(showMoreBtn);
+
+    hideFilterItems();
+}
