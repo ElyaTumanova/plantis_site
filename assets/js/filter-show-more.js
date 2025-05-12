d441afa488f4a632663f860ddc0ec611d9720b7f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-05-12 16:28:38 +0300

    filter show more

---
+++ assets/js/filter-show-more.js
@@ -0,0 +1,9 @@
+let diametrFilter = document.querySelector('.filter_diametr_gorshka');
+
+console.log(diametrFilter);
+
+const newBtn = document.createElement("button");
+
+newBtn.textContent = 'Показать все';
+
+diametrFilter.appendChild(newBtn);
