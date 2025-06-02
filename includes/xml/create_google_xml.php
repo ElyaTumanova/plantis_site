6af044af4ad5dab72389bb215c40e55626a710db
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-02 15:53:21 +0300

    google xml

--- includes/xml/create_google_xml.php
+++ includes/xml/create_google_xml.php
@@ -104,7 +104,7 @@ function create_google_xml_btn () {
                 // Получаем цену товара
                 $sale = get_post_meta($allproduct->ID, '_sale_price', true);
                 if ($sale>0) {
-                    $google_xml .= "<g:sale_price>".get_post_meta($allproduct->ID,'_sale_price',true)." RUB</g:sale_price>
+                    $google_xml .= "<g:sale_price>".number_format(get_post_meta($allproduct->ID,'_sale_price',true),2,'.','')." RUB</g:sale_price>
                     <g:price>".get_post_meta($allproduct->ID,'_regular_price',true)." RUB</g:price>";
                 } else {
                     $google_xml .= "<g:price>".get_post_meta($allproduct->ID,'_price',true)." RUB</g:price>";
