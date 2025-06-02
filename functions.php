fb050e3d6894f08ddfcc9ed433ead1b7376679a1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-02 18:26:39 +0300

    google xml

--- functions.php
+++ functions.php
@@ -283,9 +283,12 @@ function plnt_check_page() {
 	//echo  basename( get_page_template() );
 	//echo wp_kses_data(WC()->cart->get_cart_contents_count());
 	//echo rand(5, 150);
-
+ 	date_default_timezone_set('Europe/Moscow');
 	$backorderdate = date( "d.m.y", strtotime('+1 week') );
 	echo ($backorderdate);
+	echo ('<br>');
+	echo (strtotime($backorderdate));
+	echo date('d.m.Y H:i:s', (strtotime($backorderdate)));
 	echo ('<br>');
 	echo date(DATE_ISO8601, strtotime($backorderdate));
 }
