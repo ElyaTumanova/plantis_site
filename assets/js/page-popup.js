122f3c1266bfa9832700ce7f1019084931fac3a9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-10 12:35:39 +0300

    gift card page

--- assets/js/page-popup.js
+++ assets/js/page-popup.js
@@ -4,6 +4,9 @@ const pagePopup = document.querySelector('.page-popup');
 const pageClosePopupBtn = document.querySelector('.page-popup__close');
 const pagePopupOverlay = document.querySelector('.page-popup__popup-overlay');
 
+//additional for gift-card
+const giftCardPopupContainer = document.querySelector('.page-popup__container_giftcard');
+
 if (pagePopup != null && pageOpenPopupBtn != null) {
     
     pageOpenPopupBtn.addEventListener ("click", (evt)=>{
@@ -32,6 +35,8 @@ if (pagePopup != null && pageOpenPopupBtn != null) {
 function toggle_page_popup () {
     pagePopup.classList.toggle ('popup_active');
     body.classList.toggle ('fix-body');
+    //additional for gift-card
+    giftCardPopupContainer.classList.toggle('d-none');
 };
 
 
