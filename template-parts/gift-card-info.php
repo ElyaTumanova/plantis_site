03e38c80b3c03527a45c9801d39b41f25b78ca54
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-10 15:20:09 +0300

    product page

--- template-parts/gift-card-info.php
+++ template-parts/gift-card-info.php
@@ -4,26 +4,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<div class="advantages">
-  <div class="advantages__wrap">
-    <img width="111" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/фото_зел.svg" class="advantages__image" alt="" loading="lazy">														
-    <div class="advantages__title">“Живые” фотографии</div>	
-    <div class="advantages__descr">Перед оплатой мы пришлём вам фотографии именно того растения, которое поедет к вам домой</div>
+<div class="giftcard-advantages">
+  <div class="giftcard-advantages__wrap">
+    <img width="111" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/фото_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">														
+    <!-- <div class="giftcard-advantages__title">“Живые” фотографии</div>	 -->
+    <div class="giftcard-advantages__descr">Подходит для физлиц и корпоративных клиентов</div>
   </div>
-  <div class="advantages__wrap">
-    <img width="110" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/пересадка_зел.svg" class="advantages__image" alt="" loading="lazy">
-    <div class="advantages__title">Бесплатная пересадка</div>
-    <div class="advantages__descr">Абсолютно бесплатно пересадим комнатное растение, если вы приобретаете его вместе с горшком из нашего каталога</div>		
+  <div class="giftcard-advantages__wrap">
+    <img width="110" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/пересадка_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+    <!-- <div class="giftcard-advantages__title">Бесплатная пересадка</div> -->
+    <div class="giftcard-advantages__descr">Гибкий номинал сертификата от 3000 до 100 000 рублей</div>		
   </div>
-  <div class="advantages__wrap">									
-    <img width="115" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/доставка_зел.svg" class="advantages__image" alt="" loading="lazy">														
-    <div class="advantages__title">Бережная доставка</div>
+  <div class="giftcard-advantages__wrap">									
+    <img width="115" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/доставка_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">														
+    <!-- <div class="giftcard-advantages__title">Бережная доставка</div> -->
     <?php $min_free_delivery = carbon_get_theme_option('min_free_delivery');?>
-    <div class="advantages__descr">Осуществляем доставку комнатных растений до двери. <?php if($min_free_delivery) { echo 'Если сумма покупки будет выше '.$min_free_delivery.' рублей, то делаем это бесплатно';}?></div>	
+    <div class="giftcard-advantages__descr">Можно использовать сертификат онлайн или в шоурумах</div>	
   </div>
-  <div class="advantages__wrap">		
-    <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="advantages__image" alt="" loading="lazy">												
-    <div class="advantages__title">Бесплатная консультация</div>
-    <div class="advantages__descr">Поможем подобрать нового друга, ответим на вопросы по уходу и останемся на связи с вами даже после покупки</div>
+  <div class="giftcard-advantages__wrap">		
+    <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">												
+    <!-- <div class="giftcard-advantages__title">Бесплатная консультация</div> -->
+    <div class="giftcard-advantages__descr">Можно оплачивать товары и услуги со скидкой</div>
+  </div>	
+  <div class="giftcard-advantages__wrap">		
+    <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">												
+    <!-- <div class="giftcard-advantages__title">Бесплатная консультация</div> -->
+    <div class="giftcard-advantages__descr">Можно использовать многократно в рамках номинала и срока действия</div>
+  </div>	
+  <div class="giftcard-advantages__wrap">		
+    <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">												
+    <!-- <div class="giftcard-advantages__title">Бесплатная консультация</div> -->
+    <div class="giftcard-advantages__descr">Можно отправить получателю сразу или выбрать дату</div>
   </div>	
 </div>
