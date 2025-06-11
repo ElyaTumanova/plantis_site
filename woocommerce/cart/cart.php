286f8faa3b3d5faa1c73b9c342eab3a1688e5f64
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-11 22:47:27 +0300

    backorder_date-info

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -122,21 +122,24 @@ do_action( 'woocommerce_before_cart' ); ?>
 								$qty = $cart_item[ 'quantity' ];
 								$stock_qty = $_product->get_stock_quantity();
 								global $plants_cat_id;
-								if ( $_product->backorders_allowed() && $qty > $stock_qty ) {
-									if (check_category($_product) === $plants_cat_id) {
-									?><p class="backorder_date-info">Доставка после <?php echo plnt_set_backorders_date();?></p>
-									<?php } else {
-										?><p class="backorder_date-info">Доставка со склада 3 - 7 дней</p>
-										<?php
-									}
-								} else {
-									?><p class="backorder_date-info">Доставка от 2-х часов</p>
-									<?php
-								}
 								if ( check_is_treez($_product) || check_is_lechuza($_product) ) {
 									?><p class="backorder_date-info">Доставка со склада 3 - 7 дней</p>
 									<?php
-								}	
+								}	else {
+									if ( $_product->backorders_allowed() && $qty > $stock_qty ) {
+										if (check_category($_product) === $plants_cat_id) {
+										?><p class="backorder_date-info">Доставка после <?php echo plnt_set_backorders_date();?></p>
+										<?php } else {
+											?><p class="backorder_date-info">Доставка со склада 3 - 7 дней</p>
+											<?php
+										}
+									} else {
+										?><p class="backorder_date-info">Доставка от 2-х часов</p>
+										<?php
+									}
+								}
+								
+								
 							?>
 							<!-- peresadka_init -->
 							<!-- <div class="cart__peresadka">
