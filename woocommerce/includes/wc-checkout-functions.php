57a95521d1fa98793a470ecd3ccbf4e2263ce6cb
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-11 22:28:49 +0300

    checkout

--- woocommerce/includes/wc-checkout-functions.php
+++ woocommerce/includes/wc-checkout-functions.php
@@ -313,6 +313,15 @@ Contents
         );
     }
 
+    add_filter( 'woocommerce_form_field' , 'elex_remove_checkout_optional_text', 10, 4 );
+    function elex_remove_checkout_optional_text( $field, $key, $args, $value ) {
+        if( is_checkout() && ! is_wc_endpoint_url() ) {
+            $optional = '&nbsp;<span class="optional">(' . esc_html__( 'optional', 'woocommerce' ) . ')</span>';
+            $field = str_replace( $optional, '', $field );
+        }
+        return $field;
+    } 
+
     // // сохряняем новое поле в заказе
 
 
