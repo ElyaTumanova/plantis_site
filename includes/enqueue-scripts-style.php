817fff3b96a23e702a953fb9c7104a3a6ef5fd78
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-14 18:18:59 +0300

    bundle css

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -107,7 +107,7 @@ if ( ! function_exists( 'ast_styles' ) ) {
 		// wp_enqueue_style( 'owl-css', get_template_directory_uri() .
 		//                              '/assets/css/owl.carousel.min.css', array(), null, 'all' );
 		wp_enqueue_style( 'bundle', get_template_directory_uri() .
-									'/assets/css/bundle.min.css', array(), null, 'all' ); //bundle
+									'/assets/css/bundle.css', array(), null, 'all' ); //bundle
 		wp_enqueue_style( 'swiper', get_template_directory_uri() .
 									'/assets/css/swiper.css', array(), null, 'all' ); //swiper
 		// wp_enqueue_style( 'general', get_template_directory_uri() .
