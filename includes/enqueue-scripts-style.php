f8bfc6591aea1634db44875a404fce345f11711e
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-14 15:25:41 +0300

    lazy load

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -57,8 +57,8 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 		wp_enqueue_script( 'login-popup', get_template_directory_uri() .
 											 '/assets/js/login-popup.js', array( 'jquery' ), filemtime(get_stylesheet_directory() .'/assets/js/login-popup.js'), true );
 
-		wp_enqueue_script( 'lazy-load', get_template_directory_uri() .
-		                                     '/assets/js/lazy-load.js', array( 'jquery' ), null, true ); // for lazy load
+		// wp_enqueue_script( 'lazy-load', get_template_directory_uri() .
+		//                                      '/assets/js/lazy-load.js', array( 'jquery' ), null, true ); // for lazy load
 
 		// wp_enqueue_script( 'progressive-image', get_template_directory_uri() .
 		                                    //  '/assets/js/progressive-image.js', array( 'jquery' ), null, true ); // for lazy load
