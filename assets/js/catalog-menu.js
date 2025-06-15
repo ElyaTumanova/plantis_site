22a028e2fa567261340cd3270f390f4f9fe5ea5f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-15 18:08:13 +0300

    menu

--- assets/js/catalog-menu.js
+++ assets/js/catalog-menu.js
@@ -1,7 +1,7 @@
 //раскрывает пункты меню в категориях на страницах каталога
 function setCatalogDropdown() {
   const dropdownCatalog = document.querySelectorAll('.catalog__dropdown');
-  console.log(dropdownCatalog);
+
   dropdownCatalog.forEach((el) => {
     const menu = el.querySelector('.catalog__dropdown-menu');
     const btn = el.querySelector('.menu__dropdown-arrow');
