7bf6bc1b22869ed5df362c0783aaa413f4afa162
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-16 13:42:51 +0300

    ukhod

---
+++ page-professionalnyj-uhod-za-rasteniyami.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="content-area">
+	<main id="main" class="site-main" role="main">
+		<header class="entry-header">
+			<h1 class="entry-title">Профессиональный уход за растениями</h1>                
+		</header>
+		<div class="page-ukhod">
+            
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
