0d1aac0b9422fdf6ed6a01afd029d5c215a28ee0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-18 17:25:02 +0300

    delivery

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -6,7 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 //СПОСОБЫ ДОСТАВКИ
 
 //задаем по умолчанию срочную доставку
-//add_action('wp_head','plnt_set_initials');
+add_action('wp_head','plnt_set_initials');
 
 function plnt_set_initials() {
     date_default_timezone_set('Europe/Moscow');
@@ -24,7 +24,6 @@ function plnt_set_initials() {
         }
     }
    
-
     WC()->session->set('isLate', '0' );
 
 };
