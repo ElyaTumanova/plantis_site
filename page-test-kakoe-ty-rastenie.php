65851082e60ec2f76745a1d6b6e18898734192da
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-23 17:10:15 +0300

    test

--- page-test-kakoe-ty-rastenie.php
+++ page-test-kakoe-ty-rastenie.php
@@ -3,6 +3,15 @@
 <div class="content-area">
 	<main id="main" class="site-main" role="main">
     <h1>This is test</h1>
+    <div>
+      <h2 class="test__question">Question A</h2>
+      <ul class = "test__answers">
+        <li class="test__answer">Answer A</li>
+        <li class="test__answer">Answer B</li>
+        <li class="test__answer">Answer C</li>
+        <li class="test__answer">Answer D</li>
+      </ul>
+    </div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
