8048505cc9829b8db15b4bd36b51c9e8ff5be346
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-23 17:06:26 +0300

    test

---
+++ page-test.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<div class="content-area">
+	<main id="main" class="site-main" role="main">
+    <h1>This is test</h1>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer();?>
