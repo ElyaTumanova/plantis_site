70639b76735ef997cc685e8af76494a060a3ef85
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-25 15:03:29 +0300

    test

--- page-test-kakoe-ty-rastenie.php
+++ page-test-kakoe-ty-rastenie.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <div class="content-area">
-	<main id="main" class="site-main" role="main">
-    <h1>This is test</h1>
+	<main id="main" class="site-main test-page">
+    <h1>Какое ты растение?</h1>
     <div class="test">
       <h2 class="test__question"></h2>
       <form class = "test__answers-form">
@@ -13,13 +13,13 @@
         <p class = "test__error d-none">Выберите вариант ответа</p>
       </form>
       <img src="" alt="" class = "test__image" loading="lazy">
-      <div class = "test__result d-none">
+    </div> 
+     <div class = "test__result d-none">
         <p class = "test__result-name"></p>
         <p class = "test__result-descr"></p>
         <img src="" alt="" class = "test__result-image" loading="lazy">
         <a href="" class = "test__result-link">Поделиться</a>
       </div>
-    </div> 
 	</main><!-- #main -->
 </div><!-- #primary -->
 
