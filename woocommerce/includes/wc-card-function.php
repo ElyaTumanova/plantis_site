7416aafbca53fdbfce373921e248dc087b175298
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-06-27 14:50:41 +0300

    badge skidka

--- woocommerce/includes/wc-card-function.php
+++ woocommerce/includes/wc-card-function.php
@@ -163,7 +163,7 @@ function truemisha_sale_badge() {
 		$percentage = ( ( $product->get_regular_price() - $product->get_sale_price() ) / $product->get_regular_price() ) * 100;
  
 	if ( $percentage > 0 ) {
-		echo '<div class="sale_badge"><span class="sale_badge-skidka">Скидка </span>' . round( $percentage ) . '%</div>';
+		echo '<div class="sale_badge"> - ' . round( $percentage ) . '%</div>';
 	}
 };
 
