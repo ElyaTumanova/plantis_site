ec8c459faaaf23db94838c57796c79a8ab6e2bf5
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-11 11:12:13 +0300

    prods slug

--- functions.php
+++ functions.php
@@ -349,7 +349,7 @@ function plnt_get_prods_data() {
                 array(
                     'taxonomy' => 'product_cat',
                     'field' => 'slug',
-                    'terms' => 'kashpo-treez',
+                    'terms' => 'iskusstvennye-rasteniya-treez',
                     // 'terms' => array('gorshki_i_kashpo', 'komnatnye-rasteniya'),
                     // 'operator' => 'NOT IN'
                 )
