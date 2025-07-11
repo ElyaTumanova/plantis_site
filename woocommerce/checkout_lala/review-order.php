05b3c19764b1cb6e06a327cb4d5b6cfb59a629f0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-11 20:09:12 +0300

    checkout

---
+++ woocommerce/checkout_lala/review-order.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Review order table
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/review-order.php.
+ * 
+ * MODIFIED FOR PLANTIS THEME
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://woo.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 5.2.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<table class="shop_table woocommerce-checkout-review-order-table">
+	<thead>
+		<tr>
+			<th class="product-name"><?php esc_html_e( 'Product', 'woocommerce' ); ?></th>
+			<th class="product-total"><?php esc_html_e( 'Сумма', 'woocommerce' ); ?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		do_action( 'woocommerce_review_order_before_cart_contents' );
+
+		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+			$parentCatId = check_category($_product);
+			$price = get_post_meta($cart_item['product_id'] , '_price', true);
+
+			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {	
+				?>
+				<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>" 
+				data-product_name="<?php echo $_product->get_name()?>" 
+				data-product_category="<?php echo get_the_category_by_ID($parentCatId)?>" 
+				data-product_quantity="<?php echo $cart_item['quantity']?>"
+				data-product_price="<?php echo $price?>"
+				>
+					<td class="product-name">
+						<?php echo wp_kses_post( apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) ) . '&nbsp;'; ?>
+						<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<?php 
+							$qty = $cart_item[ 'quantity' ];
+							$stock_qty = $_product->get_stock_quantity();
+							global $plants_cat_id;
+							if ( check_is_treez($_product) || check_is_lechuza($_product) ) {
+								?><p class="backorder_date-info backorder_date-info_late">Доставка со склада 3 - 7 дней</p>
+								<?php
+							}	else {
+								if ( $_product->backorders_allowed() && $qty > $stock_qty ) {
+									if (check_category($_product) === $plants_cat_id) {
+									?><p class="backorder_date-info backorder_date-info_late">Доставка после <?php echo plnt_set_backorders_date();?></p>
+									<?php } else {
+										?><p class="backorder_date-info backorder_date-info_late">Доставка со склада 3 - 7 дней</p>
+										<?php
+									}
+								} else {
+									?><p class="backorder_date-info">Доставка от 2-х часов</p>
+									<?php
+								}
+							}
+						?>
+					</td>
+					<td class="product-total">
+						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</td>
+				</tr>
+				<?php
+			}
+		}
+
+		do_action( 'woocommerce_review_order_after_cart_contents' );
+		?>
+	</tbody>
+	<tfoot>
+
+		<tr class="cart-subtotal">
+			<th><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+			<td><?php wc_cart_totals_subtotal_html(); ?></td>
+		</tr>
+
+		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
+			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+				<th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
+				<td><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+		<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+
+			<?php do_action( 'woocommerce_review_order_before_shipping' ); ?>
+
+			<?php wc_cart_totals_shipping_html(); ?>
+
+			<?php do_action( 'woocommerce_review_order_after_shipping' ); ?>
+
+		<?php endif; ?>
+
+		<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
+			<tr class="fee">
+				<th><?php echo esc_html( $fee->name ); ?></th>
+				<td><?php wc_cart_totals_fee_html( $fee ); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+		<?php if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) : ?>
+			<?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
+				<?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited ?>
+					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+						<th><?php echo esc_html( $tax->label ); ?></th>
+						<td><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php else : ?>
+				<tr class="tax-total">
+					<th><?php echo esc_html( WC()->countries->tax_or_vat() ); ?></th>
+					<td><?php wc_cart_totals_taxes_total_html(); ?></td>
+				</tr>
+			<?php endif; ?>
+		<?php endif; ?>
+
+		<?php do_action( 'woocommerce_review_order_before_order_total' ); ?>
+
+		<!-- <tr class="order-total">
+			<th><?php //esc_html_e( 'Total', 'woocommerce' ); ?></th>
+			<td><?php //wc_cart_totals_order_total_html(); ?></td>
+		</tr> -->
+
+		<?php do_action( 'woocommerce_review_order_after_order_total' ); ?>
+
+	</tfoot>
+</table>
