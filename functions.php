451dfbc0427bcbeca18a0a954d71a3752efd3eee
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-11 22:44:09 +0300

    yoast

--- functions.php
+++ functions.php
@@ -172,10 +172,27 @@ function plnt_set_constants_script() {
 	<?php
 }
 
-add_filter('wpseo_opengraph_image', 'plnt_opengraph_image');
-function plnt_opengraph_image($image) {
-   $image = 'https://plantis.shop/wp-content/uploads/2025/01/пересадка_мобnorm-копия.webp';
-   return $image;
+
+add_filter( 'wpseo_opengraph_image', 'plantis_default_og_image' );
+add_filter( 'wpseo_twitter_image', 'plantis_default_og_image' );
+add_filter( 'wpseo_schema_graph_pieces', 'plantis_schema_default_image', 11, 2 );
+
+function plantis_default_og_image( $image ) {
+	if ( empty( $image ) ) {
+		$image = 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp'; // путь к изображению
+	}
+	return $image;
+}
+
+function plantis_schema_default_image( $pieces, $context ) {
+	foreach ( $pieces as $piece ) {
+		if ( isset( $piece->context ) && $piece->context === 'mainEntity' ) {
+			if ( empty( $piece->image ) ) {
+				$piece->image = 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp';
+			}
+		}
+	}
+	return $pieces;
 }
 
 // FOR DEV
