0e07fe720c7c8ef4affa7e72a568b5c16ff8ba40
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-11 19:37:39 +0300

    delivery info pages

--- template-parts/delivery-info.php
+++ template-parts/delivery-info.php
@@ -7,48 +7,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php 
 // стоимость доставки
 
-global $local_pickup;
         
-global $delivery_inMKAD;
-global $delivery_outMKAD;
+// global $delivery_inMKAD;
+// global $delivery_outMKAD;
 
-global $delivery_courier;
-global $delivery_long_dist;
-
-// global $delivery_inMKAD_small;
-// global $delivery_outMKAD_small;
-// global $delivery_inMKAD_large;
-// global $delivery_outMKAD_large;
-
-// global $urgent_delivery_inMKAD; 
-// global $urgent_delivery_outMKAD; 
-// global $urgent_delivery_inMKAD_small; 
-// global $urgent_delivery_outMKAD_small;
-// global $urgent_delivery_inMKAD_large; 
-// global $urgent_delivery_outMKAD_large;
 
 $shipping_costs = plnt_get_shiping_costs();
    
 $in_mkad = $shipping_costs[$delivery_inMKAD];
 $out_mkad = $shipping_costs[$delivery_outMKAD];
-
-// $in_mkad_urg = $shipping_costs[$urgent_delivery_inMKAD];
-// $out_mkad_urg = $shipping_costs[$urgent_delivery_outMKAD];
-
-// $in_mkad_large = $shipping_costs[$delivery_inMKAD_large];
-// $out_mkad_large = $shipping_costs[$delivery_outMKAD_large];
-
-// $in_mkad_urg_large = $shipping_costs[$urgent_delivery_inMKAD_large];
-// $out_mkad_urg_large = $shipping_costs[$urgent_delivery_outMKAD_large];
-
-// $in_mkad_small = $shipping_costs[$delivery_inMKAD_small];
-// $out_mkad_small = $shipping_costs[$delivery_outMKAD_small];
-
-// $in_mkad_small_urg = $shipping_costs[$urgent_delivery_inMKAD_small];
-// $out_mkad_small_urg = $shipping_costs[$urgent_delivery_outMKAD_small];
-
-// $large_delivery_markup_in_mkad =  floatval(str_replace(' ', '', $in_mkad_large)) - floatval(str_replace(' ', '', $in_mkad));
-// $large_delivery_markup_out_mkad =  floatval(str_replace(' ', '', $out_mkad_large)) - floatval(str_replace(' ', '', $out_mkad));
 
 $large_markup_delivery_in_mkad = carbon_get_theme_option('large_markup_delivery_in_mkad');
 $large_markup_delivery_out_mkad = carbon_get_theme_option('large_markup_delivery_out_mkad');
