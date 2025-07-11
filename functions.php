564409b31eb0fa9fe5f1d0beaa513457a071b0f1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-11 11:20:26 +0300

    prods slug

--- functions.php
+++ functions.php
@@ -349,7 +349,7 @@ function plnt_get_prods_data() {
                 array(
                     'taxonomy' => 'product_cat',
                     'field' => 'slug',
-                    'terms' => 'ukhod',
+                    'terms' => 'kashpo-lechuza',
                     // 'terms' => array('gorshki_i_kashpo', 'komnatnye-rasteniya'),
                     // 'operator' => 'NOT IN'
                 )
