90afc1a4fe4bc8d9d4577b042ccd90118c0fc8f9
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-12 12:07:10 +0300

    checkout

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ require get_template_directory() . '/includes/metrika.php';
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	require get_template_directory() . '/woocommerce/includes/wc-helpers-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-cart-functions.php';
-	require get_template_directory() . '/woocommerce/includes/wc-shipping-functions.php';
+	//require get_template_directory() . '/woocommerce/includes/wc-shipping-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-checkout-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-custom-fields.php';
 	require get_template_directory() . '/woocommerce/includes/wc-function.php';
