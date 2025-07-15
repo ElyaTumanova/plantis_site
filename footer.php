b4a608f31df73bd90ed7ece0b04a53497bdd24bb
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-15 16:54:03 +0300

    speed debug

--- footer.php
+++ footer.php
@@ -49,7 +49,10 @@ $close_icon = carbon_get_theme_option('close_icon');
         <div class="side-cart">
             <h4 class="side-cart__title heading-2">Корзина</h4>
             <div class="modal-mob__close side-cart__close button"><?php echo $close_icon ?></div>      
-            <?php plnt_woocommerce_mini_cart();?> 
+            <?php 
+                $start = microtime(true);
+                plnt_woocommerce_mini_cart();
+                echo "<!-- Timing: mini-cart = " . round((microtime(true) - $start) * 1000, 2) . " ms -->"; ?>
         </div>
         <div class="side-cart__popup-overlay popup-overlay"></div>
     </div>
