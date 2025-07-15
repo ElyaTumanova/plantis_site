8568da53074f906cfd68f972f424d50585b30afe
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-15 13:45:39 +0300

    seo

--- includes/seo-support.php
+++ includes/seo-support.php
@@ -3,34 +3,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+add_filter( 'wpseo_schema_organization', 'fix_yoast_schema_image', 10, 2 );
+function fix_yoast_schema_image( $data, $context ) {
+	if ( empty( $data['image'] ) || ! is_array( $data['image'] ) || ! isset( $data['image']['url'] ) ) {
+        $image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
+		if ( $image_id ) {
+			$image = wp_get_attachment_image_src( $image_id );
+		}
+		$data['image'] = [
+			'url'    => $image[0],
+			'width'  => $image[1],
+			'height' => $image[2],
+		];
+	}
+	return $data;
+}
+
 add_filter( 'wpseo_opengraph_image', 'plnt_default_og_image');
 add_filter( 'wpseo_opengraph_image_width', 'plnt_change_opengraph_image_width');
 add_filter( 'wpseo_opengraph_image_height', 'plnt_change_opengraph_image_height');
 add_filter( 'wpseo_twitter_image', 'plantis_default_twitter_image',10,1 );
 add_filter( 'wpseo_schema_graph_pieces', 'plantis_schema_default_image', 11, 2 );
-
-// function plantis_default_og_image( $image,$presentation ) {
-    // 	if ( empty( $image ) ) {
-    // 		$image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
-    // 		if ( $image_id ) {
-    // 			$size = wp_get_attachment_image_src( $image_id, 'full' );
-    // 			if ( $size ) {
-    // 				return [
-    // 					'url' => $size[0],
-    // 					'width' => $size[1],
-    // 					'height' => $size[2],
-    // 				];
-    // 			}
-    // 		}
-
-    // 		// fallback: без размера
-    // 		return [
-    // 			'url' => 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp',
-    // 		];
-    // 	}
-
-    // 	return $image;
-    // }
 
 function plnt_default_og_image( $image,$presentation ) {
 	if ( empty( $image ) ) {
