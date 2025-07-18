4102bf9cf9b537499e4ae33ab86134ea2e9ee252
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-18 22:12:31 +0300

    menu

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -222,7 +222,8 @@ add_filter( 'wpseo_breadcrumb_single_link', 'plnt_change_breadcrumb_title', 10, 
 function plnt_change_breadcrumb_title( $link_output, $link ) {
     print_r (gettype($link));
     if(array_key_exists('taxonomy', $link)){
-       print_r ($link); 
+       print_r ($link['taxonomy']); 
+
     }
 	// if ( is_tax( 'pa_color' ) && isset( $link['text'] ) ) {
 	// 	if ( $link['text'] === single_term_title( '', false ) ) {
