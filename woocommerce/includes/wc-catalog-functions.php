05784bc68d4a92c2ad050dc7571f06b5dce26d67
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-18 14:53:34 +0300

    menu

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -220,7 +220,7 @@ function plnt_attribute_page_title($page_title) {
 //Изменение заголовка в хлебных крошках Yoast SEO #breadcrumb
 add_filter( 'wpseo_breadcrumb_single_link', 'plnt_change_breadcrumb_title', 10, 2 );
 function plnt_change_breadcrumb_title( $link_output, $link ) {
-    print_r ($link[0]);
+    print_r (gettype($link));
 	// if ( is_tax( 'pa_color' ) && isset( $link['text'] ) ) {
 	// 	if ( $link['text'] === single_term_title( '', false ) ) {
 	// 		$link_output['text'] = 'Горшки и кашпо цвета ' . $link['text'];
