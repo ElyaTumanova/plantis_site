7116aa6c67b7bd45947b877586f646bf60690fcb
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-18 22:18:30 +0300

    menu

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -225,7 +225,7 @@ function plnt_change_breadcrumb_title( $link_output, $link ) {
        print_r ($link); 
        print_r ($link_output); 
         if ($link['taxonomy'] == 'pa_color') {
-            $link['text'] = 'Горшки и кашпо цвета ' . $link_output;
+            $link['text'] = 'Горшки и кашпо цвета ' . $link['text'];
         }
     }
 	return $link;
