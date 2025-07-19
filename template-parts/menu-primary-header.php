141cbaaed2199b441d9326d2bfb5eead8a16e76c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-19 14:43:20 +0300

    menu

--- template-parts/menu-primary-header.php
+++ template-parts/menu-primary-header.php
@@ -31,8 +31,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                         <li class="header__main-submenu-item">
                             <a class="header__main-submenu-item_accent header__main-submenu-item_link" href="<?php echo site_url()?>/product-category/komnatnye-rasteniya/">
                                 Комнатные растения
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"/></svg>
-
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7 7h8.586L5.293 17.293l1.414 1.414L17 8.414V17h2V5H7v2z"/></svg>
                             </a>
                             
                         </li>
