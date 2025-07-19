be9dc34425fdb0f55ae0a2c2954be8c5d906cc2b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-19 15:54:12 +0300

    menu

--- includes/dev-support.php
+++ includes/dev-support.php
@@ -23,6 +23,9 @@ function plnt_dev_functions() {
     $args = array( 'taxonomy' => 'product_cat', 'parent' => $plants_treez_cat_id );  
     $terms = get_terms( $args ); 
     print_r($terms);
+    $category_thumbnail = get_term_meta(137, 'thumbnail_id', true);
+    $image = wp_get_attachment_url($category_thumbnail);
+    echo($image);
 	// $cats_for_check = [$plants_cat_id, $gorshki_cat_id, $ukhod_cat_id,$treez_cat_id, $treez_poliv_cat_id, $plants_treez_cat_id, $lechuza_cat_id, $peresadka_cat_id, $misc_cat_id];
 	// $cats_for_include = [];
 	// $cats_for_include_clean = [];
