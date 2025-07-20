afc86f79b47dbda79f159d31f81411cae5436cba
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-20 16:03:56 +0300

    menu

--- assets/js/catalog-menu.js
+++ assets/js/catalog-menu.js
@@ -1,44 +1,45 @@
-let catalogMenuDiv = document.querySelector('.catalog__sidebar-cats');
+// let catalogMenuDiv = document.querySelector('.catalog__sidebar-cats');
 
-let tempMenu = document.querySelectorAll('.catalog__sidebar-temp');
-let tempMenuItems = document.querySelectorAll('.catalog__sidebar-temp li');
+// let tempMenu = document.querySelectorAll('.catalog__sidebar-temp');
+// let tempMenuItems = document.querySelectorAll('.catalog__sidebar-temp li');
 
 // tempMenuItems.forEach(element => {
 //     element.addEventListener('click',plntAjaxShowCatalogMenu,{ once: true })
 // });
 
 
-function plntAjaxShowCatalogMenu(event) {
-  const data = new URLSearchParams();
-  data.append('action', 'plnt_show_catalog_menu');
+// function plntAjaxShowCatalogMenu(event) {
+//   const data = new URLSearchParams();
+//   data.append('action', 'plnt_show_catalog_menu');
 
-  fetch(woocommerce_params.ajax_url + '?' + data.toString(), {
-    method: 'GET',
-    credentials: 'same-origin'
-  })
-  .then(response => response.text())
-  .then(html => {
-    if (catalogMenuDiv) {
-      catalogMenuDiv.innerHTML = html;
-    }
-    // tempMenu.remove();
-    setCatalogDropdown();
-  })
-  .catch(error => {
-    console.error('AJAX error:', error);
-  });
-}
-
-
+//   fetch(woocommerce_params.ajax_url + '?' + data.toString(), {
+//     method: 'GET',
+//     credentials: 'same-origin'
+//   })
+//   .then(response => response.text())
+//   .then(html => {
+//     if (catalogMenuDiv) {
+//       catalogMenuDiv.innerHTML = html;
+//     }
+//     // tempMenu.remove();
+//     setCatalogDropdown();
+//   })
+//   .catch(error => {
+//     console.error('AJAX error:', error);
+//   });
+// }
 
 
 //раскрывает пункты меню в категориях на страницах каталога
 
 const dropdownCatalog = document.querySelectorAll('.catalog__dropdown');
+console.log(dropdownCatalog);
 
 dropdownCatalog.forEach((el) => {
 const menu = el.querySelector('.catalog__dropdown-menu');
 const btn = el.querySelector('.menu__dropdown-arrow');
+console.log(menu);
+console.log(btn);
 btn.addEventListener('click', function (event) {
     menu.classList.toggle('catalog__dropdown-menu_show');
     el.classList.toggle('catalog__dropdown_open');
