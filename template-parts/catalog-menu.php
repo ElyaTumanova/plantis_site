708fc7af111e6a7e573050ac207d148010efbf69
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-20 16:50:40 +0300

    menu

--- template-parts/catalog-menu.php
+++ template-parts/catalog-menu.php
@@ -6,9 +6,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <nav class='menu-catalog-container'>
     <ul class='catalog-menu'>
-    <?php get_catalog_submenu('gorshki_i_kashpo','/product-category/gorshki_i_kashpo/',2);?>
-    <?php get_catalog_submenu('kashpo-treez','/product-category/kashpo-treez/',3,['Treez Effectory ', 'Treez Ergo ']);?>
-    <?php get_catalog_submenu('kashpo-lechuza','/product-category/kashpo-lechuza/',2,['Кашпо Lechuza ']);?>
-    <?php get_catalog_submenu('ukhod','/product-category/ukhod/',2);?>
+        <?php get_catalog_submenu('ukhod','/product-category/komnatnye-rasteniya/',3);?>
+        <?php get_catalog_submenu('gorshki_i_kashpo','/product-category/gorshki_i_kashpo/',2);?>
+        <?php get_catalog_submenu('kashpo-treez','/product-category/kashpo-treez/',3,['Treez Effectory ', 'Treez Ergo ']);?>
+        <?php get_catalog_submenu('kashpo-lechuza','/product-category/kashpo-lechuza/',2,['Кашпо Lechuza ']);?>
+        <?php get_catalog_submenu('ukhod','/product-category/ukhod/',2);?>
     </ul>
 </nav>
