5c4fc42ff334b3dbbf4b4344f6ad2e01a812b458
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-24 13:58:23 +0300

    constants

--- functions.php
+++ functions.php
@@ -114,6 +114,21 @@ function plnt_set_constants_script() {
 		// shipping methods IDs
 		let deliveryInMKAD = '<?php echo $delivery_inMKAD; ?>';
 		let deliveryOutMKAD = '<?php echo $delivery_outMKAD; ?>';
+        let deliveryInMKADSmall = '<?php echo $delivery_inMKAD_small; ?>';
+		let deliveryOutMKADSmall = '<?php echo $delivery_outMKAD_small; ?>';
+		let deliveryInMKADLarge = '<?php echo $delivery_inMKAD_large; ?>';
+		let deliveryOutMKADLarge = '<?php echo $delivery_outMKAD_large; ?>';
+		let deliveryInMKADMedium = '<?php echo $delivery_inMKAD_medium; ?>';
+		let deliveryOutMKADMedium = '<?php echo $delivery_outMKAD_medium; ?>';
+
+		let deliveryInMKADUrg = '<?php echo $urgent_delivery_inMKAD; ?>';
+		let deliveryOutMKADUrg = '<?php echo $urgent_delivery_outMKAD; ?>';
+		let deliveryInMKADSmallUrg = '<?php echo $urgent_delivery_inMKAD_small; ?>';
+		let deliveryOutMKADSmallUrg = '<?php echo $urgent_delivery_outMKAD_small; ?>';
+		let deliveryInMKADLargeUrg = '<?php echo $urgent_delivery_inMKAD_large; ?>';
+		let deliveryOutMKADLargeUrg = '<?php echo $urgent_delivery_outMKAD_large; ?>';
+		let deliveryInMKADMediumUrg = '<?php echo $urgent_delivery_inMKAD_medium; ?>';
+		let deliveryOutMKADMediumUrg = '<?php echo $urgent_delivery_outMKAD_medium; ?>';
 
 		
 		
@@ -126,7 +141,24 @@ function plnt_set_constants_script() {
 		// shipping methods costs
 		let deliveryCostInMkad = '<?php echo $in_mkad; ?>';
 		let deliveryCostOutMkad = '<?php echo $out_mkad; ?>';
-	;
+        let deliveryCostInMkadUrg = '<?php echo $in_mkad_urg; ?>';
+		let deliveryCostOutMkadUrg = '<?php echo $out_mkad_urg; ?>';
+
+		let deliveryCostInMkadLarge = '<?php echo $in_mkad_large; ?>';
+		let deliveryCostOutMkadLarge = '<?php echo $out_mkad_large; ?>';
+		let deliveryCostInMkadLargeUrg = '<?php echo $in_mkad_urg_large; ?>';
+		let deliveryCostOutMkadLargeUrg = '<?php echo $out_mkad_urg_large; ?>';
+
+		let deliveryCostInMkadSmall = '<?php echo $in_mkad_small; ?>';
+		let deliveryCostOutMkadSmall = '<?php echo $out_mkad_small; ?>';
+		let deliveryCostInMkadSmallUrg = '<?php echo $in_mkad_small_urg; ?>';
+		let deliveryCostOutMkadSmallUrg = '<?php echo $out_mkad_small_urg; ?>';
+
+		let deliveryCostInMkadMedium = '<?php echo $in_mkad_medium; ?>';
+		let deliveryCostOutMkadMedium = '<?php echo $out_mkad_medium; ?>';
+		let deliveryCostInMkadMediumUrg = '<?php echo $in_mkad_medium_urg; ?>';
+		let deliveryCostOutMkadMediumUrg = '<?php echo $out_mkad_medium_urg; ?>';
+	
 
 		let deliveryLateMarkup = '<?php echo $late_markup_delivery; ?>';
 
