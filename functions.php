783a5d4df90cc9792b00524e03107e634a871489
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-24 14:50:08 +0300

    images data

--- functions.php
+++ functions.php
@@ -293,7 +293,7 @@ function plnt_get_images_data() {
     
     $query = new WP_Query( $args );
 
-    print_r($query);
+    print_r($query->posts[0]);
 
 //    $alt = get_image_alt_by_filename( 'aglaonema-krit-flejm-12-35-3' );
 //     echo $alt ? $alt : 'Alt не найден';
