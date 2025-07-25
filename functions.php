4a34470d14d0a45e229b8d455f6653a9d7983455
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-25 13:40:23 +0300

    images data

--- functions.php
+++ functions.php
@@ -299,10 +299,10 @@ function plnt_get_images_data() {
     // echo ("</pre>");
     $images = $query->posts;
     echo ("<pre>");
-    //foreach ($images as $img ) {
-        //$alt = get_post_meta( $img->ID, '_wp_attachment_image_alt', true );
-        // echo ("['id' => '");
-		// echo($img->ID);
+    foreach ($images as $img ) {
+        $alt = get_post_meta( $img->ID, '_wp_attachment_image_alt', true );
+        echo ("['id' => '");
+		echo($img->ID);
 		// echo ("', 'opisanie'=>'");
         // echo($img->post_content);
         // echo ("', 'podpis'=>'");
@@ -312,8 +312,8 @@ function plnt_get_images_data() {
         // echo ("', 'alt'=>'");
         // echo($img->$alt);
         // echo("'],");
- 		//echo ('<br>');
-    //}
+ 		echo ('<br>');
+    }
     echo ("</pre>");
     
     
