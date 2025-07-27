c10e2c3296f77f41265cfd6cd7009063abacda4b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-27 16:56:56 +0300

    upsells

--- includes/enqueue-scripts-style.php
+++ includes/enqueue-scripts-style.php
@@ -81,6 +81,8 @@ if ( ! function_exists( 'ast_scripts' ) ) {
 
 		wp_enqueue_script( 'cart-backorder-crossell', get_template_directory_uri() .
 		                                     '/assets/js/cart-backorder-crossell.js', array( 'jquery' ), filemtime(get_stylesheet_directory() .'/assets/js/cart-backorder-crossell.js'), true );
+		wp_enqueue_script( 'cart-upsells', get_template_directory_uri() .
+		                                     '/assets/js/cart-upsells.js', array( 'jquery' ), filemtime(get_stylesheet_directory() .'/assets/js/cart-upsells.js'), true );
 
 		wp_enqueue_script( 'metrikaGoal', get_template_directory_uri() .
 		                                     '/assets/js/metrikaGoal.js', array( 'jquery' ), filemtime(get_stylesheet_directory() .'/assets/js/metrikaGoal.js'), true );  //metrikaGoal Яндекс Метрика Yandex Metrika
