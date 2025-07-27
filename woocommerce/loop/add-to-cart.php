e564cb2678bb23d70276cb1a876a06717e612dbe
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-27 15:30:26 +0300

    peresadka

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -50,24 +50,44 @@ else {
 }
 //
 
-echo apply_filters(
-	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-	sprintf(
-		'<form action="%s" method="post" class="add_to_cart_from">
-    <button type="submit" data-quantity="%s" class="%s" %s>%s lalala</button>
-    </form>',
-    // esc_url( $product->add_to_cart_url() ),
-    esc_url( $url),                                                                         //added
-		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-		// esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-		esc_attr( $class ),                                                                     //added
-		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		// esc_html( $product->add_to_cart_text() )
-		esc_html( $text )                                                                       //added
-	),
-	$product,
-	$args
-);
+if ($parentCat !== $peresadka_cat_id) {
+    echo apply_filters(
+        'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+        sprintf(
+            '<form action="%s" method="post" class="add_to_cart_from">
+        <button type="submit" data-quantity="%s" class="%s" %s>%s</button>
+        </form>',
+        // esc_url( $product->add_to_cart_url() ),
+        esc_url( $url),                                                                         //added
+            esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+            // esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+            esc_attr( $class ),                                                                     //added
+            isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+            // esc_html( $product->add_to_cart_text() )
+            esc_html( $text )                                                                       //added
+        ),
+        $product,
+        $args
+    );
+} else {
+    echo apply_filters(
+    	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+    	sprintf(
+    		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+            // esc_url( $product->add_to_cart_url() ),
+            esc_url( $url),                                                                         //added
+    		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+    		// esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+    		esc_attr( $class ),                                                                     //added
+    		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+    		// esc_html( $product->add_to_cart_text() )
+    		esc_html( $text )                                                                       //added
+    	),
+    	$product,
+    	$args
+    );
+}
+
 
 // echo apply_filters(
 // 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
