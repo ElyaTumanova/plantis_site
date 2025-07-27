6331ce5ed55bfba2505a4544c87501d9787983bc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-27 11:52:36 +0300

    yoast data

--- functions.php
+++ functions.php
@@ -385,13 +385,14 @@ function plnt_get_yoast_data() {
 
     global $wpdb;
     echo('<pre>');
-    //print_r($wpdb->tables);
+    print_r($wpdb->tables);
     //print_r($wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->aioseo_posts;" ));
     //print_r($wpdb->get_results( "SELECT * FROM $wpdb->aioseo_posts WHERE ID = '18'"));
     //print_r($wpdb->get_results( "SELECT * FROM $wpdb->posts WHERE ID = '33739'"));
-    print_r(get_post_meta( 33739, '_yoast_wpseo_title'));
-    print_r(get_post_meta( 33739, '_yoast_wpseo_metadesc'));
-    print_r(get_post_meta( 838, '_yoast_wpseo_title'));
+    print_r($wpdb->get_results( "SELECT * FROM $wpdb->terms WHERE ID = '33739'"));
+    // print_r(get_post_meta( 33739, '_yoast_wpseo_title'));
+    // print_r(get_post_meta( 33739, '_yoast_wpseo_metadesc'));
+    // print_r(get_post_meta( 838, '_yoast_wpseo_title'));
 
 
 	// $term = get_term( $plants_cat_id, 'product_cat');
