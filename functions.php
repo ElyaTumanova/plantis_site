e32f24b5121f87b42752abe44ed6b70b6f670ebc
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-27 11:32:32 +0300

    yoast data

--- functions.php
+++ functions.php
@@ -386,7 +386,8 @@ function plnt_get_yoast_data() {
     global $wpdb;
     echo('<pre>');
     //print_r($wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->aioseo_posts;" ));
-    print_r($wpdb->get_results( "SELECT * FROM $wpdb->aioseo_posts"));
+    //print_r($wpdb->get_results( "SELECT * FROM $wpdb->aioseo_posts"));
+    print_r($wpdb->get_results( "SELECT * FROM $wpdb->posts"));
 
 
 	// $term = get_term( $plants_cat_id, 'product_cat');
