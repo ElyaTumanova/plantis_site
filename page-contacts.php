59ab1cfee50919b220ca4b65c51106b882cd4225
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-29 13:58:19 +0300

    contacts

--- page-contacts.php
+++ page-contacts.php
@@ -11,17 +11,17 @@ get_header(); ?>
 		</header>
 		<div class="contacts__wrap info__content">
 			<div class="contacts__phones">
-				<h2 class="heading-2">Телефон</h2>
+				<!-- <h2 class="heading-2">Телефон</h2> -->
 				<p><a class="contacts__phone" href="tel:+78002015790">8 800 201 57 90</a></p>
 				<p><a class="contacts__phone" href="tel:+79647687944">8 964 768 79 44</a></p>
-				<div class="contacts__social-media">
-					<?php get_template_part('template-parts/social-media-btns');?>
-				</div>
 			</div>
-		
+            
 			<div class="contacts__mail">
-				<h2 class="heading-2">Почта</h2>
+                <!-- <h2 class="heading-2">Почта</h2> -->
 				<a class="contacts__mail-link" href="mailto:INFO@PLANTIS.SHOP">INFO@PLANTIS.SHOP</a>
+                <div class="contacts__social-media">
+                    <?php get_template_part('template-parts/social-media-btns');?>
+                </div>
 			</div>
 		
 			<div class="contacts__work-hours">
