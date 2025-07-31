85d25cd2219ef6578a41113a2751197c782c09ab
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-07-31 14:07:06 +0300

    orders

--- functions.php
+++ functions.php
@@ -417,31 +417,31 @@ function get_orders_meta() {
     $where_exclude = implode(" AND pm.meta_key NOT LIKE ", array_map(function($v){ return "'$v%'"; }, $exclude));
 
     echo ('<pre>');
-    print_r($wpdb->get_results( "SELECT * FROM {$wpdb->postmeta} pm
-        JOIN {$wpdb->posts} p ON p.ID = pm.post_id
-        WHERE p.post_type = 'shop_order'"));
-
-    // print_r($wpdb->get_results( "
-    //     SELECT pm.meta_key AS meta_field,
-    //            COUNT(*) AS usage_count,
-    //            SUBSTRING_INDEX(GROUP_CONCAT(pm.meta_value SEPARATOR '||'), '||', 1) AS example_value
-    //     FROM {$wpdb->postmeta} pm
+    // print_r($wpdb->get_results( "SELECT * FROM {$wpdb->postmeta} pm
     //     JOIN {$wpdb->posts} p ON p.ID = pm.post_id
-    //     WHERE p.post_type = 'shop_order'
-    //     AND pm.meta_key NOT LIKE '_edit%' 
-    //     AND pm.meta_key NOT LIKE '_order_total%'
-    //     AND pm.meta_key NOT LIKE '_order_version%'
-    //     AND pm.meta_key NOT LIKE '_customer_user%'
-    //     AND pm.meta_key NOT LIKE '_download%' 
-    //     AND pm.meta_key NOT LIKE '_wc_reserved_stock%'
-    //     AND pm.meta_key NOT LIKE '_wc_deposit%'
-    //     AND pm.meta_key NOT LIKE '_stripe%'
-    //     AND pm.meta_key NOT LIKE '_paypal%'
-    //     AND pm.meta_key NOT LIKE '_refund%'
-    //     $where_exclude
-    //     GROUP BY pm.meta_key
-    //     ORDER BY usage_count DESC
-    // "));
+    //     WHERE p.post_type = 'shop_order'"));
+
+    print_r($wpdb->get_results( "
+        SELECT pm.meta_key AS meta_field,
+               COUNT(*) AS usage_count,
+               SUBSTRING_INDEX(GROUP_CONCAT(pm.meta_value SEPARATOR '||'), '||', 1) AS example_value
+        FROM {$wpdb->postmeta} pm
+        JOIN {$wpdb->posts} p ON p.ID = pm.post_id
+        WHERE p.post_type = 'shop_order'
+        AND pm.meta_key NOT LIKE '_edit%' 
+        AND pm.meta_key NOT LIKE '_order_total%'
+        AND pm.meta_key NOT LIKE '_order_version%'
+        AND pm.meta_key NOT LIKE '_customer_user%'
+        AND pm.meta_key NOT LIKE '_download%' 
+        AND pm.meta_key NOT LIKE '_wc_reserved_stock%'
+        AND pm.meta_key NOT LIKE '_wc_deposit%'
+        AND pm.meta_key NOT LIKE '_stripe%'
+        AND pm.meta_key NOT LIKE '_paypal%'
+        AND pm.meta_key NOT LIKE '_refund%'
+        $where_exclude
+        GROUP BY pm.meta_key
+        ORDER BY usage_count DESC
+    "));
     echo ('</pre>');
 
 }
