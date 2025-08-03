f008d99eac6e26a4927e24e4be552e2c0a318274
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-03 16:42:12 +0300

    images

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 		<section class="error-404 not-found">
 						
 			<div class="page-content">
-				<img width="800" height="286" src="<?php get_template_directory_uri()?> '/images/404.png" alt="Такой страницы нет">
+				<img width="800" height="286" src="<?php echo get_template_directory_uri()?> '/images/404.png" alt="Такой страницы нет">
 				<p>Упс! Похоже что-то пошло не так…</p>
 				<p>Давайте начнем сначала!</p>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button" role="button">На главную</a>
