e02d6f15ce88f88b555b9fccdebadfc2044f8588
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-03 16:40:17 +0300

    images

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@
 		<section class="error-404 not-found">
 						
 			<div class="page-content">
-				<img width="800" height="286" src="https://plantis.shop/wp-content/uploads/2022/01/404_5-1024x366.png" alt="" srcset="https://plantis.shop/wp-content/uploads/2022/01/404_5-1024x366.png 1024w, https://plantis.shop/wp-content/uploads/2022/01/404_5-680x243.png 680w, https://plantis.shop/wp-content/uploads/2022/01/404_5-300x107.png 300w, https://plantis.shop/wp-content/uploads/2022/01/404_5-768x274.png 768w, https://plantis.shop/wp-content/uploads/2022/01/404_5-1536x549.png 1536w, https://plantis.shop/wp-content/uploads/2022/01/404_5-2048x732.png 2048w, https://plantis.shop/wp-content/uploads/2022/01/404_5-600x214.png 600w, https://plantis.shop/wp-content/uploads/2022/01/404_5.png 1600w" sizes="(max-width: 800px) 100vw, 800px">
+				<img width="800" height="286" src="get_template_directory_uri() . '/images/404.png" alt="Такой страницы нет">
 				<p>Упс! Похоже что-то пошло не так…</p>
 				<p>Давайте начнем сначала!</p>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="button" role="button">На главную</a>
