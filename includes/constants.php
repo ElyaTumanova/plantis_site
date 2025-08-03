8c305b00567593be7eb425e609295136d975aa84
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-03 17:09:22 +0300

    images

--- includes/constants.php
+++ includes/constants.php
@@ -169,7 +169,7 @@ if($site === 'https://plantis-shop.ru') {
 
 /* Изображения и иконки */
 
-$filter_icon = get_template_directory_uri() . '/images/icons/filter_new.svg";
+$filter_icon = get_template_directory_uri() . '/images/icons/filter_new.svg';
 
 
 //ЗАДАЕМ КОНСТАНТЫ ДЛЯ JS
