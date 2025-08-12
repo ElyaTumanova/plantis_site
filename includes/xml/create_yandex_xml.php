d6b4d99b0595bbcbd14640a890bd5ac4f4187dff
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-12 22:05:14 +0300

    feed

--- includes/xml/create_yandex_xml.php
+++ includes/xml/create_yandex_xml.php
@@ -1,7 +1,7 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
-}
+// if ( ! defined( 'ABSPATH' ) ) {
+// 	exit; // Exit if accessed directly
+// }
 
 global $plants_cat_id;
 global $gorshki_cat_id;
