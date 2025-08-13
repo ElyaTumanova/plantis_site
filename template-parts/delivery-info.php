0ceaf5651b202c5a4f83451d20b8f25a4264fc04
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-13 16:01:23 +0300

    product

--- template-parts/delivery-info.php
+++ template-parts/delivery-info.php
@@ -13,6 +13,7 @@ global $delivery_outMKAD;
 
 $shipping_costs = plnt_get_shiping_costs();
 print_r($shipping_costs);
+echo($delivery_outMKAD);
    
 $in_mkad = $shipping_costs[$delivery_inMKAD];
 $out_mkad = $shipping_costs[$delivery_outMKAD];
