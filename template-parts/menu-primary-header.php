4153803eda6b8ee88ca07bb2ea8d6aa046f8977b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-13 22:01:45 +0300

    menu

--- template-parts/menu-primary-header.php
+++ template-parts/menu-primary-header.php
@@ -33,7 +33,7 @@ $arrow_icon = carbon_get_theme_option('arrow_icon');
                         <li class="header__main-submenu-item">
                             <a class="header__main-submenu-item_accent header__main-submenu-item_link header__main-submenu-item_image" 
                             data-cat_id = <?php 
-                            $cat = get_category_by_slug('category-name'); 
+                            $cat = get_category_by_slug('komnatnye-rasteniya'); 
                             $id = $cat->term_id;
                             echo $id?> 
                             href="<?php echo site_url()?>/product-category/komnatnye-rasteniya/">
