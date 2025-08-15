06b4048ec6c606318845e030ca942babfbc51fe1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-15 16:23:33 +0300

    menu

--- assets/js/header-catalog-menu.js
+++ assets/js/header-catalog-menu.js
@@ -132,6 +132,8 @@ function openAZPlantsList(event) {
 
 function closeAZPlantsList() {
     closeAllSubmenu();
+    headerCatalogWrap.querySelector('[data-menu = "menu_item_plants"]').classList.add('header__main-submenu_show-slow');
     showSubmenu('menu_item_plants');
+    headerCatalogWrap.querySelector('[data-menu = "menu_item_plants"]').classList.remove('header__main-submenu_show-slow');
 }
 
