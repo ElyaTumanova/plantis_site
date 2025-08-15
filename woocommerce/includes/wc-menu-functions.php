21a608adf327e7c5abd449628aa1061727de728d
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-15 16:47:26 +0300

    menu

--- woocommerce/includes/wc-menu-functions.php
+++ woocommerce/includes/wc-menu-functions.php
@@ -158,10 +158,17 @@ function get_az_palnts_submenu() {
         return strcmp($a->name, $b->name);
     }
     usort($lowest_cats, "sortByName");
+    $words_to_remove = ['(Цитрофортунелла)']
     foreach ( $lowest_cats as $cat ) {
+        $name = $term ->name;
+        if($words_to_remove && $clean_cat_name) {
+            foreach ($words_to_remove as $word) {
+                $name = str_replace($word,'',$name);
+            }
+        }
         ?>
         <li class="header__main-submenu-item">
-            <a href="<?php echo get_term_link($cat->term_id,'product_cat')?>"><?php echo $cat->name?></a>
+            <a href="<?php echo get_term_link($cat->term_id,'product_cat')?>"><?php echo $name?></a>
         </li> 
         <?php
     }
