bb18094ca4208fb9415cd7ad33cbceafed07b9be
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-16 11:57:55 +0300

    header

--- header.php
+++ header.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> itemscope itemtype="https://schema.org/Store">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta itemprop="image" content="<?php echo carbon_get_theme_option('logo') ?>">
+    <meta itemprop="name" content="Plantis">
+    <meta itemprop="telephone" content="8 800 201 57 90">
+    <meta itemprop="address" content="г. Москва, ул. Мещерякова, д.3. м. Тушинская">
+    <meta itemprop="priceRange" content="100 - 40000">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	
