a5671e9db9eb51c77f657e5b0f3b13a3708687e0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-17 16:32:57 +0300

    catalog

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -273,7 +273,7 @@ add_filter('woocommerce_product_loop_start', function ($html) {
         1
     );
 
-    $html .= '<meta itemprop="name" content="Каталог">
+    $html .= '<meta itemprop="name" content="'echo woocommerce_page_title();'">
    <meta itemprop="description" content="Описание каталога">';
 
     return $html;
