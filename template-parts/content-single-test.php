0caba83a5b710f66218385eb23e94da4ab251645
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-21 15:33:17 +0300

    test results

--- template-parts/content-single-test.php
+++ template-parts/content-single-test.php
@@ -1,9 +1,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-        <p>hello</p>
-
+		<?php the_title( '<h1 class="entry-title">Поздравляем! Вы ', '</h1>' ); ?>
 		<div class="entry-meta">
 
 		</div><!-- .entry-meta -->
