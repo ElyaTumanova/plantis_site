a77db11664d81db556c17588a4ac577c00fef265
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-22 17:00:41 +0300

    test results

--- template-parts/products/products-plants-test.php
+++ template-parts/products/products-plants-test.php
@@ -47,7 +47,7 @@ if ($cat_exist) {
                         <?php add_filter('woocommerce_post_class', 'plnt_add_class_loop_test_swiper', 10, 3);
                             global $product;
                             function plnt_add_class_loop_test_swiper($clasess, $product){
-                                $clasess[] = 'swiper-slide';
+                                $clasess[] = 'swiper-slide2';
                                 return $clasess;
                             };?>
                         <?php wc_get_template_part( 'content', 'product' ); ?>
