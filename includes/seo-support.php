a5e6ff0c1e8bd7a532d1d51c122e451a7f768518
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-27 15:42:59 +0300

    test

--- includes/seo-support.php
+++ includes/seo-support.php
@@ -3,100 +3,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-//add_filter( 'wpseo_schema_organization', 'fix_yoast_schema_image', 10, 2 );
-function fix_yoast_schema_image( $data, $context ) {
-	if ( empty( $data['image'] ) || ! is_array( $data['image'] ) || ! isset( $data['image']['url'] ) ) {
-        $image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
-		if ( $image_id ) {
-			$image = wp_get_attachment_image_src( $image_id );
-		}
-		$data['image'] = [
-			'url'    => $image[0],
-			'width'  => $image[1],
-			'height' => $image[2],
-		];
-	}
-	return $data;
+/*--------------------------------------------------------------
+# Open Graph meta tags
+--------------------------------------------------------------*/
+
+
+function my_custom_opengraph_tags() {
+    if (is_page('test-kakoe-ty-rastenie')) { 
+        ?>
+        <meta property="og:title" content="Пройди тест — Какое ты растение?" />
+        <meta property="og:description" content="Узнай, какое растение тебе ближе всего!" />
+        <meta property="og:image" content="https://dev.plantis-shop.ru/wp-content/themes/plantis_site/images/test/test.png" />
+        <meta property="og:url" content="<?php echo get_site_url() . '/test-kakoe-ty-rastenie'; ?>" />
+        <meta property="og:type" content="website" />
+        <?php
+    }
 }
+add_action('wp_head', 'my_custom_opengraph_tags');
 
-//add_filter( 'wpseo_schema_graph_pieces', 'fix_yoast_schema_images_globally', 10, 2 );
-function fix_yoast_schema_images_globally( $pieces, $context ) {
-
-	foreach ( $pieces as &$piece ) {
-
-         // Обрабатываем только массивы
-		if ( ! is_array( $piece ) ) {
-			continue;
-		}
-
-		if ( isset( $piece['@type'] ) && isset( $piece['image'] ) && ! is_array( $piece['image'] ) ) {
-            $image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
-            if ( $image_id ) {
-                $image = wp_get_attachment_image_src( $image_id );
-            }
-			$piece['image'] = [
-				'url'    => $image[0],
-                'width'  => $image[1],
-                'height' => $image[2],
-			];
-		}
-	}
-	return $pieces;
-}
-
-// add_filter( 'wpseo_opengraph_image', 'plnt_default_og_image');
-// add_filter( 'wpseo_opengraph_image_width', 'plnt_change_opengraph_image_width');
-// add_filter( 'wpseo_opengraph_image_height', 'plnt_change_opengraph_image_height');
-// add_filter( 'wpseo_twitter_image', 'plantis_default_twitter_image',10,1 );
-// add_filter( 'wpseo_schema_graph_pieces', 'plantis_schema_default_image', 11, 2 );
-
-function plnt_default_og_image( $image ) {
-	if ( empty( $image ) ) {
-		$image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
-		if ( $image_id ) {
-			$image = wp_get_attachment_image_src( $image_id )[0];
-		}
-	}
-
-	return $image;
-}
-function plnt_change_opengraph_image_width( $width ) {
-    if ( empty( $width ) ) {
-		$image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
-		if ( $image_id ) {
-			$width = wp_get_attachment_image_src( $image_id )[1];
-		}
-	}
-    return $width;
-}
-function plnt_change_opengraph_image_height( $height  ) {
-    if ( empty( $height  ) ) {
-		$image_id = attachment_url_to_postid( 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp' );
-		if ( $image_id ) {
-			$height  = wp_get_attachment_image_src( $image_id )[2];
-		}
-	}
-    return $height ;
-}
-
-function plantis_default_twitter_image( $image ) {
-	if ( empty( $image ) ) {
-		$image = 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp'; // путь к изображению
-	}
-	return $image;
-}
-
-function plantis_schema_default_image( $pieces, $context ) {
-	foreach ( $pieces as $piece ) {
-		if ( isset( $piece->context ) && $piece->context === 'mainEntity' ) {
-			if ( empty( $piece->image ) ) {
-				$piece->image = 'https://plantis-shop.ru/wp-content/uploads/2025/07/mainbannermob.webp';
-			}
-		}
-	}
-	return $pieces;
-}
-
+/*--------------------------------------------------------------
+# Shcema org
+--------------------------------------------------------------*/
 // отключаем schema.org в Yoast
 add_filter( 'wpseo_json_ld_output', '__return_false' );
 
