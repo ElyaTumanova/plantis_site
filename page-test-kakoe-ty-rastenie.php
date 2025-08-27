f0f2a58f4db1a1e17b046f1e8e82b9ab6202c286
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-27 17:29:22 +0300

    test

--- page-test-kakoe-ty-rastenie.php
+++ page-test-kakoe-ty-rastenie.php
@@ -10,7 +10,7 @@
             <?php echo get_template_directory_uri() .'/images/test/test_cover_long.webp'?> 600w,
             <?php echo get_template_directory_uri() .'/images/test/test_cover.webp'?> 1200w"
             sizes="
-            (max-width: 767px) 500px,
+            (max-width: 767px) 800px,
             767px"
             alt="Тест - Какое ты комнатное растение?">
         <div class="test__disclaimer-text test__disclaimer-text_desktop">
