6543dd458f753366ec183812caf8b46b946f1e2f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-08-29 15:33:20 +0300

    cats grid

--- template-parts/plants-cats-grid.php
+++ template-parts/plants-cats-grid.php
@@ -6,39 +6,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <div class="cats-grid">
     <a href="<?php echo get_site_url();?>/product-category/komnatnye-rasteniya/palms/" class="cats-grid__inner cats-grid__inner_palms">
-        <h3 class="cats-grid__title">Пальмы</h3>
+        <!-- <h3 class="cats-grid__title">Пальмы</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/palms.webp'?>" class="cats-grid__img" alt="Пальмы">
     </a>
     <a href="<?php echo get_site_url();?>/product-category/komnatnye-rasteniya/fikusy/" class="cats-grid__inner cats-grid__inner_fikus">
-        <h3 class="cats-grid__title">Фикусы</h3>
+        <!-- <h3 class="cats-grid__title">Фикусы</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/fikus.webp' ?>" class="cats-grid__img" alt="Фикусы">
     </a>
     <a href="<?php echo get_site_url();?>/product-category/komnatnye-rasteniya/dekorativno-listvennye/" class="cats-grid__inner cats-grid__inner_lisv">
-        <h3 class="cats-grid__title">Декоративно-лиственные</h3>
+        <!-- <h3 class="cats-grid__title">Декоративно-лиственные</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/decor.webp' ?>" class="cats-grid__img" alt="Декоративно-лиственные">
     </a>
     <a href="<?php echo get_site_url();?>/product-tag/napolnye/" class="cats-grid__inner cats-grid__inner_napol">
-        <h3 class="cats-grid__title">Напольные</h3>
+        <!-- <h3 class="cats-grid__title">Напольные</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/napol.webp' ?>" class="cats-grid__img" alt="Напольные">
     </a>
     <a href="<?php echo get_site_url();?>/product-category/komnatnye-rasteniya/dekorativno-cvetushchie/" class="cats-grid__inner cats-grid__inner_cvetush">
-        <h3 class="cats-grid__title">Цветущие</h3>
+        <!-- <h3 class="cats-grid__title">Цветущие</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/cvet.webp' ?>" class="cats-grid__img" alt="Цветущие">
     </a>
     <a href="<?php echo get_site_url();?>/product-category/komnatnye-rasteniya/lianas/" class="cats-grid__inner cats-grid__inner_lianas">
-        <h3 class="cats-grid__title">Лианы</h3>
+        <!-- <h3 class="cats-grid__title">Лианы</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/lians.webp' ?>" class="cats-grid__img" alt="Лианы">
     </a>
     <a href="<?php echo get_site_url();?>/product-tag/novichkam/" class="cats-grid__inner cats-grid__inner_neprikhotliv">
-        <h3 class="cats-grid__title">Неприхотливые</h3>
+        <!-- <h3 class="cats-grid__title">Неприхотливые</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/nepr.webp' ?>" class="cats-grid__img" alt="Неприхотливые растения">
     </a>
     <a href="<?php echo get_site_url();?>/product-category/komnatnye-rasteniya/succulent/" class="cats-grid__inner cats-grid__inner_succulent">
-        <h3 class="cats-grid__title">Суккуленты</h3>
+        <!-- <h3 class="cats-grid__title">Суккуленты</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/succ.webp' ?>" class="cats-grid__img" alt="Суккуленты">
     </a>
     <a href="<?php echo get_site_url();?>/product-tag/pet-friendly/" class="cats-grid__inner cats-grid__inner_petfriendly">
-        <h3 class="cats-grid__title">Pet Friendly</h3>
+        <!-- <h3 class="cats-grid__title">Pet Friendly</h3> -->
         <img src="<?php echo get_template_directory_uri() .'/images/cats-grid/pet.webp' ?>" class="cats-grid__img" alt="Pet Friendly">
     </a>
 </div>
