188fddca48ef11305a85b43fdf0c0d39e7f07932
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-02 11:39:49 +0300

    noindex

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -666,7 +666,7 @@ function move_to_top_on_pagination() {
 add_action('woocommerce_after_main_content', 'move_to_top_on_pagination');
 
 // добавляем директивы ноиндекс, фоллоу для страниц пагинации, начиная со 2 #SEO
-add_filter( 'wpseo_robots', 'filter_wpseo_robots' );
+add_filter( 'wpseo_robots', 'filter_wpseo_robots', 20, 1 );
 
 /* Yoast SEO -  add noindex, follow for paginated pages */
 function filter_wpseo_robots( $robotsstr ) {
