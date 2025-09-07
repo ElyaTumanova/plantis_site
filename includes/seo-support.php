f0f61df7da1151821ac266350fa0d460ce2777b6
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-07 16:10:47 +0300

    seo

--- includes/seo-support.php
+++ includes/seo-support.php
@@ -61,7 +61,7 @@ add_action('wp_head', function() {
         $gen = get_query_var('gen');
         $plant = get_query_var('plant');
         $img = $plants_by_slug[$plant]['image'][$gen];
-        echo '<meta property="og:image" content="'. $img . "\n";
+        echo '<meta property="og:image" content="'. $img .'/>' . "\n";
         echo '<meta property="og:image:width" content="1200" />' . "\n";
         echo '<meta property="og:image:height" content="630" />' . "\n";
     }
