0bd13f5c9446d7a4c6b02192c6a9ee6c807d2624
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 22:44:11 +0300

    gift

--- woocommerce/single-product/add-to-cart/gift-card-add-to-cart.php
+++ woocommerce/single-product/add-to-cart/gift-card-add-to-cart.php
@@ -23,7 +23,7 @@ $product_id = yit_get_product_id( $product );
 		<?php woocommerce_quantity_input( array( 'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : 1 ) ); //phpcs:ignore --nonce and wc function?>
 	<?php endif; ?>
 	<button type="submit"
-			class="single_add_to_cart_button ajax_add_to_cart  button alt"><?php echo esc_html( apply_filters( 'ywgc_add_to_cart_button_text', $product->single_add_to_cart_text() ) ); ?></button>
+			class="single_add_to_cart_button gift_card_add_to_cart_button button alt"><?php echo esc_html( apply_filters( 'ywgc_add_to_cart_button_text', $product->single_add_to_cart_text() ) ); ?></button>
 	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product_id ); ?>" />
 	<input type="hidden" name="product_id" value="<?php echo absint( $product_id ); ?>" />
 </div>
