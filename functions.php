b0f73df27237180c7a60d0e17e7e32ca0d54193f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 17:41:22 +0300

    gift

--- functions.php
+++ functions.php
@@ -67,7 +67,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	require get_template_directory() . '/woocommerce/includes/wc-custom-fields.php';
 	require get_template_directory() . '/woocommerce/includes/wc-function.php';
 	require get_template_directory() . '/woocommerce/includes/wc-remove-functions.php';
-	require get_template_directory() . '/woocommerce/includes/wc-card-function.php';
+	//require get_template_directory() . '/woocommerce/includes/wc-card-function.php';
 	require get_template_directory() . '/woocommerce/includes/wc-catalog-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-yith-wishlist-finctions.php';
 	//require get_template_directory() . '/woocommerce/includes/wc-yith-giftcards-functions.php';
