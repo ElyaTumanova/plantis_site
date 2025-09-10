cba432b2bc3b020ff23c9479d0dd376efc2cb5fa
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 17:09:16 +0300

    gift

--- page-gift-card.php
+++ page-gift-card.php
@@ -4,9 +4,12 @@ $gcid = 15419;
 
 <h1>Hi this is gift card page</h1>
 
+<a href="/?add-to-cart=15419" class="button ajax_add_to_cart">В корзину</a>
+
+
 <?php 
 
-echo do_shortcode('[add_to_cart id="15419" show_price="false"]');
+
 
 
 ?>
