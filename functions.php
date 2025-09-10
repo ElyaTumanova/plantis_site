617056e79c65d68d73897c360898a3e4416b312c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 17:04:32 +0300

    gift

--- functions.php
+++ functions.php
@@ -70,6 +70,6 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 	require get_template_directory() . '/woocommerce/includes/wc-card-function.php';
 	require get_template_directory() . '/woocommerce/includes/wc-catalog-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-yith-wishlist-finctions.php';
-	//require get_template_directory() . '/woocommerce/includes/wc-yith-giftcards-functions.php';
+	require get_template_directory() . '/woocommerce/includes/wc-yith-giftcards-functions.php';
 	require get_template_directory() . '/woocommerce/includes/wc-account-functions.php';
 }
