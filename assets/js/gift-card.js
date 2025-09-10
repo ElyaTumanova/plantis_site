0d82ba1df48bca99ac340ca29edb8c937e675942
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 22:08:41 +0300

    gift

--- assets/js/gift-card.js
+++ assets/js/gift-card.js
@@ -4,9 +4,8 @@ console.log(giftAmounts)
 
 giftAmounts.forEach(el => {
   el.addEventListener('click', function () {
-    console.log(el.childNodes[0].textContent)
-    console.log(amountInput)
-    amountInput.value = el.childNodes.text.textContent
+    let amount = el.childNodes[0].textContent
+    amountInput.value = amount
     
   })
 
