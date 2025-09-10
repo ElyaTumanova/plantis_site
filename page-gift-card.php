b47a1c4bc0554353764553d5c554577e14d01e7c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 17:19:22 +0300

    gift

--- page-gift-card.php
+++ page-gift-card.php
@@ -1,16 +1,17 @@
 <?php get_header(); 
 $gcid = 15419;
+$product    = wc_get_product( $gcid );
 ?>
 
 <h1>Hi this is gift card page</h1>
 
-<a href="/?add-to-cart=15419" class="button ajax_add_to_cart">В корзину</a>
+
 
 
 <?php 
 
 
-
+do_action( 'woocommerce_' . $product->get_type() . '_add_to_cart' );
 
 ?>
 
