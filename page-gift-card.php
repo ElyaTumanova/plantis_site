5d232b0e7c4f4972baefd3a7fbf701dd10812d15
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 17:06:32 +0300

    gift

--- page-gift-card.php
+++ page-gift-card.php
@@ -1,5 +1,14 @@
-<?php get_header(); ?>
+<?php get_header(); 
+$gcid = 15419;
+?>
 
 <h1>Hi this is gift card page</h1>
+
+<?php 
+
+echo do_shortcode('[add_to_cart id="15419" show_price="false"]');
+
+
+?>
 
 <?php get_footer();?>
