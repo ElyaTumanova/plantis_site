5825311d66ab5fa1447cd50a92f4313dbd8eb6b8
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-10 21:47:30 +0300

    gift

--- woocommerce/includes/wc-yith-giftcards-functions.php
+++ woocommerce/includes/wc-yith-giftcards-functions.php
@@ -103,6 +103,10 @@ function plnt_get_giftcard_popup() {
   }
 }
 
+add_action('yith_ywgc_show_gift_card_amount_selection', function(){
+  echo ('hihih');
+},10)
+
 // add_action('yith_gift_cards_template_before_add_to_cart_form', function (){
 //   echo '<div class="page-popup__container page-popup__container_giftcard d-none">
 //   <span class="page-popup__close"><svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.00045 24L24 0.999999M23.9995 24L0.999999 1" stroke="currentColor" stroke-miterlimit="10"></path></svg></span>';
