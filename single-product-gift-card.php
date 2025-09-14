aac75249a1e576006852abfd21f3f384c3a3a0f0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-14 14:19:30 +0300

    gift

--- single-product-gift-card.php
+++ single-product-gift-card.php
@@ -6,8 +6,12 @@ $target_id = 15419; // <-- ID нужного товара
 
 //wc_get_template( 'single-product/product-image.php' );
 ?>
-<div class="content-area">
-  <h1>Hi this is gift card page</h1>
+<div class="gift-content-area">
+  <h1>Электронный подарочный сертификат</h1>
+  <div class="gift-image-wrap">
+    <img src="https://plantis-shop.ru/wp-content/uploads/2025/07/decor-n.webp" class="gift-image" alt="Подарочная карта">
+    <p class="gift-image-amount">1500<span>₽</span></p>
+  </div>
   <?php echo do_shortcode('[yith_ywgc_display_gift_card_form]');?>
 </div>
 
