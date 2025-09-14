4f9ffb78456c856005c8aa678ebe794ddc0494fd
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-14 14:32:05 +0300

    gift

--- assets/js/gift-card.js
+++ assets/js/gift-card.js
@@ -1,5 +1,6 @@
 let giftAmounts = document.querySelectorAll('.gift__amounts p')
 let amountInput = document.querySelector('#ywgc-manual-amount')
+let imageAmount = document.querySelector('.gift-image-amount')
 let submitBtn = document.querySelector('.gift_card_add_to_cart_button')
 let gifForm = document.querySelector('.gift-cards_form')
 let allInputs = gifForm.querySelectorAll('input')
@@ -9,6 +10,7 @@ let minAmount = 1500
 let maxAmount = 30000
 amountInput.value = minAmount
 amountInput.setAttribute('value',minAmount)
+imageAmount.innerHTML = `${minAmount}<span>₽</span>`
 
 //for dev
 let mail = document.querySelector('#ywgc-recipient-email')
@@ -49,6 +51,7 @@ giftAmounts.forEach(el => {
   el.addEventListener('click', function () {
     let amount = el.childNodes[0].textContent
     amountInput.value = amount
+    imageAmount.innerHTML = `${amount}<span>₽</span>`
     updateState()
   })
 })
@@ -111,18 +114,13 @@ amountInput.addEventListener('input', function () {
     if (this.value === '' || parseInt(this.value, 10) < minAmount) {
       this.value = minAmount;
       this.setAttribute('value',minAmount)
+      imageAmount.innerHTML = `${minAmount}<span>₽</span>`
       updateState()
+    } else {
+      imageAmount.innerHTML = `${this.value}<span>₽</span>`
     }
   });
 
-  // После завершения ввода — проверяем минимум
-  // amountInput.addEventListener('blur', function () {
-  //   let val = this.value;
-  //   if (val === '' || parseInt(val, 10) < minAmount) {
-  //     this.value = minAmount;
-  //     updateState()
-  //   }
-  // });
 
 let buyNowInput = document.createElement('input');
 buyNowInput.type = 'hidden';
