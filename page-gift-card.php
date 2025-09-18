8d97218c7b03dadf2e4a9f007b270dea9722735c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-18 17:46:36 +0300

    gift card page

--- page-gift-card.php
+++ page-gift-card.php
@@ -12,12 +12,12 @@
         'post_type'      => 'gift_card',     // тип поста для YITH карт
         'post_status'    => 'publish',
         'posts_per_page' => 1,
-        // 'meta_query'     => array(
-        //     array(
-        //         'key'   => '_ywgc_number',   // метаполе с номером карты
-        //         'value' => $code,
-        //     ),
-        // ),
+        'meta_query'     => array(
+            array(
+                'key'   => 'post_title',   // метаполе с номером карты
+                'value' => $code,
+            ),
+        ),
         'fields' => 'ids',
     ) );
 
