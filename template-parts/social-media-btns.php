453f4d2b55a27ebe4e0bb209235a459db2553eb7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-09-30 15:08:21 +0300

    max

--- template-parts/social-media-btns.php
+++ template-parts/social-media-btns.php
@@ -11,4 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     <a class="social-media__button button social-media__button-telegram" href="https://t.me/+79647687944" target="_blank">
         <svg class="social-media__button-icon" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></svg>					
     </a>
+    <a class="social-media__button button social-media__button-max" href="https://max.ru/u/f9LHodD0cOLQHw-6h3MzFiHfxOLYm1P_mEvfMe4GaAlf82uc2mzUdb1OFDc" target="_blank">
+        <svg class="social-media__button-icon" viewBox="0 0 496 512" xmlns="http://www.w3.org/2000/svg"><path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm121.8 169.9l-40.7 191.8c-3 13.6-11.1 16.9-22.4 10.5l-62-45.7-29.9 28.8c-3.3 3.3-6.1 6.1-12.5 6.1l4.4-63.1 114.9-103.8c5-4.4-1.1-6.9-7.7-2.5l-142 89.4-61.2-19.1c-13.3-4.2-13.6-13.3 2.8-19.7l239.1-92.2c11.1-4 20.8 2.7 17.2 19.5z"></path></svg>					
+    </a>
 </div>
