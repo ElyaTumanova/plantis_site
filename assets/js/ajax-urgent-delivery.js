5ea99eafbf88803063163bf81dba0cb308ad1c6c
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-08 00:10:28 +0300

    urgent

--- assets/js/ajax-urgent-delivery.js
+++ assets/js/ajax-urgent-delivery.js
@@ -53,7 +53,8 @@ function getOrderParametrs(event) {
 
   if(event.target.className == "shipping_method" || 
     event.target.name == "delivery_dates" || 
-    event.target.name == "additional_delivery_interval") {
+    event.target.name == "additional_delivery_interval" ||
+    event.target == 'document') {
       console.log('нужен пересчет')
     } else {
       console.log('не нужен пересчет')
