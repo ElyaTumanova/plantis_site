d72fc94860e053d52a9d97ba25bb16b8b5c9c051
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-14 14:31:19 +0300

    search

--- includes/ajax.php
+++ includes/ajax.php
@@ -56,67 +56,18 @@ function plnt_search_ajax_action_callback (){
       $sale = get_post_meta( $product_sku_id, '_sale_price', true);
       render_search_result($product);
       ?>
-      <!-- <div class="search-result__item">
-          <a href="<?php //echo $product->get_permalink();?>" class="search-result__link" target="blank">
-              <img src="<?php //echo get_the_post_thumbnail_url( $product_sku_id, 'thumbnail' );?>" class="search-result__image" alt="<?php echo $title;?>">
-              <div class="search-result__info">
-                  <span class="search-result__title"><?php //echo $title?></span>
-                  <span class="search-result__descr"><?php //echo $short_descr?></span>
-                  <?php if ($sale) {
-                      ?>
-                      <span class="search-result__reg-price"><?php //echo get_post_meta( $product_sku_id, '_regular_price', true);?>&#8381;</span>
-                      <span class="search-result__price"><?php //echo get_post_meta( $product_sku_id, '_sale_price', true);?>&#8381;</span>
-                      <?php
-                  } else {
-                      ?>
-                      <span class="search-result__price"><?php //echo get_post_meta( $product_sku_id, '_price', true);?>&#8381;</span>
-                      <?php 
-                  }
-                  ?>
-              </div>
-          </a>  
-      </div>
-
-      <input class="search-result__btn button" type="submit" form ="searchform" value="Посмотреть все" /> -->
-
+        <input class="search-result__btn button" type="submit" form ="searchform" value="Посмотреть все" />
       <?php
     } else {
       // $search_timing_1 = round((microtime(true) - $start_search) * 1000, 2);
-
-      // $json_data['out'] = ob_start(PHP_OUTPUT_HANDLER_CLEANABLE);
       if ($query_ajax->have_posts()) {
         // echo '<pre>';
         // print_r( $query_ajax );
         // echo '</pre>';
         while ($query_ajax->have_posts()){
-            $query_ajax->the_post();
-            $product = wc_get_product( get_the_ID() );
-            // print_r($product);
-            $short_descr = $product->get_short_description();
-            $sale = get_post_meta( get_the_ID(), '_sale_price', true);
-            ?>
-            <div class="search-result__item">
-                <a href="<?php echo get_permalink();?>" class="search-result__link" target="blank">
-                    <?php plnt_check_stock_status();?>
-                    <img src="<?php echo get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );?>" class="search-result__image" alt="<?php echo get_the_title();?>">
-                    <div class="search-result__info">
-                        <span class="search-result__title"><?php echo get_the_title();?></span>
-                        <span class="search-result__descr"><?php echo $short_descr?></span>
-                        <?php if ($sale) {
-                            ?>
-                            <span class="search-result__reg-price"><?php echo get_post_meta( get_the_ID(), '_regular_price', true);?>&#8381;</span>
-                            <span class="search-result__price"><?php echo get_post_meta( get_the_ID(), '_sale_price', true);?>&#8381;</span>
-                            <?php
-                        } else {
-                            ?>
-                            <span class="search-result__price"><?php echo get_post_meta( get_the_ID(), '_price', true);?>&#8381;</span>
-                            <?php 
-                        }
-                        ?>
-                    </div>
-                </a>  
-            </div>
-            <?php
+          $query_ajax->the_post();
+          $product = wc_get_product( get_the_ID() );
+          render_search_result($product);
         }
         ?>
         <input class="search-result__btn button" type="submit" form ="searchform" value="Посмотреть все" />
@@ -160,8 +111,6 @@ function render_search_result($product) {
           </div>
       </a>  
   </div>
-  <input class="search-result__btn button" type="submit" form ="searchform" value="Посмотреть все" />
-
   <?php
 }
 // вывод товаров в результатх теста
