47260479d74a6059c3c15df4e84e3d1c031304a3
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-14 14:15:09 +0300

    search

--- includes/ajax.php
+++ includes/ajax.php
@@ -44,7 +44,7 @@ function plnt_search_ajax_action_callback (){
       $query_ajax = new WP_Query($arg);
 
     $product_sku_id = wc_get_product_id_by_sku( $query_ajax->query_vars[ 's' ] );
-    print_r($product_sku_id);
+    // print_r($product_sku_id);
     $json_data['out'] = ob_start(PHP_OUTPUT_HANDLER_CLEANABLE);
 
     if ($product_sku_id) { 
@@ -125,7 +125,7 @@ function plnt_search_ajax_action_callback (){
         <?php
       }
     } 
-    // $search_timing_2 = round((microtime(true) - $start_search) * 1000, 2);
+    $search_timing_2 = round((microtime(true) - $start_search) * 1000, 2);
     $json_data['out'] = ob_get_clean();
     // $json_data['search_timing_1'] = $search_timing_1;
     $json_data['search_timing_2'] = $search_timing_2;
