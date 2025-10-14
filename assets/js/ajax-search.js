eec54f81716e1307e9bdff780e4a2e5381df452b
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-14 14:50:15 +0300

    search

--- assets/js/ajax-search.js
+++ assets/js/ajax-search.js
@@ -1,28 +1,33 @@
-jQuery(function ($){
-    $('.search-form input[name="s"]').on('keyup', function (){  //стили контейнеров формы поиска из header
-        var search = $('.search-form input[name="s"]').val();
-        if (search.length <3) {
-            return false; //ajax works after 4 digits input
-        }
-        var data = {
-            s: search,
-            action: 'search-ajax',
-            nonce: search_form.nonce
-        };
-        $.ajax ({
-            url: search_form.url,
-            data: data,
-            type: 'POST',
-            dataType: 'json',
-            beforeSend: function(xhr){
-            },
-            success: function(data){
-                // console.log(data.search_timing_1);
-                // console.log(data.search_timing_2);
-                console.log(data);
-                $('.search .search-result').html(data.out);    //стили контейнеров формы поиска из header
-            }
-        });
-    });
-});
+jQuery(function ($) {
+  let timer = null;
+  let xhr = null;
 
+  $('.search-form input[name="s"]').on('input', function () {
+    const search = this.value.trim();
+
+    clearTimeout(timer);
+
+    if (search.length < 4) return;
+
+    timer = setTimeout(function () {
+      // отменяем предыдущий незавершённый запрос (не обязательно, но полезно)
+      if (xhr && xhr.readyState !== 4) xhr.abort();
+
+      xhr = $.ajax({
+        url: search_form.url,
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          s: search,
+          action: 'search-ajax',
+          nonce: search_form.nonce
+        },
+        success: function (data) {
+          // console.log(data.search_timing_1, data.search_timing_2);
+          console.log(data);
+          $('.search .search-result').html(data.out);
+        }
+      });
+    }, 300); // подождать 300 мс после ввода
+  });
+});
