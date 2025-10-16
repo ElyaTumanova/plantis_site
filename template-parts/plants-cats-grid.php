63c7847a3c75255051dd4cc702adcb5edf503db7
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-16 17:49:38 +0300

    images

--- template-parts/plants-cats-grid.php
+++ template-parts/plants-cats-grid.php
@@ -30,7 +30,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/succulent/" class="cats-grid__inner cats-grid__inner_succulent">
         <h3 class="cats-grid__title">Суккуленты</h3>
     </a>
-    <a href="<?php// echo get_site_url();?>/product-tag/pet-friendly/" class="cats-grid__inner cats-grid__inner_petfriendly">
+    <a href="<?php // echo get_site_url();?>/product-tag/pet-friendly/" class="cats-grid__inner cats-grid__inner_petfriendly">
         <h3 class="cats-grid__title">Pet Friendly</h3>
     </a>
 </div> -->
