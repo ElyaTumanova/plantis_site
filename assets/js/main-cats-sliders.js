20cc30b2462b3d04bf6422345b55604bb4c03f6a
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-16 23:21:51 +0300

    ajax

--- assets/js/main-cats-sliders.js
+++ assets/js/main-cats-sliders.js
@@ -37,7 +37,7 @@ function ajaxGetMainCatTerm() {
             success: function (data) {
                 // console.log('ajaxGetMainCatTerm success');
                 $('.main__cats-slider').html(data.out);
-                console.log(data.main_timing);
+                // console.log(data.main_timing);
                 let slider = document.querySelectorAll(".product-slider-swiper .product");
                 slider.forEach((slide) => {
                     slide.classList.add('swiper-slide');
@@ -54,31 +54,31 @@ navItems.forEach((el,index) => {
     el.addEventListener('click',() => showSlider(index));
 });
 
-(function ($) {
-  function ajaxPing(cb) {
-    const url = window.ajaxurl || '/wp-admin/admin-ajax.php'; // WP сам задаёт ajaxurl в админке; на фронте лучше локализовать (см. ниже)
-    const t0 = performance.now();
+// (function ($) {
+//   function ajaxPing(cb) {
+//     const url = window.ajaxurl || '/wp-admin/admin-ajax.php'; // WP сам задаёт ajaxurl в админке; на фронте лучше локализовать (см. ниже)
+//     const t0 = performance.now();
 
-    $.ajax({
-      url,
-      type: 'POST',
-      data: { action: 'ping' },
-      timeout: 8000, // 8s
-      success: function (resp, status, xhr) {
-        const duration = performance.now() - t0;
-        if (cb) cb(null, resp, { duration });
-        console.log('ping ok:', resp, `${duration.toFixed(1)}ms`);
-      },
-      error: function (xhr, status, err) {
-        if (cb) cb(err || status);
-        console.warn('ping error:', status, err);
-      }
-    });
-  }
+//     $.ajax({
+//       url,
+//       type: 'POST',
+//       data: { action: 'ping' },
+//       timeout: 8000, // 8s
+//       success: function (resp, status, xhr) {
+//         const duration = performance.now() - t0;
+//         if (cb) cb(null, resp, { duration });
+//         console.log('ping ok:', resp, `${duration.toFixed(1)}ms`);
+//       },
+//       error: function (xhr, status, err) {
+//         if (cb) cb(err || status);
+//         console.warn('ping error:', status, err);
+//       }
+//     });
+//   }
 
-  // пример вызова сразу при загрузке
-  $(function () {
-    ajaxPing();
-    // или по клику: $('#btn-ping').on('click', ajaxPing);
-  });
-})(jQuery);
+//   // пример вызова сразу при загрузке
+//   $(function () {
+//     ajaxPing();
+//     // или по клику: $('#btn-ping').on('click', ajaxPing);
+//   });
+// })(jQuery);
