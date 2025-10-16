c6fd6a829290c5b3ec832859ffb3f0bae1aa0a59
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-16 12:57:27 +0300

    checkout

--- woocommerce/includes/wc-shipping-functions.php
+++ woocommerce/includes/wc-shipping-functions.php
@@ -24,7 +24,7 @@ function plnt_set_initials() {
 
 //for dev
 
-add_action('woocommerce_review_order_before_shipping','plnt_check');
+//add_action('woocommerce_review_order_before_shipping','plnt_check');
 //add_action('wp_head','plnt_check');
 
 function plnt_check() {
@@ -85,34 +85,6 @@ function plnt_get_urgent_shipping() {
 
   //wp_die(); // (required)
 }
-
-// add_action( 'woocommerce_checkout_update_order_review', 'plnt_refresh_shipping_methods_for_urgent', 10, 1 );
-// function plnt_refresh_shipping_methods_for_urgent( $post_data ){
-//     $bool = true;
-
-//     if ( WC()->session->get('isUrgent' ) === '1' )
-//         $bool = false;
-
-//     // Mandatory to make it work with shipping methods
-//     foreach ( WC()->cart->get_shipping_packages() as $package_key => $package ){
-//         WC()->session->set( 'shipping_for_package_' . $package_key, $bool );
-//     }
-//     WC()->cart->calculate_shipping();
-// }
-
-// add_action( 'woocommerce_checkout_update_order_review', 'plnt_refresh_shipping_methods_for_late', 10, 1 );
-// function plnt_refresh_shipping_methods_for_late( $post_data ){
-//     $bool = true;
-
-//     if ( WC()->session->get('isLate' ) === '1')
-//         $bool = false;
-
-//     // Mandatory to make it work with shipping methods
-//     foreach ( WC()->cart->get_shipping_packages() as $package_key => $package ){
-//         WC()->session->set( 'shipping_for_package_' . $package_key, $bool );
-//     }
-//     WC()->cart->calculate_shipping();
-// }
 
 add_action( 'woocommerce_checkout_update_order_review', 'plnt_refresh_shipping_methods', 10, 1 );
 function plnt_refresh_shipping_methods( $post_data ){
