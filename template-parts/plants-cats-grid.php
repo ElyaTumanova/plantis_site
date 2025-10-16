959ef9e87dc3e166d63c77092f3711863e6adc63
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-16 17:50:04 +0300

    images

--- template-parts/plants-cats-grid.php
+++ template-parts/plants-cats-grid.php
@@ -4,37 +4,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<!-- <div class="cats-grid">
-    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/palms/" class="cats-grid__inner cats-grid__inner_palms">
-        <h3 class="cats-grid__title">Пальмы</h3>
-        <?php //echo wp_get_attachment_image( 15491, 'large' );?>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/fikusy/" class="cats-grid__inner cats-grid__inner_fikus">
-        <h3 class="cats-grid__title">Фикусы</h3>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/dekorativno-listvennye/" class="cats-grid__inner cats-grid__inner_lisv">
-        <h3 class="cats-grid__title">Декоративно-лиственные</h3>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-tag/napolnye/" class="cats-grid__inner cats-grid__inner_napol">
-        <h3 class="cats-grid__title">Напольные</h3>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/dekorativno-cvetushchie/" class="cats-grid__inner cats-grid__inner_cvetush">
-        <h3 class="cats-grid__title">Цветущие</h3>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/lianas/" class="cats-grid__inner cats-grid__inner_lianas">
-        <h3 class="cats-grid__title">Лианы</h3>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-tag/novichkam/" class="cats-grid__inner cats-grid__inner_neprikhotliv">
-        <h3 class="cats-grid__title">Неприхотливые</h3>
-    </a>
-    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/succulent/" class="cats-grid__inner cats-grid__inner_succulent">
-        <h3 class="cats-grid__title">Суккуленты</h3>
-    </a>
-    <a href="<?php // echo get_site_url();?>/product-tag/pet-friendly/" class="cats-grid__inner cats-grid__inner_petfriendly">
-        <h3 class="cats-grid__title">Pet Friendly</h3>
-    </a>
-</div> -->
-
 <div class="cats-grid">
     <a href="<?php echo get_site_url(); ?>/product-category/komnatnye-rasteniya/palms/" class="cats-grid__inner cats-grid__inner_palms">
         <h3 class="cats-grid__title">Пальмы</h3>
@@ -126,3 +95,34 @@ if ( ! defined( 'ABSPATH' ) ) {
         ?>
     </a>
 </div>
+
+<!-- <div class="cats-grid">
+    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/palms/" class="cats-grid__inner cats-grid__inner_palms">
+        <h3 class="cats-grid__title">Пальмы</h3>
+        <?php //echo wp_get_attachment_image( 15491, 'large' );?>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/fikusy/" class="cats-grid__inner cats-grid__inner_fikus">
+        <h3 class="cats-grid__title">Фикусы</h3>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/dekorativno-listvennye/" class="cats-grid__inner cats-grid__inner_lisv">
+        <h3 class="cats-grid__title">Декоративно-лиственные</h3>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-tag/napolnye/" class="cats-grid__inner cats-grid__inner_napol">
+        <h3 class="cats-grid__title">Напольные</h3>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/dekorativno-cvetushchie/" class="cats-grid__inner cats-grid__inner_cvetush">
+        <h3 class="cats-grid__title">Цветущие</h3>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/lianas/" class="cats-grid__inner cats-grid__inner_lianas">
+        <h3 class="cats-grid__title">Лианы</h3>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-tag/novichkam/" class="cats-grid__inner cats-grid__inner_neprikhotliv">
+        <h3 class="cats-grid__title">Неприхотливые</h3>
+    </a>
+    <a href="<?php //echo get_site_url();?>/product-category/komnatnye-rasteniya/succulent/" class="cats-grid__inner cats-grid__inner_succulent">
+        <h3 class="cats-grid__title">Суккуленты</h3>
+    </a>
+    <a href="<?php // echo get_site_url();?>/product-tag/pet-friendly/" class="cats-grid__inner cats-grid__inner_petfriendly">
+        <h3 class="cats-grid__title">Pet Friendly</h3>
+    </a>
+</div> -->
