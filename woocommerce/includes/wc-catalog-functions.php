7bdf7dae3384d2c2c25d4b9704372b793e441da0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-24 17:42:21 +0300

    filter plants

--- woocommerce/includes/wc-catalog-functions.php
+++ woocommerce/includes/wc-catalog-functions.php
@@ -881,14 +881,22 @@ function plnt_plant_name_filter_content($template_content) {
 			print_r( $template_content['template']['content'] );
 			echo '</pre>';
 
-        $template_content['template']['content']['filter']['content'] = berocket_insert_to_array(
-				$template_content['template']['content']['filter']['content'],
-				'list',
-				array(
-					'custom_content' =>  '<input type="text" placeholder="Поиск..." class="berocket-search-checkbox">'
-				),
-				true
-			);
+        $template_content['template']['content'] = berocket_insert_to_array(
+				$template_content['template']['content'],
+          'filter',
+          array(
+            'custom_content' =>  '<input type="text" placeholder="Поиск..." class="berocket-search-checkbox">'
+          ),
+          true
+			  );
+        // $template_content['template']['content']['filter']['content'] = berocket_insert_to_array(
+				// $template_content['template']['content']['filter']['content'],
+        //   'list',
+        //   array(
+        //     'custom_content' =>  '<input type="text" placeholder="Поиск..." class="berocket-search-checkbox">'
+        //   ),
+        //   true
+			  // );
 	}
     return $template_content;
 
