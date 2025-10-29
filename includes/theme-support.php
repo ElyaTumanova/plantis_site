7e60b05fdb62e3c53dbdc2f99aca1911bd2d85f0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-29 14:59:20 +0300

    search

--- includes/theme-support.php
+++ includes/theme-support.php
@@ -146,7 +146,7 @@ function check_301redirect_tax_url(){
 add_filter('query_vars', function ($vars) {
     $vars[] = 'gen';
     $vars[] = 'plant';
-    $vars[] = 's';
+    $vars[] = 'search';
     return $vars;
 });
 
