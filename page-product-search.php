de66fea85a37525b5aaf8d222363129a6d2b7db0
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-10-29 14:29:34 +0300

    search

--- page-product-search.php
+++ page-product-search.php
@@ -64,8 +64,9 @@ $query_ajax_other = new WP_Query($argOther);
 
 // $total = count($query_ajax_plants->posts) + count($query_ajax_other->posts);
 $total = count($query_ajax_plants->posts);
-echo($total);
-echo($query_ajax_plants->max_num_pages);
+// echo($total);
+// echo($query_ajax_plants->max_num_pages);
+echo($paged);
 
 get_header( 'shop' );
 
