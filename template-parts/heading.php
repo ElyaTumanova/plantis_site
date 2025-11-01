8806d37b448957269a0ae71a000e66deff162d40
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-11-01 13:47:04 +0300

    search

--- template-parts/heading.php
+++ template-parts/heading.php
@@ -3,6 +3,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 $close_icon = carbon_get_theme_option('close_icon');
+$search_icon = carbon_get_theme_option('search_icon');
+$logo = carbon_get_theme_option('logo');
+$site_title = carbon_get_theme_option('site_title');
+$account_icon = carbon_get_theme_option('account_icon');
+$account_logged_icon = carbon_get_theme_option('account_logged_icon');
+$menu_icon_mob = carbon_get_theme_option('menu_icon_mob');
+$phone_icon = carbon_get_theme_option('phone_icon');
+$catalog_icon = carbon_get_theme_option('catalog_icon');
 ?>
 
 <header id="header" class="header" role="banner">
@@ -10,9 +18,7 @@ $close_icon = carbon_get_theme_option('close_icon');
 			<div class="header__info">
 				<div class="header__info-wrap container">
 					<nav class="header__info-navigation" role="navigation">
-                        <?php 
-                            get_template_part( 'template-parts/info-menu-header');
-                        ?>
+            <?php get_template_part( 'template-parts/info-menu-header');?>
 					</nav>
           <div class="header__wrap">
             <?php get_template_part('template-parts/social-media-btns');?>
@@ -29,12 +35,10 @@ $close_icon = carbon_get_theme_option('close_icon');
 				<div class="container">
 					<div class="header__wrap_logo">
 						<div class="logo">
-							<?php $logo = carbon_get_theme_option('logo');?>
 							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo__link"><img src="<?php echo $logo ?>" class="logo__img" alt="Plantis" width="150" height="26"></a>
 						</div><!-- .logo -->
 					</div>
 					<div class="header__description">
-						<?php $site_title = carbon_get_theme_option('site_title')?>
 						<?php if ( is_front_page()) : ?>
 							<h1 class="site-title"><?php echo $site_title ?></h1>
 						<?php else : ?>
@@ -43,8 +47,6 @@ $close_icon = carbon_get_theme_option('close_icon');
 					</div><!-- .description -->
 					<div class="header__wrap">
 						<div class="search-btn">
-							<?php $search_icon = carbon_get_theme_option('search_icon');
-              $close_icon = carbon_get_theme_option('close_icon');?>
 							<button class="header-btn__wrap">
 								<?php echo $search_icon ?>
 								<?php echo $close_icon ?>
@@ -52,8 +54,6 @@ $close_icon = carbon_get_theme_option('close_icon');
 							</button>
 						</div>
 						<div class="header__account">
-							<?php $account_icon = carbon_get_theme_option('account_icon');
-							$account_logged_icon = carbon_get_theme_option('account_logged_icon')?>
 							<?php if (!is_user_logged_in()) : 
 								if (is_account_page()) :?>
 									<a href ="<?php echo esc_url( home_url( '/my-account' ) ); ?>" class="header-btn__wrap">
@@ -92,20 +92,15 @@ $close_icon = carbon_get_theme_option('close_icon');
 
 		<div class="header__mob container">
 			<div class="search-btn">
-				<?php $search_icon_mob = carbon_get_theme_option('search_icon_mob');
-        $close_icon = carbon_get_theme_option('close_icon');?>
 				<button class="header-btn__wrap header-btn__wrap_mob">
 					<?php echo $search_icon ?>
           <?php echo $close_icon ?>
-					<!-- <img class="header-btn__icon" src="<?php //echo $search_icon_mob ?>" alt="search" width="21" height="21">		 -->
 				</button>
 			</div>
 			<div class="logo">
-				<?php $logo = carbon_get_theme_option('logo');?>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="logo__link"><img src="<?php echo $logo ?>" class="logo__img" alt="Plantis" width="150" height="26"></a>
 			</div><!-- .logo -->
 			<div class="header__mob-menu">
-				<!-- <?php $menu_icon_mob = carbon_get_theme_option('menu_icon_mob')?> -->
 				<button class="header-btn__wrap header-btn__wrap_mob">
 					<img class="header-btn__icon" src="<?php echo $menu_icon_mob ?>" alt="menu" width="21" height="21">		
 				</button>
@@ -114,7 +109,10 @@ $close_icon = carbon_get_theme_option('close_icon');
 		<div class="header__notice-wrap_mob">
 			<?php get_template_part( 'template-parts/header-notice' );?>
 		</div>
-    <div class="search__wrap"><?get_search_form();?></div>
+    <div class="search__wrap">
+      <?get_search_form();?>
+      <div class="search__close"><?php echo $close_icon ?></div>
+    </div>
 
     <div class="search-result">
     </div>
@@ -123,7 +121,6 @@ $close_icon = carbon_get_theme_option('close_icon');
 		
 		<div class="header__nav-wrap container">
 			<div class="header__phone">
-				<?php $phone_icon = carbon_get_theme_option('phone_icon')?>
 					<a href="tel:+79647687944" class="header-btn__wrap">	
 						<?php echo $phone_icon ?>
 						<span class="header-btn__label">Позвонить</span>	
@@ -132,7 +129,6 @@ $close_icon = carbon_get_theme_option('close_icon');
 			</div>
 
 			<div class="header__catalog_mob">
-				<?php $catalog_icon = carbon_get_theme_option('catalog_icon')?>
 					<div class="header-btn__wrap">	
 						<?php echo $catalog_icon ?>	
 						<span class="header-btn__label">Каталог</span>		
