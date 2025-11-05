2ef2da37c3bd6aeab22911272dba4654adfbe78f
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-11-05 13:50:16 +0300

    search

--- page-search-results.php
+++ page-search-results.php
@@ -93,100 +93,101 @@ if ($product_sku_id) {
   $all_ids = array_values(array_unique(array_merge($ids_plants, $ids_others)));
 }
 
-$q_args = [
-    'post_type'      => 'product',
-    'post_status'    => 'publish',
-    'post__in'       => $all_ids,
-    'orderby'        => $ordering_args['orderby'],
-    'order'          => $ordering_args['order'],
-    'posts_per_page' => $per_page,
-    'paged' => $paged,
-    'ignore_sticky_posts' => true,
-    'no_found_rows'  => false,
-];
-
-if ( ! empty( $ordering_args['meta_key'] ) ) {
-    $q_args['meta_key']  = $ordering_args['meta_key'];      // '_price' или 'total_sales'
-    if ( $orderby_value === 'price' || $orderby_value === 'price-desc' ) {
-        $q_args['meta_type'] = 'DECIMAL';                   // чтобы точно числовая сортировка
-    }
-}
-
-
-
-$q_page = new WP_Query( $q_args );
-
-foreach ( $added_filters as $af ) {
-    remove_filter( $af[0], $af[1] );
-}
-
-wc_set_loop_prop( 'is_paginated', $q_page->max_num_pages > 1 );
-wc_set_loop_prop( 'page', $paged );
-wc_set_loop_prop( 'per_page', $per_page );
-wc_set_loop_prop( 'total', (int) $q_page->found_posts );
-
-get_header( 'shop' );
-
-do_action( 'woocommerce_before_main_content' );
-
-?>
-<header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-		<h1 class="woocommerce-products-header__title page-title">Результаты поиска: <?php if ($product_sku_id){echo('Артикул ');}; echo esc_html($search) ?></h1>
-	<?php endif; ?>
-
-	<?php	do_action( 'woocommerce_archive_description' );?>
-</header>
-<?php
-
-if ($q_page->have_posts()) {
-
-    // Используем Woo-компоненты, чтобы сохранить верстку/сетки
-    do_action('woocommerce_before_shop_loop');
-    woocommerce_product_loop_start();
-  
-      while ($q_page->have_posts()) {
-          $q_page->the_post();
-            do_action( 'woocommerce_shop_loop' );
-            wc_get_template_part( 'content', 'product' );
-      }
-
-    do_action( 'woocommerce_before_product_loop_end' );   //plnt new action 
-    woocommerce_product_loop_end();
-
-     // ПРАВИЛЬНАЯ ПАГИНАЦИЯ
-    $big = 999999999; // need an unlikely integer
-    
-    // Создаем правильную базовую ссылку для пагинации
-    $pagination_args = array(
-        'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-        'format' => '?paged=%#%',
-        'current' => $paged,
-        'total' => $q_page->max_num_pages,
-        'prev_text' => '&larr;',
-        'next_text' => '&rarr;',
-        'type' => 'list',
-        'end_size' => 2,
-        'mid_size' => 1,
-        // 'add_args'  => array(
-        //   'orderby' => $orderby_value, // ← сохраняем выбранную сортировку
-        //   'search'  => $search,        // ← и строку поиска, если вы храните её в query_var 'search'
-        // ),
-    );
-    
-    echo '<nav class="woocommerce-pagination">';
-    echo paginate_links($pagination_args);
-    echo '</nav>';
-
-    do_action( 'woocommerce_after_shop_loop' );
-
-    
-    wp_reset_postdata();
-} else {
-   	do_action( 'woocommerce_no_products_found' );
+if ( empty( $all_ids ) ) {
+    do_action( 'woocommerce_no_products_found' );
     get_template_part('template-parts/products/products-popular');
+} else {
+
+  $q_args = [
+      'post_type'      => 'product',
+      'post_status'    => 'publish',
+      'post__in'       => $all_ids,
+      'orderby'        => $ordering_args['orderby'],
+      'order'          => $ordering_args['order'],
+      'posts_per_page' => $per_page,
+      'paged' => $paged,
+      'ignore_sticky_posts' => true,
+      'no_found_rows'  => false,
+  ];
+
+  if ( ! empty( $ordering_args['meta_key'] ) ) {
+      $q_args['meta_key']  = $ordering_args['meta_key'];      // '_price' или 'total_sales'
+      if ( $orderby_value === 'price' || $orderby_value === 'price-desc' ) {
+          $q_args['meta_type'] = 'DECIMAL';                   // чтобы точно числовая сортировка
+      }
+  }
+
+  $q_page = new WP_Query( $q_args );
+
+  foreach ( $added_filters as $af ) {
+      remove_filter( $af[0], $af[1] );
+  }
+
+  wc_set_loop_prop( 'is_paginated', $q_page->max_num_pages > 1 );
+  wc_set_loop_prop( 'page', $paged );
+  wc_set_loop_prop( 'per_page', $per_page );
+  wc_set_loop_prop( 'total', (int) $q_page->found_posts );
+
+  get_header( 'shop' );
+
+  do_action( 'woocommerce_before_main_content' );
+
+  ?>
+  <header class="woocommerce-products-header">
+    <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+      <h1 class="woocommerce-products-header__title page-title">Результаты поиска: <?php if ($product_sku_id){echo('Артикул ');}; echo esc_html($search) ?></h1>
+    <?php endif; ?>
+
+    <?php	do_action( 'woocommerce_archive_description' );?>
+  </header>
+  <?php
+
+  if ($q_page->have_posts()) {
+
+      // Используем Woo-компоненты, чтобы сохранить верстку/сетки
+      do_action('woocommerce_before_shop_loop');
+      woocommerce_product_loop_start();
+    
+        while ($q_page->have_posts()) {
+            $q_page->the_post();
+              do_action( 'woocommerce_shop_loop' );
+              wc_get_template_part( 'content', 'product' );
+        }
+
+      do_action( 'woocommerce_before_product_loop_end' );   //plnt new action 
+      woocommerce_product_loop_end();
+
+      // ПРАВИЛЬНАЯ ПАГИНАЦИЯ
+      $big = 999999999; // need an unlikely integer
+      
+      // Создаем правильную базовую ссылку для пагинации
+      $pagination_args = array(
+          'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+          'format' => '?paged=%#%',
+          'current' => $paged,
+          'total' => $q_page->max_num_pages,
+          'prev_text' => '&larr;',
+          'next_text' => '&rarr;',
+          'type' => 'list',
+          'end_size' => 2,
+          'mid_size' => 1,
+          // 'add_args'  => array(
+          //   'orderby' => $orderby_value, // ← сохраняем выбранную сортировку
+          //   'search'  => $search,        // ← и строку поиска, если вы храните её в query_var 'search'
+          // ),
+      );
+      
+      echo '<nav class="woocommerce-pagination">';
+      echo paginate_links($pagination_args);
+      echo '</nav>';
+
+      do_action( 'woocommerce_after_shop_loop' );
+
+      
+      wp_reset_postdata();
+  } 
+  do_action( 'woocommerce_after_main_content' );
 }
 
-do_action( 'woocommerce_after_main_content' );
 get_footer( 'shop' );
 ?>
