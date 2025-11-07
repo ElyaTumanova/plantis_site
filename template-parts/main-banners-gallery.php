a3eb69d6ad646fab08e47fdf7396b4913d01b0e2
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-11-07 15:52:28 +0300

    main banners

--- template-parts/main-banners-gallery.php
+++ template-parts/main-banners-gallery.php
@@ -7,35 +7,33 @@ if ( ! defined( 'ABSPATH' ) ) {
 <?php 
 
 $banners_arr = carbon_get_theme_option('banners');
-
-print_r($banners_arr);
 	
 ?>
 <div class="main__banners main__banners-swiper swiper">
 	<div class="swiper-wrapper">
-	<?php foreach ($banners_arr as $banner): ?>
-		<a 
-			class="swiper-slide <?php if (!$banner['banner_link']) echo 'main__banners_no-link'; ?>" 
-			<?php if ($banner['banner_link']): ?> href="<?php echo $banner['banner_link']; ?>" <?php endif; ?>
-		>
-			<picture>
-				<!-- Мобильное изображение -->
-				<source 
-					srcset="<?php echo $banner['banner_mob']; ?>" 
-					media="(max-width: 767px)"
-				>
-				<!-- Десктопное изображение -->
-				<img 
-					src="<?php echo $banner['banner_desktop']; ?>" 
-					class="main__banner-img" 
-					alt="<?php echo htmlspecialchars($banner['banner_name']); ?>" 
-					loading="lazy"
-				>
-			</picture>
-		</a>
-	<?php endforeach; ?>
+	<?php
+		foreach ($banners_arr as $banner) {
+			?>
+			<a class="swiper-slide <?php if(!$banner['banner_link']) : ?> main__banners_no-link <?php endif; ?>" href="<?php echo $banner['banner_link'] ?>"><img src="<?php echo $banner['banner_desktop'] ?>" class="main__banner-img" alt="<?php echo $banner['banner_name']?>" loading=" lazy"></a>
+			<?php 
+		}
+	?>
 	</div>
+	<div class="swiper-pagination"></div>
+	<div class="swiper-button-prev"></div>
+	<div class="swiper-button-next"></div>
+</div>
 
+<div class="main__banners_mob main__banners-swiper swiper">
+	<div class="swiper-wrapper">
+	<?php
+		foreach ($banners_arr as $banner) {
+			?>
+			<a class="swiper-slide <?php if(!$banner['banner_link']) : ?> main__banners_no-link <?php endif; ?>" href="<?php echo $banner['banner_link'] ?>"><img src="<?php echo $banner['banner_mob'] ?>" class="main__banner-img" alt="<?php echo $banner['banner_name']?>" loading=" lazy"></a>
+			<?php 
+		}
+	?>
+	</div>
 	<div class="swiper-pagination"></div>
 	<div class="swiper-button-prev"></div>
 	<div class="swiper-button-next"></div>
