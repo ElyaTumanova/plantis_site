9aa981dca28f25eace1e7ce11ad20709d99b5a95
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2025-11-15 13:09:12 +0300

    giftcard

--- single-product-gift-card.php
+++ single-product-gift-card.php
@@ -3,7 +3,7 @@
   <div class="gift-content-area">
     <h1 class="gift-card__title">Электронный подарочный сертификат</h1>
     <button class="gift-card__example-btn page-popup-open-btn">Посмотреть пример</button>
-    <a class="gift-card__example-btn gc-popup-open-btn" href="<?php echo get_site_url()?>/gift-card" target="_blank">Проверить баланс</a>
+    <a class="gift-card__example-btn" href="<?php echo get_site_url()?>/gift-card" target="_blank">Проверить баланс</a>
     <div class="gift-image-wrap">
       <img src="<?php echo get_template_directory_uri()?>/images/gift-card/gc_cover.jpg" class="gift-image" alt="Подарочная карта">
       <p class="gift-image-amount">1500<span>₽</span></p>
@@ -13,27 +13,27 @@
       <h2 class="giftcard-advantages__title">Преимущества подарочного сертификата</h2>
       <div class="giftcard-advantages">
         <div class="giftcard-advantages__wrap">
-          <img width="111" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/фото_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+          <span class="giftcard-advantages__number">01</span>
           <div class="giftcard-advantages__descr">Подходит для физлиц и корпоративных клиентов</div>
         </div>
         <div class="giftcard-advantages__wrap">
-          <img width="110" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/пересадка_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+          <span class="giftcard-advantages__number">02</span>
           <div class="giftcard-advantages__descr">Гибкий номинал сертификата от 1500 до 30 000 рублей</div>
         </div>
         <div class="giftcard-advantages__wrap">
-          <img width="115" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/доставка_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+          <span class="giftcard-advantages__number">03</span>
           <div class="giftcard-advantages__descr">Можно использовать сертификат онлайн или в шоурумах</div>
         </div>
         <div class="giftcard-advantages__wrap">
-          <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+          <span class="giftcard-advantages__number">04</span>
           <div class="giftcard-advantages__descr">Можно оплачивать товары и услуги со скидкой</div>
         </div>
         <div class="giftcard-advantages__wrap">
-          <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+          <span class="giftcard-advantages__number">05</span>
           <div class="giftcard-advantages__descr">Можно использовать многократно в рамках номинала и срока действия</div>
         </div>
         <div class="giftcard-advantages__wrap">
-          <img width="112" height="100" src="https://plantis.shop/wp-content/uploads/2023/11/уход_зел.svg" class="giftcard-advantages__image" alt="" loading="lazy">
+          <span class="giftcard-advantages__number">06</span>
           <div class="giftcard-advantages__descr">Удобно, если вы не знаете, какое именно растение подойдет </div>
         </div>
       </div>
