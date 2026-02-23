6ab700c89db9b65f701823afa8de04d3f79a0735
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2026-02-23 16:21:14 +0300

    login

--- assets/js/login-popup.js
+++ assets/js/login-popup.js
@@ -3,11 +3,12 @@ const loginOpenPopupBtn = document.querySelectorAll('.login-btn');
 const loginPopup = document.querySelector('.login-popup');
 const loginClosePopupBtn = document.querySelector('.login__close');
 const loginPopupOverlay = document.querySelector('.login__popup-overlay');
+const customerLogin = document.querySelector('#customer_login');
 //переменные для переключения форм
 const regOpenPopupBtn = document.querySelectorAll('.login-form__registration-btn');
 const loginOnRegPopupBtn = document.querySelectorAll('.register-form__login-btn');
-const loginForm = loginPopup.querySelector('.u-column1.col-1')
-const regForm = loginPopup.querySelector('.u-column2.col-2')
+const loginForm = customerLogin.querySelector('.u-column1.col-1')
+const regForm = customerLogin.querySelector('.u-column2.col-2')
 let isLogin = true;
 
 
