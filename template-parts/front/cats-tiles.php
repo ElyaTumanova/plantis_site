e82552292d84a49e9541b8cafa4f1b05414cfba1
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2026-06-23 19:13:09 +0300

    fixes

--- template-parts/front/cats-tiles.php
+++ template-parts/front/cats-tiles.php
@@ -16,82 +16,103 @@ if ( ! function_exists( 'wc_get_page_permalink' ) ) {
 | Параметры плиток
 |--------------------------------------------------------------------------
 */
-global $plants_cat_id;
-$parent     = $plants_cat_id;
-$hide_empty = true;
-// $number     = 0;
 
-$show_count = false;
+$tiles = [
+    [
+        'taxonomy' => 'product_tag',
+        'slug'     => 'napolnye',
+    ],
+    [
+      'taxonomy' => 'product_cat',
+      'slug'     => 'dekorativno-listvennye',
+    ],
+    [
+        'taxonomy' => 'product_cat',
+        'slug'     => 'fikusy',
+    ],
+    [
+        'taxonomy' => 'product_tag',
+        'slug'     => 'novichkam',
+    ],
+    [
+        'taxonomy' => 'product_cat',
+        'slug'     => 'dekorativno-cvetushchie',
+    ],
+    [
+        'taxonomy' => 'product_cat',
+        'slug'     => 'palms',
+    ],
+    [
+        'taxonomy' => 'product_tag',
+        'slug'     => 'pet-friendly',
+    ],
+    [
+        'taxonomy' => 'product_cat',
+        'slug'     => 'lianas',
+    ],
+    [
+        'taxonomy' => 'product_cat',
+        'slug'     => 'succulent',
+    ],
+];
 
-$placeholder_image = wc_placeholder_img_src();
+$terms = [];
 
+foreach ( $tiles as $tile ) {
 
-$shop_url = wc_get_page_permalink( 'shop' );
+    $term = get_term_by(
+        'slug',
+        $tile['slug'],
+        $tile['taxonomy']
+    );
 
-$args_terms = array(
-	'taxonomy'   => 'product_cat',
-	'parent'     => $parent,
-	'hide_empty' => $hide_empty,
-	'orderby'    => 'menu_order',
-	'order'      => 'ASC',
-);
-
-// if ( $number > 0 ) {
-// 	$args_terms['number'] = $number;
-// }
-
-$terms = get_terms( $args_terms );
-if ( is_wp_error( $terms ) ) {
-	return;
+    if ( $term && ! is_wp_error( $term ) ) {
+        $terms[] = $term;
+    }
 }
 ?>
 
 <div class="cats-tiles swiper--over">
-  <div class="swiper">
-    <div class="cats-tiles__wrapper swiper-wrapper">
-  
-      <?php foreach ( $terms as $term ) : ?>
-        <?php
-        // if ( ! reazy_category_has_available_products( $term->term_id ) ) {
-        // 	continue;
-        // }
-  
-        $front_image_id = (int) get_term_meta( $term->term_id, 'front_image', true );
-        $thumb_id       = $front_image_id ?: (int) get_term_meta( $term->term_id, 'thumbnail_id', true );
+    <div class="swiper">
+        <div class="cats-tiles__wrapper swiper-wrapper">
 
-        $img_url = $thumb_id ? wp_get_attachment_image_url( $thumb_id, 'woocommerce_thumbnail' ) : '';
-  
-        if ( ! $img_url && function_exists( 'wc_placeholder_img_src' ) ) {
-          $img_url = wc_placeholder_img_src( 'woocommerce_thumbnail' );
-        }
-  
-        $link = get_term_link( $term );
-  
-        if ( is_wp_error( $link ) ) {
-          continue;
-        }
-        ?>
-  
-        <a class="cats-tiles__link swiper-slide" href="<?php echo esc_url( $link ); ?>">
-          <span class="cats-tiles__media darken">
-            <?php if ( $img_url ) : ?>
-              <img loading="lazy" src="<?php echo esc_url( $img_url ); ?>" alt="<?php echo esc_attr( $term->name ); ?>">
-            <?php endif; ?>
-          </span>
-  
-          <span class="cats-tiles__title">
-            <?php echo esc_html( $term->name ); ?>
-  
-            <?php if ( $show_count ) : ?>
-              <span class="cats-tiles__count">(<?php echo (int) $term->count; ?>)</span>
-            <?php endif; ?>
-          </span>
-        </a>
-      <?php endforeach; ?>
-  
+            <?php foreach ( $terms as $term ) : ?>
+
+                <?php
+                $front_image_id = (int) get_term_meta( $term->term_id, 'front_image', true );
+                $thumb_id       = $front_image_id ?: (int) get_term_meta( $term->term_id, 'thumbnail_id', true );
+
+                $img_url = $thumb_id
+                    ? wp_get_attachment_image_url( $thumb_id, 'woocommerce_thumbnail' )
+                    : wc_placeholder_img_src( 'woocommerce_thumbnail' );
+
+                $link = get_term_link( $term );
+
+                if ( is_wp_error( $link ) ) {
+                    continue;
+                }
+                ?>
+
+                <a class="cats-tiles__link swiper-slide" href="<?php echo esc_url( $link ); ?>">
+                    <span class="cats-tiles__media darken">
+                        <img
+                            loading="lazy"
+                            src="<?php echo esc_url( $img_url ); ?>"
+                            alt="<?php echo esc_attr( $term->name ); ?>">
+                    </span>
+
+                    <span class="cats-tiles__title">
+                        <?php echo esc_html( $term->name ); ?>
+                    </span>
+                </a>
+
+            <?php endforeach; ?>
+
+        </div>
+
+        <div class="swiper-scrollbar"></div>
     </div>
-    <div class="swiper-scrollbar"></div>
-  </div>
-  <div class="swiper-button-prev"></div>
-  <div class="swiper-button-next"></div>
+
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
 </div>
