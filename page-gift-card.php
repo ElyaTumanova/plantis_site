2ed5c48e002189c575dac7bfc03608d2da350cbb
ElyaTumanova <122990149+ElyaTumanova@users.noreply.github.com>
2026-07-13 16:20:27 +0300

    gift card

--- page-gift-card.php
+++ page-gift-card.php
@@ -68,22 +68,22 @@ if ( $gift_card_id > 0 ) {
 
 //for dev
 
-// echo('<pre>');
+echo('<pre>');
 
-// // print_r($gift_card);
-// // print_r($order);
-// // if ( $gift_card ) {
-// //     echo 'Карта найдена.';
-// // } else {
-// //     echo 'Карта с таким номером не найдена.';
-// // }
-// // print_r($raw_gcnum);
-// // print_r($gcnum);
-// // print_r($gift_card_design);
-// // print_r($image_key);
-// // print_r($gradient_key);
+print_r($gift_card);
+// print_r($order);
+// if ( $gift_card ) {
+//     echo 'Карта найдена.';
+// } else {
+//     echo 'Карта с таким номером не найдена.';
+// }
+// print_r($raw_gcnum);
+// print_r($gcnum);
+// print_r($gift_card_design);
+// print_r($image_key);
+// print_r($gradient_key);
 
-// echo('</pre>');
+echo('</pre>');
 ?>
 
 <?php if ( $gift_card ):?>
